2ebaac9d2f7b0ca2c44b2a7efcb27483c776f4ec
Amir Ebrahimi <amir@unity3d.com>
2016-06-27 17:25:15 -0700

    Add Touch support to EditorVR

---
+++ Scripts/Proxies/TouchProxy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine.InputNew;
+
+namespace UnityEngine.VR.Proxies
+{
+	public class TouchProxy : TwoHandedProxyBase
+	{
+		public override void Awake()
+		{
+			base.Awake();
+			U.AddComponent<OVRTouchInputToEvents>(gameObject);
+		}		
+	}
+}
