ab8e3d91fc430002d696124e8bd2ae490b84fc9d
Dylan Urquidi <dylanu@unity3d.com>
2016-07-13 11:50:33 -0700

    Add initial Blink Locomotion Tool content

---
+++ Tools/BlinkLocomotionTool/BlinkLocomotionTool.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputNew;
+using UnityEngine.UI;
+using UnityEngine.VR.Tools;
+
+public class BlinkLocomotionTool : MonoBehaviour, ITool, ILocomotion, IRay, IStandardActionMap
+{
+    public Transform RayOrigin { get; set; }
+
+    // TODO: 
+    // TrackedObjectInput should probably be coming from an interface / setter in EditorVR. 
+    // My intention is to use TrackedObjectInput to be able to preview where the user's hands will be when the blink is executed. 
+    // Normally we'd use a ray transform to get the hand position, but in this case it's a one-handed tool (so only gets the one ray), 
+    // but we want to have the option to visualize both hands. 
+    public TrackedObject TrackedObjectInput { get; set; }
+
+    public Standard StandardInput { get; set; }
+
+    public Transform ViewerPivot
+    {
+        set
+        {
+            // TODO:
+            // We need a separate transform reference for body, with its origin at the ground / feet,
+            // so we can move the avatar to a spot on the ground (without moving their *head* into the ground!). 
+            // For the time being, I'm just setting BodyOrigin to the ViewerPivot.
+            m_ViewerPivot = value;
+            m_BodyOrigin = value;
+        }
+    }
+    
+    private Transform m_ViewerPivot;
+
+    private Plane m_DefaultGroundPlane;
+
+    private Transform m_BodyOrigin;
+
+    [Header("Settings")]
+    [Tooltip("Total fade time (fade in / fade out each take half this amount)")]
+    [SerializeField]
+    private float m_FadeTime = 0.5f;
+    [SerializeField]
+    private bool m_ShowHands = true;
+    [SerializeField]
+    private bool m_DoSmoothing = true;
+    [Tooltip("Lerp amount. Closer to 0 = smoother, closer to 1 = faster.")]
+    [Range(0f, 1f)]
+    [SerializeField]
+    private float m_IndicatorSmoothing = 0.2f;
+    [Header("References")]
+    [SerializeField]
+    private ActionMap m_BlinkActionMap;
+    [SerializeField]
+    private GameObject m_BlinkArcRendererPrefab;
+    [Tooltip("UI plane that will be instantiated in front of the user's vision and provide the fade color.")]
+    [SerializeField]
+    private GameObject m_FadeImagePrefab;
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject m_AvatarBaseIndicatorPrefab;
+    [SerializeField]
+    private GameObject m_AvatarHeadIndicatorPrefab;
+    [SerializeField]
+    private GameObject m_AvatarLeftControllerIndicatorPrefab; // TODO: We probably want this to just be fetched from the proxy / avatar rig.
+    [SerializeField]
+    private GameObject m_AvatarRightControllerIndicatorPrefab;
+
+    private Transform m_TrackingCenter;
+    private Transform m_FootIndicator;
+    private Transform m_HeadIndicator;
+    private Transform m_LeftControllerIndicator;
+    private Transform m_RightControllerIndicator;
+    private RaycastHit m_BlinkTargetHit;
+    private RaycastHit m_HitBelowHead;
+    private bool m_WasOnGround;
+    private float m_StartingElevation;
+    private Image m_FadeImage;
+    private GameObject m_FadeImageGO;
+    private GameObject m_BlinkArcGO;
+    
+    void Start()
+    {
+        // Creating a default plane to raycast against, so that the user can blink around without having to create a ground first. 
+        m_DefaultGroundPlane = new Plane(Vector3.up, Vector3.zero);
+        
+        m_FadeTime = m_FadeTime / 2f; // Fade in/out are each half of the total.
+        m_StartingElevation = m_ViewerPivot.position.y;
+        m_TrackingCenter = new GameObject("Tracking Center Indicator").transform;
+        m_FootIndicator = Instantiate(m_AvatarBaseIndicatorPrefab).transform;
+        m_HeadIndicator = Instantiate(m_AvatarHeadIndicatorPrefab).transform;
+        // Parent other indicators to the trackingCenter xform so that we don't have to position (and smooth) each of them;
+        // we can just adjust local position for head height etc.
+        m_FootIndicator.parent = m_TrackingCenter; 
+        m_HeadIndicator.parent = m_TrackingCenter;
+        if (m_ShowHands)
+        {
+            m_LeftControllerIndicator = Instantiate(m_AvatarLeftControllerIndicatorPrefab).transform;
+            m_RightControllerIndicator = Instantiate(m_AvatarRightControllerIndicatorPrefab).transform;
+            m_LeftControllerIndicator.parent = m_TrackingCenter;
+            m_RightControllerIndicator.parent = m_TrackingCenter;
+            m_LeftControllerIndicator.localPosition = m_RightControllerIndicator.localPosition = Vector3.zero;
+        }
+        m_HeadIndicator.localPosition = Vector3.zero;
+        HideIndicator();
+
+        m_BlinkArcGO = Instantiate(m_BlinkArcRendererPrefab);
+        m_BlinkArcGO.transform.parent = RayOrigin;
+        m_BlinkArcGO.transform.localPosition = Vector3.zero;
+        m_BlinkArcGO.SetActive(false);
+
+        // Create the UI element that does the fading. 
+        // TODO: 
+        // This should probably be a static piece of the avatar rig rather than created by this tool.
+        // We should also look into alternate methods for doing fades - maybe using the SDK-specific 
+        // fades for platforms that support it, and fall back to a method like this for those that don't.
+        m_FadeImageGO = Instantiate(m_FadeImagePrefab);
+        m_FadeImageGO.transform.position = m_ViewerPivot.position + m_ViewerPivot.forward * 0.1f;
+        m_FadeImageGO.transform.parent = m_ViewerPivot;
+        m_FadeImage = m_FadeImagePrefab.GetComponent<Image>();
+    }
+    
+    // Clean up objects when the tool is deactivated -- assuming that removing a tool from its stack destroys the component.
+    // TODO: Pool these objects, rather than creating and destroying them with the tool.
+    void OnDestroy()
+    {
+        Destroy(m_TrackingCenter.gameObject);
+        Destroy(m_BlinkArcGO);
+        Destroy(m_FadeImageGO);
+    }
+
+    void Update()
+    {
+        if (StandardInput.action.wasJustPressed)
+        {
+            m_BlinkArcGO.SetActive(true);
+        }
+        else if (StandardInput.action.isHeld)
+        {
+            Ray ray = new Ray(RayOrigin.position, RayOrigin.forward);
+            float rayDistance;
+            if (m_DefaultGroundPlane.Raycast(ray, out rayDistance))
+            {
+                Vector3 offset = m_BodyOrigin.position - m_HitBelowHead.point;
+                offset.y = m_StartingElevation - (m_ViewerPivot.position.y - m_HitBelowHead.point.y);
+                m_TrackingCenter.position = m_BlinkTargetHit.point;
+                m_FootIndicator.localPosition = m_HitBelowHead.point;
+                m_HeadIndicator.localPosition = m_ViewerPivot.position - m_HitBelowHead.point;
+                m_HeadIndicator.rotation = m_ViewerPivot.rotation;
+                if (m_ShowHands)
+                {
+                    m_LeftControllerIndicator.localPosition = TrackedObjectInput.leftPosition.vector3 - m_HitBelowHead.point;
+                    m_LeftControllerIndicator.rotation = TrackedObjectInput.leftRotation.quaternion;
+                    m_RightControllerIndicator.localPosition = TrackedObjectInput.rightPosition.vector3 - m_HitBelowHead.point;
+                    m_RightControllerIndicator.rotation = TrackedObjectInput.rightRotation.quaternion;
+                }
+                if (m_DoSmoothing)
+                {
+                    m_TrackingCenter.position = !m_WasOnGround
+                        ? m_BlinkTargetHit.point
+                        : Vector3.Lerp(m_TrackingCenter.position, m_BlinkTargetHit.point, m_IndicatorSmoothing);
+                }
+                else
+                {
+                    m_TrackingCenter.position = m_BlinkTargetHit.point;
+                }
+                m_WasOnGround = true;
+
+            }
+            else
+            {
+                m_WasOnGround = false;
+                HideIndicator();
+            }
+        }
+        else if (StandardInput.action.wasJustReleased)
+        {
+            m_BlinkArcGO.SetActive(false);
+            if (m_WasOnGround)
+            {
+                StartCoroutine(FadeAndTeleport());
+            }
+        }
+    }
+    
+    // TODO: Fade should be a common/U method, perhaps with a delegate to execute during the blackout.
+    private IEnumerator FadeAndTeleport()
+    {
+        m_FadeImage.gameObject.SetActive(true);
+        m_FadeImage.CrossFadeAlpha(0, 0, false);
+        m_FadeImage.CrossFadeAlpha(1, m_FadeTime, false);
+        yield return new WaitForSeconds(m_FadeTime);
+        Vector3 pos = m_TrackingCenter.position + (m_BodyOrigin.position - m_ViewerPivot.position);
+        pos.y = m_StartingElevation;
+        m_BodyOrigin.position = pos;
+        m_WasOnGround = false;
+        HideIndicator();
+        m_FadeImage.CrossFadeAlpha(0, m_FadeTime, false);
+        yield return new WaitForSeconds(m_FadeTime);
+        m_FadeImage.gameObject.SetActive(false);
+    }
+
+    private void HideIndicator()
+    {
+        m_TrackingCenter.position = Vector3.down * 10000;
+    }
+}
---
+++ Tools/BlinkLocomotionTool/VRTeleporter.cs
@@ -0,0 +1,304 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class VRTeleporter : MonoBehaviour
+{
+    public bool useColliders = true;
+
+    [Tooltip( "The layer of the collider that the blink tool will cast against" )]
+    public LayerMask layerMask;
+
+    public bool useBlinkAvatar;
+    public Transform avatar;
+    [Tooltip( "The transform of the object used to indicate where theplayArea's head will be after the blink. Should be a child of the avatar" )]
+    public Transform avatarHead;
+
+    [Tooltip( "The max distance that the user can move in one blink" )]
+    public float maxDistance = 30f;
+
+    public float moveTime = 0.2f;
+    public float moveDelay = 0.5f;
+
+    public float hoverSphereRadius = 0.05f;
+    public LayerMask hoverLayerMask = -1;
+    public float hoverUpdateInterval = 0.1f;
+
+    // refs
+    protected Transform hmdTransform;
+    protected Vector3 lastTargetPosition;
+    //
+
+    protected bool isBlinking = false;
+    protected bool isMoving = false; // are we lerping to our blink target?
+    protected Vector3 startingPosition = Vector3.zero;
+    protected Transform avatarCachedParent;
+
+    protected GameObject[] blinkColliders;
+    Transform playArea;
+    Transform toolPoint;
+
+    public void Awake()
+    {
+        avatar.gameObject.SetActive(false);
+        playArea = FindObjectOfType<SteamVR_PlayArea>().transform;
+    }
+
+    public void Start()
+    {
+        blinkColliders = GameObject.FindGameObjectsWithTag("Blink Colliders");
+    }
+
+    /// <summary>
+    /// Start the blink
+    /// </summary>
+    public void InitializeTeleport(Transform origin)
+    {
+        if (isBlinking || isMoving || IsOriginInsideBlocker(transform) || !gameObject.activeInHierarchy) return;
+
+        // cache the parent for returning to after blink
+        avatarCachedParent = avatar.parent;
+
+        // unparent the avatar for free movement
+        avatar.parent = null;
+
+        // save the starting position to get the blink vector when the blink is done
+        startingPosition = new Vector3(hmdTransform.position.x, 0f, hmdTransform.position.z);
+
+        StartCoroutine(ProcessLocomotionAiming());
+        // avatar only set to be visible when a ray hits a valid location
+    }
+
+    /// <summary>
+    /// Called when the appropriate button is lifted up
+    /// </summary>
+    public void EndTeleport(GameObject hand)
+    {
+        // make sure it's with the same hand as we started with
+        if (toolPoint != hand.transform)
+        {
+            Debug.Log( "Blink button raised on a different controller!" );
+            return;
+        }
+
+        //todo add a fadein/out
+        avatar.gameObject.SetActive( false );
+        if (!isBlinking || isMoving) return;
+
+        isBlinking = false;
+
+        if (!IsOriginInsideBlocker(hand.transform))
+        {
+            StartCoroutine(MoveToTargetLocation());
+        }
+
+        // place the avatar back under it's old parent
+        avatar.parent = avatarCachedParent;
+    }
+
+    public IEnumerator MoveToTargetLocation()
+    {
+        isMoving = true;
+        // find out how far we moved
+        var blinkVector = avatar.position - startingPosition;
+        var targetPosition = playArea.transform.position + blinkVector;
+
+        yield return new WaitForSecondsRealtime(moveDelay);
+
+        var t = 0f;
+        var perc = 0f;
+        while (perc < 1f)
+        {
+            playArea.transform.position = Vector3.Lerp(playArea.transform.position, targetPosition, perc);
+            t += Time.unscaledDeltaTime;
+            perc = t / moveTime;
+            yield return null;
+        }
+        // move the root transform
+        playArea.transform.position = targetPosition;
+
+        isMoving = false;
+//        avatar.parent = avatarCachedParent;
+    }
+
+    // Update is called once per frame
+    IEnumerator ProcessLocomotionAiming()
+    {
+        isBlinking = true;
+
+        while (isBlinking)
+        {
+            if (IsOriginInsideBlocker(toolPoint))
+            {
+                avatar.gameObject.SetActive(false);
+                yield return null;
+                continue;
+            }
+
+            avatar.gameObject.SetActive(true);
+        
+            // Move the target visualizer off by the beam
+
+            var transformPos = playArea.transform.position; // ???
+
+            var targetPosition = transformPos;
+
+            Ray ray = new Ray(toolPoint.position, toolPoint.forward);
+
+            // if there are no colliders just use a ray
+            // If we are, project a ray from the controller to the ground
+            if (!useColliders)
+            {
+                // Start at the max distance
+                var timeToFloor = maxDistance;
+
+                // make sure ray isn't parallel to the ground
+                if (Mathf.Abs(ray.direction.y) > 0.01f)
+                {
+                    var rayToFloor = (transformPos.y - toolPoint.position.y)/ray.direction.y;
+                    timeToFloor = Mathf.Min(Mathf.Abs(rayToFloor), timeToFloor);
+
+                    avatar.gameObject.SetActive(true);
+                }
+
+                //	    // Move the target visualizer off by the beam
+                //		var targetPosition = objectToMove.position;
+                targetPosition.x += (ray.direction.x*timeToFloor);
+                targetPosition.z += (ray.direction.z*timeToFloor);
+            }
+            else // there are blink colliders
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, maxDistance, layerMask) )
+//                    && (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Blink Collider"))
+                {
+                    // if we hit a blink blocker...
+                    if (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Blink Blocker")
+                    {
+                        if (Mathf.Abs(Vector3.Dot(hit.normal, Vector3.up)) < 0.9f) // horizontal-ish -- most likely hit a wall collider...
+                        {
+                            var newRayOrigin = hit.point + hit.normal * 0.1f;
+                            var newRay = new Ray( newRayOrigin, Vector3.down );
+
+                            // cast down a little ways from the wall looking for blink collider
+                            if ( Physics.Raycast( newRay, out hit, maxDistance, layerMask ) )
+                            {
+                                if ( LayerMask.LayerToName( hit.transform.gameObject.layer ) == "Blink Blocker" )
+                                {
+                                    var shortestDistance = Mathf.Infinity;
+
+                                    // we hit another blocker, need to do a search for the closest collider
+                                    foreach (var bc in blinkColliders)
+                                    {
+                                        var closestPoint = bc.GetComponent<Collider>().ClosestPointOnBounds(hit.point);
+                                        var distance = Vector3.Distance(toolPoint.position, closestPoint);
+                                        if (distance < shortestDistance)
+                                        {
+                                            shortestDistance = distance;
+                                            avatar.gameObject.SetActive(true);
+                                            targetPosition = closestPoint;
+                                        }
+                                    }
+                                }
+                                else if ( LayerMask.LayerToName( hit.transform.gameObject.layer ) == "Blink Collider" )
+                                {
+                                    avatar.gameObject.SetActive( true );
+                                    targetPosition = hit.point;
+                                }
+                            }
+                            else
+                            {
+                                Debug.LogError( "No collider hit -- odd" );
+                                break;
+                            }
+                        }
+                        else // over a hole or prop
+                        {
+                            // find a spot where you can be
+                            var spotFound = false;
+                            var newRayOrigin = hit.point + Vector3.up * 1f;
+                            while (!spotFound)
+                            {
+                                var newRay = new Ray(newRayOrigin, Vector3.down);
+
+                                if (Physics.Raycast(newRay, out hit, maxDistance, layerMask))
+                                {
+                                    if (LayerMask.LayerToName(hit.transform.gameObject.layer) != "Blink Collider")
+                                    {
+                                        newRayOrigin = newRayOrigin + 0.1f*-ray.direction;
+
+                                        if (Vector3.Distance(newRayOrigin, toolPoint.position) < 0.2f)
+                                        {
+                                            spotFound = true;
+                                        }
+                                    }
+                                    else if ( LayerMask.LayerToName( hit.transform.gameObject.layer ) == "Blink Collider" )
+                                    {
+                                        spotFound = true;
+                                        avatar.gameObject.SetActive( true );
+                                        targetPosition = hit.point;
+                                    }
+                                }
+                                else
+                                {
+                                    Debug.LogError("No collider hit -- odd");
+                                    break;
+                                }
+
+                            }
+                        }
+
+                    } // else we are gravy
+                    else if (LayerMask.LayerToName(hit.transform.gameObject.layer) == "Blink Collider")
+                    {
+                        avatar.gameObject.SetActive(true);
+                        targetPosition = hit.point;
+
+                    }
+                }
+                else // we didn't hit any collider
+                {
+                    targetPosition = lastTargetPosition;
+                }
+            }
+            
+            // if no new valid blink location was found, hide the avatar
+            if (targetPosition == transformPos)
+            {
+                avatar.gameObject.SetActive(false);
+            }
+
+            var smoothingFactor = Mathf.Min((lastTargetPosition - targetPosition).magnitude, 1.0f);
+            targetPosition = Vector3.Lerp(lastTargetPosition, targetPosition, smoothingFactor);
+            lastTargetPosition = targetPosition;
+
+            var standInPosition = targetPosition;
+            standInPosition.y = 0f;
+            avatar.transform.position = standInPosition;
+
+            // show hmd height on avatar
+            var hmdPos = hmdTransform.position;
+            avatarHead.position = new Vector3(avatarHead.position.x, hmdPos.y, avatarHead.position.z);
+
+            // show hmd head rotation on avatar
+            avatar.rotation = Quaternion.Euler(avatar.eulerAngles.x, hmdTransform.eulerAngles.y, avatar.eulerAngles.z);
+
+            yield return null;
+        }
+    }
+
+    protected bool IsOriginInsideBlocker(Transform hand)
+    {
+        var overlappingColliders = Physics.OverlapSphere( hand.position, hoverSphereRadius, hoverLayerMask.value );
+        foreach ( Collider collider in overlappingColliders )
+        {
+            if (LayerMask.LayerToName(collider.gameObject.layer) == "Blink Blocker")
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
