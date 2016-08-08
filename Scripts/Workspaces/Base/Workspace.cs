be171b6fa7d32abe7473019bb21d821e0e516bd8
Matt Schoen <schoen@unity3d.com>
2016-08-08 16:33:48 -0400

    Removes gizmos, cleans up Workspace.cs

--- Scripts/Workspaces/Base/Workspace.cs
+++ Scripts/Workspaces/Base/Workspace.cs
@@ -1,6 +1,4 @@
-﻿#define DEBUGDRAW
-
-using System;
+﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.VR.Utilities;
@@ -41,24 +39,14 @@ public abstract class Workspace : MonoBehaviour, IInstantiateUI
 		baseObject.transform.SetParent(transform);
 		handle = baseObject.GetComponent<WorkspaceHandle>();
 		handle.owner = this;
-		handle.sceneContainer.transform.localPosition = Vector3.up * contentHeight;
-		//TODO: ASSERT if handleRect is null
-		//Q: use SetParent(transform, false)?
+		handle.sceneContainer.transform.localPosition = Vector3.up * contentHeight;	   
 		baseObject.transform.localPosition = Vector3.zero;
 		baseObject.transform.localRotation = Quaternion.identity;
 		//baseObject.transform.localScale = Vector3.one;   
 		bounds = new Bounds(Vector3.up * defaultBounds.y * 0.5f, defaultBounds);
 		handle.SetBounds(bounds);
-	}
-#if DEBUGDRAW
-	void OnDrawGizmos()
-	{
-		Gizmos.matrix = handle.sceneContainer.transform.localToWorldMatrix;
-		Gizmos.DrawWireCube(bounds.center, bounds.size);
-		Gizmos.DrawWireCube(outerBounds.center, outerBounds.size);
-	}
-#endif
-	//Q: Should we allow SetBounds to change position?
+	}											
+
 	public void SetBounds(Bounds b)
 	{
 		if (!b.Equals(bounds))
