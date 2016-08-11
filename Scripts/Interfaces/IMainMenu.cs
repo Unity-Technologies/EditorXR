6e22a86e1cad72d070196a28914b2db905fe5945
Dylan Urquidi <dylanu@unity3d.com>
2016-08-11 11:12:14 -0700

    Add additional properties and function declarations to IMainMenu

--- Scripts/Interfaces/IMainMenu.cs
+++ Scripts/Interfaces/IMainMenu.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine.InputNew;
 
 namespace UnityEngine.VR.Tools
 {
-	public interface IMainMenu
-	{
-		List<Type> menuTools { set; }
-		Func<IMainMenu, Type, bool> selectTool { set; }
-	}
+    public interface IMainMenu
+    {
+        Camera eventCamera { get; set; }
+        Menu menuActionInput { get; set; }
+        Transform menuInputOrigin { get; set; }
+        Transform menuOrigin { get; set; }
+        List<Type> menuTools { set; }
+        Func<IMainMenu, Type, bool> selectTool { set; }
+
+        /// <summary>
+        /// Allow external enabling of the MainMenu
+        /// </summary>
+        void Enable();
+
+        /// <summary>
+        /// Allow external disabling of the MainMenu
+        /// </summary>
+        void Disable();
+    }
 }
