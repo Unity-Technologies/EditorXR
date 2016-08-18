66e1a318bf61b7425c97d2c02a2f0a3e5dc1c4c4
Dylan Urquidi <dylanu@unity3d.com>
2016-08-17 17:59:33 -0700

    Add IAction core interface lattice

---
+++ Scripts/Interfaces/IAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+
+namespace UnityEngine.VR.Tools
+{
+	public interface IAction
+	{
+        /// <summary>
+        /// The icon representing this Action that will be displayed in menus
+        /// </summary>
+	    Sprite icon { get; set; }
+
+		/// <summary>
+		/// Execute this action
+		/// </summary>
+		void Execute();
+	}
+}
