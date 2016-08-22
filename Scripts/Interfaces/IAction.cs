7a140e97c99994233cca7ce764183491914746d8
Dylan Urquidi <dylanu@unity3d.com>
2016-08-22 01:17:54 -0700

    Refactor the Execute() method in IAction to return a bool for success testing

--- Scripts/Interfaces/IAction.cs
+++ Scripts/Interfaces/IAction.cs
@@ -4,14 +4,14 @@ namespace UnityEngine.VR.Tools
 {
 	public interface IAction
 	{
-        /// <summary>
-        /// The icon representing this Action that will be displayed in menus
-        /// </summary>
-	    Sprite icon { get; set; }
+		/// <summary>
+		/// The icon representing this Action that will be displayed in menus
+		/// </summary>
+		Sprite icon { get; set; }
 
 		/// <summary>
 		/// Execute this action
 		/// </summary>
-		void Execute();
+		bool Execute(); 
 	}
 }
