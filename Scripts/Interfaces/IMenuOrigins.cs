d4d96445093053b1e8ed849624be2917ecc08505
Dylan Urquidi <dylanu@unity3d.com>
2016-08-31 18:24:03 -0700

    Add IMenuOrigins interface

---
+++ Scripts/Interfaces/IMenuOrigins.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public interface IMenuOrigins
+{
+	/// <summary>
+	/// The transform under which the menu input object should be parented, inheriting position, scale, and rotation
+	/// </summary>
+	Transform menuInputOrigin { get; set; }
+
+	/// <summary>
+	/// The transform under which the menu should be parented, inheriting position and rotation
+	/// </summary>
+	Transform menuOrigin { get; set; }
+}
