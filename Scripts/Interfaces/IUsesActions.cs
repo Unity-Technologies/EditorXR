0696bd72ae216e93e0e5f5e55fa6062b5e71c135
Dylan Urquidi <dylanu@unity3d.com>
2016-09-06 11:05:03 -0700

    Move IUsesActions into UnityEngine.VR.Actions; Add performAction() as well

--- Scripts/Interfaces/IUsesActions.cs
+++ Scripts/Interfaces/IUsesActions.cs
@@ -1,7 +1,16 @@
-﻿using System.Collections.Generic;
-using UnityEngine.VR.Tools;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.VR.Actions;
 
 public interface IUsesActions
 {
+	/// <summary>
+	/// Collection of actions
+	/// </summary>
 	List<IAction> actions { set; }
+
+	/// <summary>
+	/// The delegate that performs an action
+	/// </summary>
+	Func<IAction, bool> performAction { set; }
 }
