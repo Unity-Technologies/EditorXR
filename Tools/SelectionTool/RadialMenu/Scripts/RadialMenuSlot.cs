6125802d71358818f045aee8623b9f8c67d3dfa1
Dylan Urquidi <dylanu@unity3d.com>
2016-09-06 11:02:36 -0700

    Add RadialMenuSlot & RadialMenu UI controllers

---
+++ Tools/SelectionTool/RadialMenu/Scripts/RadialMenuSlot.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+
+namespace UnityEngine.VR.Menus
+{
+	public class RadialMenuSlot : MonoBehaviour
+	{
+		[SerializeField]
+		private MeshRenderer m_InsetMeshRenderer;
+
+		[SerializeField]
+		private Transform m_MenuInset;
+
+		[SerializeField]
+		private CanvasGroup m_CanvasGroup;
+
+		[SerializeField]
+		private Image m_Icon;
+
+		[SerializeField]
+		private Button m_Button;
+
+		private Transform m_parentTransform;
+		private Vector3 m_IconDirection;
+
+		private Material m_Material;
+		private Vector3 m_VisibleMenuInsetLocalScale;
+		private Vector3 m_HiddenMenuInsetLocalScale;
+
+		public Button button { get { return m_Button; } }
+
+		private static Quaternion m_HiddenLocalRotation; // All menu slots share the same hidden location
+		public static Quaternion hiddenLocalRotation { get { return m_HiddenLocalRotation; } }
+
+		private Quaternion m_VisibleLocalRotation;
+		public Quaternion visibleLocalRotation { get { return m_VisibleLocalRotation; } set { m_VisibleLocalRotation = value; } }
+
+		private Sprite m_IconSprite;
+		public Sprite iconSprite
+		{
+			set
+			{
+				m_IconSprite = value;
+				m_Icon.sprite = m_IconSprite;
+			}
+		}
+
+		private void Awake()
+		{
+			m_HiddenLocalRotation = transform.localRotation;
+			m_Material = m_InsetMeshRenderer.sharedMaterial;
+			m_VisibleMenuInsetLocalScale = m_MenuInset.localScale;
+			m_HiddenMenuInsetLocalScale = new Vector3(m_VisibleMenuInsetLocalScale.x, 0f, m_VisibleMenuInsetLocalScale.z);
+		}
+
+		private void OnEnable()
+		{
+			m_MenuInset.localScale = m_HiddenMenuInsetLocalScale;
+			StartCoroutine(FadeSlotOpacityIn());
+		}
+
+		private void OnDisable()
+		{
+			//StartCoroutine(FadeSlotOpacityOut());
+		}
+
+		private void OnTransformParentChanged()
+		{
+			m_parentTransform = transform.parent;
+			//m_IconDirection = m_parentTransform.local
+			//Debug.LogError("<color=red>Radial Menu Slot Transform parent changed to : " + parentTransform.name + "</color>");
+		}
+
+		private void Update()
+		{
+			//m_Icon.transform.LookAt(m_parentTransform.forward);
+		}
+
+		private IEnumerator FadeSlotOpacityIn()
+		{
+			m_CanvasGroup.interactable = false;
+			m_Material.SetFloat("_Alpha", 0);
+			m_MenuInset.localScale = m_HiddenMenuInsetLocalScale;
+
+			float opacity = 0;
+			while (opacity < 1)
+			{
+				opacity += Time.unscaledDeltaTime / 2f;
+				m_Material.SetFloat("_Alpha", opacity);
+				m_MenuInset.localScale = Vector3.Lerp(m_HiddenMenuInsetLocalScale, m_VisibleMenuInsetLocalScale, opacity);
+				//m_CanvasGroup.alpha = opacity;
+				yield return null;
+			}
+
+			m_CanvasGroup.interactable = true;
+		}
+
+		private IEnumerator FadeSlotOpacityOut()
+		{
+			m_CanvasGroup.interactable = false;
+
+			float opacity = m_Material.GetFloat("_Alpha");
+			while (opacity > 0)
+			{
+				opacity -= Time.unscaledDeltaTime * 1.5f;
+				m_Material.SetFloat("_Alpha", opacity);
+				m_MenuInset.localScale = Vector3.Lerp(m_HiddenMenuInsetLocalScale, m_VisibleMenuInsetLocalScale, opacity * opacity);
+				//m_CanvasGroup.alpha = opacity;
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Tools/SelectionTool/RadialMenu/Scripts/RadialMenuUI.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.VR.Actions;
+using UnityEngine.VR.Utilities;
+
+namespace UnityEngine.VR.Menus
+{
+	public class RadialMenuUI : MonoBehaviour, IInstantiateUI
+	{
+		[SerializeField]
+		private Image m_SlotsMask;
+
+		[SerializeField]
+		private RadialMenuSlot m_RadialMenuSlotTemplate;
+
+		[SerializeField]
+		private Transform m_SlotContainer;
+
+		private const int m_SlotCount = 16;
+		private Vector3 m_AlternateMenuOriginOriginalLocalScale;
+
+		private List<RadialMenuSlot> m_RadialMenuSlots;
+		private Coroutine m_ShowCoroutine;
+		private Coroutine m_HideCoroutine;
+
+		public Func<IAction, bool> performAction { private get; set; }
+		public Func<GameObject, GameObject> instantiateUI { private get; set; }
+
+		public Transform alternateMenuOrigin
+		{
+			get { return m_AlternateMenuOrigin; }
+			set
+			{
+				m_AlternateMenuOrigin = value;
+				transform.SetParent(m_AlternateMenuOrigin);
+				transform.localPosition = Vector3.zero;
+				transform.localRotation = Quaternion.identity;
+				transform.localScale = Vector3.one;
+				m_AlternateMenuOriginOriginalLocalScale = m_AlternateMenuOrigin.localScale;
+			}
+		}
+		private Transform m_AlternateMenuOrigin;
+
+		public List<IAction> m_Actions;
+		public List<IAction> actions
+		{
+			get { return m_Actions; }
+			set
+			{
+				if (value == m_Actions) // only change visual state if the actions have changed.  Reference checking for now.
+					return;
+
+				m_Actions = value;
+
+				if (m_ShowCoroutine != null)
+				{
+					StopCoroutine(m_ShowCoroutine);
+					m_ShowCoroutine = null;
+				}
+
+				if (m_HideCoroutine != null)
+				{
+					StopCoroutine(m_HideCoroutine);
+					m_HideCoroutine = null;
+				}
+				//TODO validate that actions & count are the same
+				if (value != null && value.Count > 0)
+					m_ShowCoroutine = StartCoroutine(AnimateShow());
+				else
+					m_HideCoroutine = StartCoroutine(AnimateHide());
+			}
+		}
+
+		public bool visible
+		{
+			get; set;
+			/*
+			get { return m_VisibilityState == VisibilityState.Visible; }
+			set
+			{
+				switch (m_VisibilityState)
+				{
+					case VisibilityState.TransitioningOut:
+					case VisibilityState.Hidden:
+						if (value)
+						{
+							if (m_VisibilityCoroutine != null)
+								StopCoroutine(m_VisibilityCoroutine);
+							m_VisibilityCoroutine = StartCoroutine(AnimateShow());
+						}
+						return;
+					case VisibilityState.TransitioningIn:
+					case VisibilityState.Visible:
+						if (!value)
+						{
+							if (m_VisibilityCoroutine != null)
+								StopCoroutine(m_VisibilityCoroutine);
+							m_VisibilityCoroutine = StartCoroutine(AnimateHide());
+						}
+						return;
+				}
+			}
+			*/
+		}
+
+		public void Setup()
+		{
+			m_RadialMenuSlots = new List<RadialMenuSlot>();
+
+			for (int i = 0; i < m_SlotCount; ++i)
+			{
+				Transform menuSlot = null;
+#if UNITY_EDITOR
+				menuSlot = instantiateUI(m_RadialMenuSlotTemplate.gameObject).transform;
+#else
+				menuSlot = GameObject.Instantiate(m_RadialMenuSlotTemplate.gameObject).transform;
+#endif
+				var slotController = menuSlot.GetComponent<RadialMenuSlot>();
+				m_RadialMenuSlots.Add(slotController);
+				menuSlot.SetParent(m_SlotContainer);
+				menuSlot.localPosition = Vector3.zero;
+				menuSlot.localRotation = Quaternion.identity;
+				menuSlot.localScale = Vector3.one;
+			}
+			SetupRadialSlotPositions();
+		}
+
+		private void Start()
+		{
+			//SetupRadialSlotPositions();
+			Debug.LogError("<color=green>Beginning Radial menu Start</color>");
+			//StartCoroutine(AnimateSlotReveal(8));
+		}
+
+		private void SetupRadialSlotPositions()
+		{
+			const float rotationSpacing = 22.5f;
+			for (int i = 0; i < m_SlotCount; ++i)
+			{
+				m_RadialMenuSlots[i].visibleLocalRotation = Quaternion.AngleAxis(rotationSpacing * i, Vector3.up);
+				// todo setup automatic hiding in start on the slot controllers
+			}
+		}
+
+		private void Show(List<IAction> menuActions)
+		{
+			// if list count is zero, hide
+			// if greather, and the icons are not the same, then hide then show with new icons
+			// if the same, dont hide, just stay showing
+			Debug.LogError("Show called in RadialMenuVisuals");
+
+			// BLOCK INPUT WHILE SHOWING OR HIDING!!!!!
+
+			/*
+			b.button.onClick.RemoveAllListeners();
+			b.button.onClick.AddListener(() =>
+			{
+				if (visible && b.node.HasValue)
+					selectTool(b.node.Value, toolType);
+			});
+			*/
+
+			if (menuActions != null && menuActions.Count > 0)
+			{
+				for (int i = 0; i < menuActions.Count; ++i)
+				{
+					m_RadialMenuSlots[i].iconSprite = menuActions[i].icon;
+					m_RadialMenuSlots[i].button.onClick.RemoveAllListeners();
+					m_RadialMenuSlots[i].button.onClick.AddListener(() =>
+					{
+						performAction(menuActions[i]);
+					});
+				}
+
+				StartCoroutine(AnimateShow());
+			}
+			else
+				StartCoroutine(AnimateHide());
+		}
+
+		private void Hide()
+		{
+			Debug.LogError("Hide called in RadialMenuVisuals");
+		}
+
+		private IEnumerator AnimateShow()
+		{
+			for (int i = 0; i < m_Actions.Count; ++i)
+			{
+				m_RadialMenuSlots[i].iconSprite = m_Actions[i].icon;
+				m_RadialMenuSlots[i].button.onClick.RemoveAllListeners();
+				m_RadialMenuSlots[i].button.onClick.AddListener(() =>
+				{
+					performAction(m_Actions[i]);
+				});
+			}
+			
+			//int slotsToReveal = m_Actions.Count;
+			m_SlotsMask.fillAmount = 1f;
+			//yield return new WaitForSeconds(1f);
+
+			float revealAmount = 0f;
+			Quaternion hiddenSlotRotation = RadialMenuSlot.hiddenLocalRotation;
+
+			for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				m_RadialMenuSlots[i].enabled = true;
+
+			while (revealAmount < 1)
+			{
+				revealAmount += Time.unscaledDeltaTime * 4;
+
+				for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				{
+					m_RadialMenuSlots[i].enabled = true;
+					m_RadialMenuSlots[i].transform.localRotation = Quaternion.Lerp(hiddenSlotRotation,
+					m_RadialMenuSlots[i].visibleLocalRotation, revealAmount);
+				}
+
+				yield return null;
+			}
+
+			revealAmount = 0;
+			while (revealAmount < 1)
+			{
+				revealAmount += Time.unscaledDeltaTime * 0.5f;
+				m_SlotsMask.fillAmount = Mathf.Lerp(m_SlotsMask.fillAmount, 0f, revealAmount);
+				yield return null;
+			}
+
+			m_ShowCoroutine = null;
+		}
+
+		private IEnumerator AnimateHide()
+		{
+			m_SlotsMask.fillAmount = 1f;
+			//yield return new WaitForSeconds(1f);
+
+			float revealAmount = 0f;
+			Quaternion hiddenSlotRotation = RadialMenuSlot.hiddenLocalRotation;
+
+			for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				m_RadialMenuSlots[i].enabled = true;
+
+			
+			for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				m_RadialMenuSlots[i].enabled = false;
+
+			revealAmount = 1;
+			while (revealAmount > 0)
+			{
+				revealAmount -= Time.unscaledDeltaTime * 3;
+
+				//m_SlotsMask.fillAmount = Mathf.Lerp(1f, m_SlotsMask.fillAmount, revealAmount * revealAmount);
+
+				for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+					m_RadialMenuSlots[i].transform.localRotation = Quaternion.Lerp(hiddenSlotRotation, m_RadialMenuSlots[i].visibleLocalRotation, revealAmount);
+
+				yield return null;
+			}
+
+			m_HideCoroutine = null;
+		}
+
+		private IEnumerator AnimateSlotRevealLoop(int slotsToReveal)
+		{
+			m_SlotsMask.fillAmount = 1f;
+			//yield return new WaitForSeconds(1f);
+
+			float revealAmount = 0f;
+			Quaternion hiddenSlotRotation = RadialMenuSlot.hiddenLocalRotation;
+
+			for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				m_RadialMenuSlots[i].enabled = true;
+
+			while (revealAmount < 1)
+			{
+				revealAmount += Time.unscaledDeltaTime * 4;
+
+				for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				{
+					m_RadialMenuSlots[i].enabled = true;
+					m_RadialMenuSlots[i].transform.localRotation = Quaternion.Lerp(hiddenSlotRotation,
+					m_RadialMenuSlots[i].visibleLocalRotation, revealAmount);
+				}
+
+				yield return null;
+			}
+
+			revealAmount = 0;
+			while (revealAmount < 1)
+			{
+				revealAmount += Time.unscaledDeltaTime * 0.5f;
+				m_SlotsMask.fillAmount = Mathf.Lerp(m_SlotsMask.fillAmount, 0f, revealAmount);
+				yield return null;
+			}
+
+			//yield return new WaitForSeconds(0.5f);
+
+			//revealAmount = 1;
+			//while (revealAmount > 0)
+			//{
+			//	revealAmount -= Time.unscaledDeltaTime * 0.5f;
+			//	m_SlotsMask.fillAmount = Mathf.Lerp(m_SlotsMask.fillAmount, 0f, revealAmount);
+			//	yield return null;
+			//}
+
+			for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+				m_RadialMenuSlots[i].enabled = false;
+
+			revealAmount = 1;
+			while (revealAmount > 0)
+			{
+				revealAmount -= Time.unscaledDeltaTime * 3;
+
+				//m_SlotsMask.fillAmount = Mathf.Lerp(1f, m_SlotsMask.fillAmount, revealAmount * revealAmount);
+
+				for (int i = 0; i < m_RadialMenuSlots.Count; ++i)
+					m_RadialMenuSlots[i].transform.localRotation = Quaternion.Lerp(hiddenSlotRotation, m_RadialMenuSlots[i].visibleLocalRotation, revealAmount);
+
+				yield return null;
+			}
+
+			//StartCoroutine(AnimateSlotReveal(8));
+		}
+
+		/*
+		public void SetupButton(Sprite icon, Action<MainMenuButton> buttonCreationCallback)
+		{
+			var button = U.Object.Instantiate(m_ButtonTemplatePrefab.gameObject);
+			button.name = buttonData.name;
+			MainMenuButton mainMenuButton = button.GetComponent<MainMenuButton>();
+			buttonCreationCallback(mainMenuButton);
+
+			if (buttonData.sectionName != null)
+			{
+				mainMenuButton.SetData(buttonData.name, buttonData.description);
+
+				var found = m_FaceButtons.Any(x => x.Key == buttonData.sectionName);
+				if (found)
+				{
+					var kvp = m_FaceButtons.First(x => x.Key == buttonData.sectionName);
+					kvp.Value.Add(button.transform);
+				}
+				else
+				{
+					m_FaceButtons.Add(buttonData.sectionName, new List<Transform>() { button.transform });
+				}
+			}
+			else
+				mainMenuButton.SetData(buttonData.name, string.Empty);
+		}
+		*/
+	}
+}
