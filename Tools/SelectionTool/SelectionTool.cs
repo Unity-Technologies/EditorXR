063948acb47bad43911c4e17b5557d606b9775eb
Dylan Urquidi <dylanu@unity3d.com>
2016-09-06 11:32:43 -0700

    Add initial actions & radial menu support to SelectionTool

--- Tools/SelectionTool/SelectionTool.cs
+++ Tools/SelectionTool/SelectionTool.cs
@@ -3,137 +3,180 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine.InputNew;
+using UnityEngine.VR.Actions;
 using UnityEngine.VR.Utilities;
 
 namespace UnityEngine.VR.Tools
 {
-[UnityEngine.VR.Tools.MainMenuItem("Selection", "Transform", "Select items in the scene")]
-public class SelectionTool : MonoBehaviour, ITool, IRay, IRaycaster, ICustomActionMap, IHighlight, IUsesActions, IMenuOrigins
-{
-	private static HashSet<GameObject> s_SelectedObjects = new HashSet<GameObject>(); // Selection set is static because multiple selection tools can simulataneously add and remove objects from a shared selection
+	using RadialMenu = UnityEngine.VR.Menus.RadialMenu;
 
-	private GameObject m_HoverGameObject;
-	private DateTime m_LastSelectTime;
-
-	// The prefab (if any) that was double clicked, whose individual pieces can be selected
-	private static GameObject s_CurrentPrefabOpened; 
-
-	[SerializeField]
-	private RadialMenu m_RadialMenu;
-
-	public ActionMap actionMap { get { return m_ActionMap; } }
-	[SerializeField]
-	private ActionMap m_ActionMap;
-
-	public ActionMapInput actionMapInput
+	[UnityEngine.VR.Tools.MainMenuItem("Selection", "Transform", "Select items in the scene")]
+	public class SelectionTool : MonoBehaviour, ITool, IRay, IRaycaster, ICustomActionMap, IHighlight, IUsesActions, IMenuOrigins, IInstantiateUI
 	{
-		get { return m_SelectionInput; }
-		set { m_SelectionInput = (SelectionInput)value; }
-	}
-	private SelectionInput m_SelectionInput;
+		private static HashSet<GameObject> s_SelectedObjects = new HashSet<GameObject>(); // Selection set is static because multiple selection tools can simulataneously add and remove objects from a shared selection
 
-	public Func<Transform, GameObject> getFirstGameObject { private get; set; }
+		private GameObject m_HoverGameObject;
+		private DateTime m_LastSelectTime;
 
-	public Transform rayOrigin { private get; set; }
+		// The prefab (if any) that was double clicked, whose individual pieces can be selected
+		private static GameObject s_CurrentPrefabOpened; 
 
-	public Action<GameObject, bool> setHighlight { private get; set; }
+		public ActionMap actionMap { get { return m_ActionMap; } }
+		[SerializeField]
+		private ActionMap m_ActionMap;
 
-	public List<IAction> actions { private get; set; }
+		[SerializeField]
+		private RadialMenu m_RadialMenuPrefab;
 
-	public Transform menuInputOrigin { get; set; }
-	public Transform menuOrigin { get; set; }
+		private RadialMenu m_RadialMenu;
 
-	void Update()
-	{
-		if (rayOrigin == null)
-			return;
-
-		// Change activeGameObject selection to its parent transform when parent button is pressed 
-		if (m_SelectionInput.parent.wasJustPressed)
+		public ActionMapInput actionMapInput
 		{
-			var go = Selection.activeGameObject;
-			if (go != null && go.transform.parent != null)
+			get { return m_SelectionInput; }
+			set { m_SelectionInput = (SelectionInput)value; }
+		}
+		private SelectionInput m_SelectionInput;
+
+		public Func<Transform, GameObject> getFirstGameObject { private get; set; }
+		public Transform rayOrigin { private get; set; }
+
+		public Action<GameObject, bool> setHighlight { private get; set; }
+		
+		public List<IAction> actions { get; set; }
+
+		public Transform menuOrigin { get; set; }
+
+		private Transform m_AlternateMenuOrigin; // TODO delete if not needed
+		public Transform alternateMenuOrigin
+		{
+			get { return m_AlternateMenuOrigin; }
+			set
 			{
-				s_SelectedObjects.Remove(go);
-				s_SelectedObjects.Add(go.transform.parent.gameObject);
-				Selection.objects = s_SelectedObjects.ToArray();
+				m_AlternateMenuOrigin = value;
+
+				if (m_RadialMenu != null)
+					m_RadialMenu.alternateMenuOrigin = value;
 			}
 		}
-		var newHoverGameObject = getFirstGameObject(rayOrigin);
-		var newPrefabRoot = newHoverGameObject;
 
-		if (newHoverGameObject != null)
+		public Transform menuInputOrigin { get; set; }
+
+		public Func<IAction, bool> performAction
 		{
-			// If gameObject is within a prefab and not the current prefab, choose prefab root
-			newPrefabRoot = PrefabUtility.FindPrefabRoot(newHoverGameObject);
-			if (newPrefabRoot != s_CurrentPrefabOpened)
-				newHoverGameObject = newPrefabRoot;
+			get { return m_PerformAction; }
+			set
+			{
+				m_PerformAction = value;
+
+				if (m_RadialMenu != null)
+					m_RadialMenu.performAction = value;
+			}
+		}
+		private Func<IAction, bool> m_PerformAction;
+
+		public Func<GameObject, GameObject> instantiateUI
+		{
+			set
+			{
+				m_RadialMenu = value(m_RadialMenuPrefab.gameObject).GetComponent<RadialMenu>();
+				m_RadialMenu.allActions = actions;
+				m_RadialMenu.instantiateUI = value;
+				m_RadialMenu.performAction = performAction;
+				m_RadialMenu.alternateMenuOrigin = m_AlternateMenuOrigin;
+			}
 		}
 
-		// Handle changing highlight
-		if (newHoverGameObject != m_HoverGameObject)
+		private void Update()
 		{
-			if (m_HoverGameObject != null)
-				setHighlight(m_HoverGameObject, false);
+			if (rayOrigin == null)
+				return;
+
+			// Change activeGameObject selection to its parent transform when parent button is pressed 
+			if (m_SelectionInput.parent.wasJustPressed)
+			{
+				var go = Selection.activeGameObject;
+				if (go != null && go.transform.parent != null)
+				{
+					s_SelectedObjects.Remove(go);
+					s_SelectedObjects.Add(go.transform.parent.gameObject);
+					Selection.objects = s_SelectedObjects.ToArray();
+				}
+			}
+			var newHoverGameObject = getFirstGameObject(rayOrigin);
+			var newPrefabRoot = newHoverGameObject;
 
 			if (newHoverGameObject != null)
-				setHighlight(newHoverGameObject, true);
-		}
-
-		m_HoverGameObject = newHoverGameObject;
-
-		// Handle select button press
-		if (m_SelectionInput.select.wasJustPressed) 
-		{
-			// Detect double click
-			var timeSinceLastSelect = (float)(DateTime.Now - m_LastSelectTime).TotalSeconds;
-			m_LastSelectTime = DateTime.Now;
-			if (U.Input.DoubleClick(timeSinceLastSelect))
 			{
-				s_CurrentPrefabOpened = m_HoverGameObject;
-				s_SelectedObjects.Remove(s_CurrentPrefabOpened);
-			}
-			else
-			{
-				// Reset current prefab if selecting outside of it
+				// If gameObject is within a prefab and not the current prefab, choose prefab root
+				newPrefabRoot = PrefabUtility.FindPrefabRoot(newHoverGameObject);
 				if (newPrefabRoot != s_CurrentPrefabOpened)
-					s_CurrentPrefabOpened = null;
+					newHoverGameObject = newPrefabRoot;
+			}
 
-				// Multi-Select
-				if (m_SelectionInput.multiSelect.isHeld)
+			// Handle changing highlight
+			if (newHoverGameObject != m_HoverGameObject)
+			{
+				if (m_HoverGameObject != null)
+					setHighlight(m_HoverGameObject, false);
+
+				if (newHoverGameObject != null)
+					setHighlight(newHoverGameObject, true);
+			}
+
+			m_HoverGameObject = newHoverGameObject;
+
+			// Handle select button press
+			if (m_SelectionInput.select.wasJustPressed) 
+			{
+				// Detect double click
+				var timeSinceLastSelect = (float)(DateTime.Now - m_LastSelectTime).TotalSeconds;
+				m_LastSelectTime = DateTime.Now;
+				if (U.Input.DoubleClick(timeSinceLastSelect))
 				{
-					
-					if (s_SelectedObjects.Contains(m_HoverGameObject))
-					{
-						// Already selected, so remove from selection
-						s_SelectedObjects.Remove(m_HoverGameObject);
-					}
-					else
-					{
-						// Add to selection
-						s_SelectedObjects.Add(m_HoverGameObject); 
-						Selection.activeGameObject = m_HoverGameObject;
-					}
+					s_CurrentPrefabOpened = m_HoverGameObject;
+					s_SelectedObjects.Remove(s_CurrentPrefabOpened);
 				}
 				else
 				{
-					s_SelectedObjects.Clear();
-					Selection.activeGameObject = m_HoverGameObject;
-					s_SelectedObjects.Add(m_HoverGameObject);
-					m_RadialMenu.Show(actions); // Show the radial menu if there are any objects in the set, hide it otherwise.
-				}
-			}
-			Selection.objects = s_SelectedObjects.ToArray();
-		}
-	}
+					// Reset current prefab if selecting outside of it
+					if (newPrefabRoot != s_CurrentPrefabOpened)
+						s_CurrentPrefabOpened = null;
 
-	void OnDisable()
-	{
-		if (m_HoverGameObject != null)
+					// Multi-Select
+					if (m_SelectionInput.multiSelect.isHeld)
+					{
+					
+						if (s_SelectedObjects.Contains(m_HoverGameObject))
+						{
+							// Already selected, so remove from selection
+							s_SelectedObjects.Remove(m_HoverGameObject);
+						}
+						else
+						{
+							// Add to selection
+							s_SelectedObjects.Add(m_HoverGameObject); 
+							Selection.activeGameObject = m_HoverGameObject;
+						}
+					}
+					else
+					{
+						s_SelectedObjects.Clear();
+						Selection.activeGameObject = m_HoverGameObject;
+						s_SelectedObjects.Add(m_HoverGameObject);
+						m_RadialMenu.Show(); // Show the radial menu if there are any objects in the set, hide it otherwise.
+					}
+				}
+				Selection.objects = s_SelectedObjects.ToArray();
+			}
+		}
+
+		void OnDisable()
 		{
-			setHighlight(m_HoverGameObject, false);
-			m_HoverGameObject = null;
+			if (m_HoverGameObject != null)
+			{
+				setHighlight(m_HoverGameObject, false);
+				m_HoverGameObject = null;
+			}
 		}
 	}
-}
 }
