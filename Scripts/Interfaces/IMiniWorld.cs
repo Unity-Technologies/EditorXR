d60e969821379f3884d26d2ebcc59d867e1dbd42
Matt Schoen <schoen@unity3d.com>
2016-09-08 13:43:58 -0700

    Start on MiniWorldRays

---
+++ Scripts/Interfaces/IMiniWorld.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IMiniWorld
+{
+	bool IsContainedWithin(Vector3 position);
+	Transform referenceTransform { get; }
+}
--- Workspaces/Chessboard/Scripts/MiniWorld.cs
+++ Workspaces/Chessboard/Scripts/MiniWorld.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.VR.Utilities;
 
-public class MiniWorld : MonoBehaviour
+public class MiniWorld : MonoBehaviour, IMiniWorld
 {
 	public LayerMask rendererCullingMask
 	{
@@ -43,6 +43,11 @@ public class MiniWorld : MonoBehaviour
 	}
 
 	public Bounds localBounds { get { return new Bounds(Vector3.zero, m_LocalBoundsSize); } set { m_LocalBoundsSize = value.size; } }
+
+	public bool IsContainedWithin(Vector3 position)
+	{
+		return localBounds.Contains(transform.InverseTransformPoint(position));
+	}
 
 	private void OnEnable()
 	{
