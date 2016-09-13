ad9332370c5a21cce3ef62b3d1650d49f750fe83
Dylan Urquidi <dylanu@unity3d.com>
2016-09-13 11:08:18 -0700

    Add support for Name/SectionName/IndexPosition to IAction

--- Scripts/Interfaces/IAction.cs
+++ Scripts/Interfaces/IAction.cs
@@ -1,15 +1,33 @@
 ﻿namespace UnityEngine.VR.Actions
 {
+	/// <summary>
+	/// Interface that mandates the properties & methods that must be implemented for EditorVR Actions
+	/// </summary>
 	public interface IAction
 	{
 		/// <summary>
-		/// The icon representing this Action that will be displayed in menus
+		/// The name of this action
+		/// </summary>
+		string name { get; set; }
+
+		/// <summary>
+		/// The name of the section within which this action resides
+		/// </summary>
+		string sectionName { get; set; }
+
+		/// <summary>
+		/// The numeric position of this action within its section
+		/// </summary>
+		int indexPosition { get; set; }
+
+		/// <summary>
+		/// The icon representing this Action that can be displayed in menus
 		/// </summary>
 		Sprite icon { get; set; }
 
 		/// <summary>
 		/// Execute this action
 		/// </summary>
-		bool Execute(); 
+		bool Execute();
 	}
 }
