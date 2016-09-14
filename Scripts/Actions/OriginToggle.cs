09ccca13bdc7c766f8557bb65d665a7cccb17a7a
Dylan Urquidi <dylanu@unity3d.com>
2016-09-14 15:21:03 -0700

    Add current implementation of OriginToggle & RotationToggle actions

---
+++ Scripts/Actions/OriginToggle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor;
+
+namespace UnityEngine.VR.Actions
+{
+	[ToggleActionItem("Rotation", "Center",  "ActionIcons/OriginCenterIcon", "Pivot",  "ActionIcons/OriginPivotIcon", "DefaultActions", 5)]
+	[ExecuteInEditMode]
+	public class OriginToggle : MonoBehaviour, IToggleAction, IUsesTransformTool
+	{
+		[SerializeField]
+		private Sprite m_Icon01;
+		
+		[SerializeField]
+		private Sprite m_Icon02;
+
+		public string groupName { get; set; }
+		public string sectionName { get; set; }
+		public int indexPosition { get; set; }
+		public Sprite icon { get; set; }
+		public Sprite icon02 { get; set; }
+
+		public Func<PivotMode> switchOriginMode { get; set; }
+		public Func<PivotRotation> switchRotationMode { get; set; }
+
+		public bool Execute()
+		{
+			Debug.LogError("Toggle Origin Center/Pivot Action called!");
+
+			//SwitchPivotMode in transform tool called here
+
+			return false;
+		}
+	}
+}
---
+++ Scripts/Actions/RotationToggle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEditor;
+
+namespace UnityEngine.VR.Actions
+{
+	[ToggleActionItem("Rotation", "Local",  "ActionIcons/RotationLocalIcon", "Global",  "ActionIcons/RotationGlobalIcon", "DefaultActions", 5)]
+	[ExecuteInEditMode]
+	public class RotationToggle : MonoBehaviour, IToggleAction, IUsesTransformTool
+	{
+		[SerializeField]
+		private Sprite m_Icon01;
+		
+		[SerializeField]
+		private Sprite m_Icon02;
+
+		public string groupName { get; set; }
+		public string sectionName { get; set; }
+		public int indexPosition { get; set; }
+		public Sprite icon { get; set; }
+		public Sprite icon02 { get; set; }
+
+		public Func<PivotMode> switchOriginMode { get; set; }
+		public Func<PivotRotation> switchRotationMode { get; set; }
+
+		public bool Execute()
+		{
+			Debug.LogError("Toggle Local/Global rotation Action called!");
+
+			//SwitchPivotRotation in transform tool called here
+
+			return false;
+		}
+
+	}
+}
