fe1e2739bb05149bc8a66060b77d42e40d1189e6
Dylan Urquidi <dylanu@unity3d.com>
2016-09-14 15:24:25 -0700

    Add IToggleAction interface; used for actions that are toggles (switch between 2 states)

---
+++ Scripts/Interfaces/IToggleAction.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityEngine.VR.Actions
+{
+	/// <summary>
+	/// Interface that mandates the properties & methods that must be implemented for EditorVR Actions
+	/// </summary>
+	public interface IToggleAction : IAction
+	{
+		string groupName {  get; set; }
+	}	
+}
