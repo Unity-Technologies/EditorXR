8f0696be6760d49335a0938d235186c30e11cd12
Dylan Urquidi <dylanu@unity3d.com>
2016-09-14 13:13:11 -0700

    Add MainMenuActionToggle controller; Add this controller to the MenuToggle prefab

---
+++ Tools/MainMenu/Scripts/MainMenuActionToggle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuActionToggle : MainMenuActionButton
+{
+	[SerializeField]
+	private Button m_Button2;
+
+	[SerializeField]
+	new private Sprite m_Icon02; // // Hide the parent sprite, as these are not shown in main menu toggles
+
+	[SerializeField]
+	private Text m_NameText2;
+}
