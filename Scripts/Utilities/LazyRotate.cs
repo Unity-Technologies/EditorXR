096ce707218634a3ea5585b2766e9337d6f4ecf6
Dylan Urquidi <dylanu@unity3d.com>
2016-09-15 12:36:00 -0700

    Add LazyRotate anim utility controller

---
+++ Scripts/Utilities/LazyRotate.cs
@@ -0,0 +1,32 @@
+﻿namespace UnityEngine.VR.Utilities
+{
+	public class LazyRotate : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform m_TargetTransform;
+		
+		[SerializeField]
+		private float m_DampingReduction = 5f;
+
+		private Quaternion lazyRotation;
+
+		private void Start()
+		{
+			if (m_TargetTransform == null)
+				m_TargetTransform = transform.parent;
+
+			if (m_TargetTransform != null)
+				lazyRotation = transform.rotation;
+		}
+
+		private void LateUpdate ()
+		{
+			if (m_TargetTransform == null)
+				return;
+
+			Quaternion targetRotation = m_TargetTransform.rotation;
+			lazyRotation = Quaternion.Lerp(lazyRotation, targetRotation, m_DampingReduction * Time.unscaledDeltaTime);
+			transform.rotation = lazyRotation;
+		}
+	}
+}
