bd2b3727a9f9433dd5e22ea414b57a6e03e9830c
Matt Schoen <schoen@unity3d.com>
2016-09-15 14:07:20 -0700

    Update InspectorListViewController to use templates of varying size

--- Workspaces/Inspector/Scripts/InspectorListViewController.cs
+++ Workspaces/Inspector/Scripts/InspectorListViewController.cs
@@ -1,4 +1,6 @@
-﻿using ListView;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ListView;
 using UnityEngine;
 using UnityEngine.VR.Utilities;
 
@@ -8,23 +10,72 @@ public class InspectorListViewController : NestedListViewController<InspectorDat
 
 	private Material m_CubeMaterial;
 
+	private readonly Dictionary<string, Vector3> m_TemplateSizes = new Dictionary<string, Vector3>();
+
+	public override InspectorData[] data
+	{
+		set
+		{
+			base.data = value;
+		}
+	}
+
 	protected override void Setup()
 	{
 		base.Setup();
 		var item = m_Templates[0].GetComponent<InspectorListItem>();
 		item.GetMaterials(out m_CubeMaterial);
+
+		foreach (var template in m_TemplateDictionary)
+			m_TemplateSizes[template.Key] = GetObjectSize(template.Value.prefab);
 	}
 
 	protected override void ComputeConditions()
 	{
 		base.ComputeConditions();
 
+		m_StartPosition = bounds.extents.z * Vector3.forward;
+
 		var parentMatrix = transform.worldToLocalMatrix;
 		m_CubeMaterial.SetMatrix("_ParentMatrix", parentMatrix);
-		m_CubeMaterial.SetVector("_ClipExtents", bounds.extents);
+		m_CubeMaterial.SetVector("_ClipExtents", bounds.extents * 100);
 	}
 
-	protected override void UpdateItemRecursive(InspectorData data, int offset, int depth)
+	protected override void UpdateItems()
+	{
+		float totalOffset = 0;
+		UpdateRecursively(m_Data, ref totalOffset);
+	}
+
+	private void UpdateRecursively(InspectorData[] data, ref float totalOffset, int depth = 0)
+	{
+		foreach (var item in data)
+		{
+			m_ItemSize = m_TemplateSizes[item.template];
+			if (totalOffset + scrollOffset + m_ItemSize.z < 0)
+				CleanUpBeginning(item);
+			else if (totalOffset + scrollOffset > bounds.size.z)
+				CleanUpEnd(item);
+			else
+				UpdateItemRecursive(item, totalOffset, depth);
+			totalOffset += m_ItemSize.z;
+			if (item.children != null)
+			{
+				if (item.expanded)
+					UpdateRecursively(item.children, ref totalOffset, depth + 1);
+				else
+					RecycleChildren(item);
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawWireCube(Vector3.zero, bounds.size);
+	}
+
+	private void UpdateItemRecursive(InspectorData data, float offset, int depth)
 	{
 		if (data.item == null)
 			data.item = GetItem(data);
@@ -32,6 +83,12 @@ public class InspectorListViewController : NestedListViewController<InspectorDat
 		item.UpdateTransforms(bounds.size.x - kClipMargin, depth);
 
 		UpdateItem(item.transform, offset);
+	}
+
+	private void UpdateItem(Transform t, float offset)
+	{
+		t.localPosition = m_StartPosition + (offset + m_ScrollOffset) * Vector3.back;
+		t.localRotation = Quaternion.identity;
 	}
 
 	protected override ListViewItem<InspectorData> GetItem(InspectorData listData)
--- Workspaces/Inspector/Scripts/ListItems/InspectorHeaderItem.cs
+++ Workspaces/Inspector/Scripts/ListItems/InspectorHeaderItem.cs
@@ -11,4 +11,11 @@ public class InspectorHeaderItem : InspectorListItem
 
 	[SerializeField]
 	private Toggle m_StaticToggle;
+
+	public override void Setup(InspectorData data)
+	{
+		base.Setup(data);
+
+		m_NameField.text = data.serializedObject.targetObject.name;
+	}
 }
