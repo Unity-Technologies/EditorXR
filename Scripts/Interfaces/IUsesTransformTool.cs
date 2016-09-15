f8e93de14cfa7f8ab7f0b5bba9a3af8273d57b99
Dylan Urquidi <dylanu@unity3d.com>
2016-09-14 18:12:23 -0700

    Add IUsesTransformTool interface; Used in actions requiring transform tool functionality access

---
+++ Scripts/Interfaces/IUsesTransformTool.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEditor;
+
+public interface IUsesTransformTool
+{
+	Func <PivotMode> switchOriginMode { get; set; }
+
+	Func <PivotRotation> switchRotationMode { get; set; }
+}
