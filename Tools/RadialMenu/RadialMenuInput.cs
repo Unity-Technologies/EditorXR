0c17d5975c6f855dc0d8941eedd018e4e3d92b97
Dylan Urquidi <dylanu@unity3d.com>
2016-09-21 15:13:39 -0700

    Rebuild RadialMenuInput action map

--- Tools/RadialMenu/RadialMenuInput.cs
+++ Tools/RadialMenu/RadialMenuInput.cs
@@ -8,8 +8,8 @@ namespace UnityEngine.InputNew
 		public RadialMenuInput (ActionMap actionMap) : base (actionMap) { }
 		
 		public ButtonInputControl @selectItem { get { return (ButtonInputControl)this[0]; } }
-		public Vector2InputControl @navigateMenu { get { return (Vector2InputControl)this[1]; } }
-		public AxisInputControl @navigateMenuX { get { return (AxisInputControl)this[2]; } }
-		public AxisInputControl @navigateMenuY { get { return (AxisInputControl)this[3]; } }
+		public AxisInputControl @navigateX { get { return (AxisInputControl)this[1]; } }
+		public AxisInputControl @navigateY { get { return (AxisInputControl)this[2]; } }
+		public Vector2InputControl @navigate { get { return (Vector2InputControl)this[3]; } }
 	}
 }
