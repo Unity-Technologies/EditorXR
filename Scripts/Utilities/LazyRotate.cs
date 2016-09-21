e9372ea294dbdec67cca60364d7e1486c662707b
Dylan Urquidi <dylanu@unity3d.com>
2016-09-20 17:48:08 -0700

    Add automatic parent cacheing to LazyRotate; Cleanup conventions

--- Scripts/Utilities/LazyRotate.cs
+++ Scripts/Utilities/LazyRotate.cs
@@ -3,30 +3,31 @@
 	public class LazyRotate : MonoBehaviour
 	{
 		[SerializeField]
-		private Transform m_TargetTransform;
-		
+		private Transform m_Target;	
 		[SerializeField]
-		private float m_DampingReduction = 10f;
-
-		private Quaternion lazyRotation;
+		private float m_Tighten = 20f;
+		private Quaternion m_LazyRotation;
 
 		private void Start()
 		{
-			if (m_TargetTransform == null)
-				m_TargetTransform = transform.parent;
-
-			if (m_TargetTransform != null)
-				lazyRotation = transform.rotation;
+			if (m_Target == null && transform.parent != null)
+			{
+				m_Target = transform.parent;
+				m_LazyRotation = transform.rotation;
+			}
 		}
 
 		private void LateUpdate ()
 		{
-			if (m_TargetTransform == null)
+			if (m_Target != transform.parent)
+				m_Target = transform.parent; // cache new parent as this transform is assigned to different objects
+
+			if (m_Target == null)
 				return;
 
-			Quaternion targetRotation = m_TargetTransform.rotation;
-			lazyRotation = Quaternion.Lerp(lazyRotation, targetRotation, m_DampingReduction * Time.unscaledDeltaTime);
-			transform.rotation = lazyRotation;
+			Quaternion targetRotation = m_Target.rotation;
+			m_LazyRotation = Quaternion.Lerp(m_LazyRotation, targetRotation, m_Tighten * Time.unscaledDeltaTime);
+			transform.rotation = m_LazyRotation;
 		}
 	}
 }
