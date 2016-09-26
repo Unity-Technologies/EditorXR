236c88719c531463a96ff74fd441bc56f68e5a16
Gergo Foldhazi <devnull@localhost>
2016-09-26 11:46:06 +0200

    fix profiler window position to spawn off-screen use different method to check visibility of profiler workspace

--- Workspaces/Profiler/ProfilerWorkspace.cs
+++ Workspaces/Profiler/ProfilerWorkspace.cs
@@ -9,6 +9,11 @@ public class ProfilerWorkspace : Workspace
 
 	private Transform m_ProfilerWindow;
 
+	private bool m_InView;
+
+	private const float kQuarter = 0.25f;
+	private const float kThreeQuarters = 0.75f;
+
 	public override void Setup()
 	{
 		base.Setup();
@@ -24,8 +29,9 @@ public class ProfilerWorkspace : Workspace
 
 		m_ProfilerWindow.localScale = size;
 
-		UnityEditorInternal.ProfilerDriver.profileEditor = true;
-	}
+		UnityEditorInternal.ProfilerDriver.profileEditor = false;	
+		m_InView = false;
+    }
 
 	void Update()
 	{
@@ -34,18 +40,27 @@ public class ProfilerWorkspace : Workspace
 
 	void UpdateProfilerInView()
 	{
-		RaycastHit hit;
-		Ray ray = VRView.viewerCamera.ViewportPointToRay(new Vector2(0.5f,0.5f));
-		if(Physics.Raycast(ray,out hit))
+		RectTransform rect = GetComponentInChildren<EditorWindowCapture>().GetComponent<RectTransform>();
+		Vector3[] corners = new Vector3[4];
+		rect.GetWorldCorners(corners);
+
+		float minX = VRView.viewerCamera.pixelRect.width * kQuarter;
+		float minY = VRView.viewerCamera.pixelRect.height * kQuarter;
+		float maxX = VRView.viewerCamera.pixelRect.width * kThreeQuarters;
+		float maxY = VRView.viewerCamera.pixelRect.height * kThreeQuarters;
+
+		m_InView = false;
+		foreach(Vector3 vec in corners)
 		{
-			if(hit.collider.GetComponentInParent<ProfilerWorkspace>() != null)
+			Vector3 screenPoint = VRView.viewerCamera.WorldToScreenPoint(vec);
+			if(screenPoint.x > minX && screenPoint.x < maxX && screenPoint.y > minY && screenPoint.y < maxY)
 			{
-				UnityEditorInternal.ProfilerDriver.profileEditor = true;
-				return;
+				m_InView = true;
+				break;
 			}
 		}
 
-		UnityEditorInternal.ProfilerDriver.profileEditor = false;
+		UnityEditorInternal.ProfilerDriver.profileEditor = m_InView;
 	}
 
 	protected override void OnBoundsChanged()
