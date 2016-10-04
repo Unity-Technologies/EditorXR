f1868867e7c483bdb353392ee01f609fd5fa50b2
Dylan Urquidi <dylanu@unity3d.com>
2016-10-03 17:52:19 -0700

    Add additional functionality and optimizations to SmoothMotion

--- Scripts/Utilities/SmoothMotion.cs
+++ Scripts/Utilities/SmoothMotion.cs
@@ -2,19 +2,23 @@
 {
 	public class SmoothMotion : MonoBehaviour
 	{
+		const float kDefaultTighteningAmount = 20f;
+
+		public bool smoothRotation { private get { return m_SmoothRotation; } set { m_SmoothRotation = value; } }
 		[Header("Rotation")]
 		[SerializeField]
 		bool m_SmoothRotation;
 
 		[SerializeField]
-		float m_TightenRotation = 20f;
+		float m_TightenRotation = kDefaultTighteningAmount;
 
+		public bool smoothPosition { private get { return m_SmoothPosition; } set { m_SmoothPosition = value; } }
 		[Header("Position")]
 		[SerializeField]
 		bool m_SmoothPosition;
 
 		[SerializeField]
-		float m_TightenPosition = 20f;
+		float m_TightenPosition = kDefaultTighteningAmount;
 
 		[Header("Optional")]
 		[SerializeField]
@@ -37,7 +41,7 @@
 			if (m_Target != transform.parent)
 				m_Target = transform.parent; // cache new parent as this transform is assigned to different objects
 
-			if (m_Target == null)
+			if (m_Target == null || (!m_SmoothPosition && !m_SmoothRotation))
 				return;
 
 			if (m_SmoothRotation)
@@ -53,6 +57,26 @@
 				m_LazyPosition = Vector3.Lerp(m_LazyPosition, targetPosition, m_TightenPosition * Time.unscaledDeltaTime);
 				transform.position = m_LazyPosition;
 			}
+		}
+
+		/// <summary>
+		/// Setup rotation smoothing
+		/// </summary>
+		/// <param name="tightenAmount">A value of zero allows for full rotation smoothing, a value of 20 tightens greatly the rotation smoothing</param>
+		public void SetRotationSmoothing(float tightenAmount = kDefaultTighteningAmount)
+		{
+			m_SmoothRotation = true;
+			m_TightenRotation = tightenAmount;
+		}
+
+		/// <summary>
+		/// Setup position smoothing
+		/// </summary>
+		/// <param name="tightenAmount">A value of zero allows for full position smoothing, a value of 20 tightens greatly the position smoothing</param>
+		public void SetPositionSmoothing(float tightenAmount = kDefaultTighteningAmount)
+		{
+			m_SmoothPosition = true;
+			m_TightenPosition = tightenAmount;
 		}
 	}
 }
