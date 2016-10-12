16ee4663385c867ad015efbe7b2118d684bd61f5
Dylan Urquidi <dylanu@unity3d.com>
2016-10-12 10:36:31 -0700

    Cleanup formatting in AssetGridItem

--- Workspaces/Project/Scripts/AssetGridItem.cs
+++ Workspaces/Project/Scripts/AssetGridItem.cs
@@ -285,9 +285,10 @@ public class AssetGridItem : ListViewItem<AssetData>, IPlaceObjects, IPositionPr
 	IEnumerator AnimatesToPreviewScale()
 	{
 		var currentLocalScale = m_GrabbedObject.localScale;
-		var targetLocalScale = Vector3.one * 0.125f;
+		const float smallerLocalScaleMultiplier = 0.125f;
+		var targetLocalScale = Vector3.one * smallerLocalScaleMultiplier;
 		var currentTime = 0f;
-		float currentVelocity = 0f;
+		var currentVelocity = 0f;
 		const float kDuration = 1f;
 		while (currentTime < kDuration)
 		{
