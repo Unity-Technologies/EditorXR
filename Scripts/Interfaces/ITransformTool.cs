90f181145582e15235c5e73cb40b0e55f5e90b34
Matt Schoen <schoen@unity3d.com>
2016-10-18 12:18:41 -0700

    Document ITransformTool

--- Scripts/Interfaces/ITransformTool.cs
+++ Scripts/Interfaces/ITransformTool.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine.InputNew;
-
-namespace UnityEngine.VR.Tools
+﻿namespace UnityEngine.VR.Tools
 {
 	/// <summary>
-	/// Designates a tool as a Transform tool and allows for its control
+	/// Designates a tool as a Transform tool and allows for control and state queries
 	/// </summary>
 	public interface ITransformTool
 	{
+		/// <summary>
+		/// Get or set whether the tool can direclty manipulate objects using the pointers
+		/// </summary>
 		bool directManipulationEnabled { get; set; }
+
+		/// <summary>
+		/// Drop a currently held object
+		/// </summary>
+		/// <param name="obj">The object we wish to drop</param>
 		void DropHeldObject(Transform obj);
-		Transform GetHeldObject(Transform obj);
+		
+		/// <summary>
+		/// Get the object held by a given rayOrign
+		/// </summary>
+		/// <param name="rayOrigin">The rayOrigin to query</param>
+		/// <returns></returns>
+		Transform GetHeldObject(Transform rayOrigin);
 	}
 }
