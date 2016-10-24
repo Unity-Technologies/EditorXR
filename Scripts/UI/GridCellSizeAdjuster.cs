702d008840b4c3e74c00d8ab934baee5db073872
Dylan Urquidi <dylanu@unity3d.com>
2016-10-24 13:09:57 -0700

    Add new GridCellSizeAdjuster controller, used to drive GridLayoutGroup cell size (x-only for now), via rect transform size changes on the GridLayoutGroup transform

---
+++ Scripts/UI/GridCellSizeAdjuster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.UI;
+
+namespace UnityEngine.VR.Helpers
+{
+	public class GridCellSizeAdjuster : MonoBehaviour
+	{
+		static readonly Vector2 kTransformPivot = new Vector2(0.5f, 0.5f);
+
+		RectTransform m_LayoutGroupTransform;
+
+		[SerializeField]
+		GridLayoutGroup m_LayoutGroup;
+
+		[SerializeField]
+		float m_XScalePadding = 0.01f;
+
+		void Awake()
+		{
+			m_LayoutGroupTransform = m_LayoutGroup.transform as RectTransform;
+			DriveCellSizeWithLayoutGroupTransform();
+		}
+
+		void Update()
+		{
+			if (m_LayoutGroupTransform.hasChanged)
+				DriveCellSizeWithLayoutGroupTransform();
+		}
+
+		void DriveCellSizeWithLayoutGroupTransform()
+		{
+			if (!m_LayoutGroupTransform)
+				return;
+
+			m_LayoutGroup.cellSize = new Vector2(m_LayoutGroupTransform.rect.left - m_LayoutGroupTransform.rect.right + m_XScalePadding, m_LayoutGroup.cellSize.y);
+		}
+	}
+}
