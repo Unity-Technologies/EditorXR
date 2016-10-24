426ef7c4a290fb70d891d032b0a574cc0b31c463
Gergo Foldhazi <devnull@localhost>
2016-10-24 15:23:08 +0200

    add locking to selection tool

--- Scripts/Modules/Lock/LockModule.cs
+++ Scripts/Modules/Lock/LockModule.cs
@@ -6,9 +6,14 @@ using UnityEditor.VR;
 public class LockModule : MonoBehaviour
 {
 	private readonly List<GameObject> m_LockedGameObjects = new List<GameObject>();
+	private GameObject m_CurrentHoverObject;
+	private const float kMaxHoverTime = 2.0f;
+	private float m_HoverTime;
 
 	void Awake()
 	{
+		m_CurrentHoverObject = null;
+		m_HoverTime = 0.0f;
 		GameObject[] temp = GameObject.FindGameObjectsWithTag("testcube");
 		foreach(GameObject go in temp)
 		{
@@ -40,6 +45,24 @@ public class LockModule : MonoBehaviour
 			{
 				m_LockedGameObjects.Remove(go);
 				Debug.Log(go.name + " unlocked");
+			}
+		}
+	}
+
+	public void CheckhoverTime(GameObject go)
+	{
+		if (go != m_CurrentHoverObject)
+		{
+			m_CurrentHoverObject = go;
+			m_HoverTime = 0.0f;
+			return;
+		}
+		else
+		{
+			m_HoverTime += Time.unscaledDeltaTime;
+			if (m_HoverTime >= kMaxHoverTime)
+			{
+				//pop up radial menu
 			}
 		}
 	}
--- Tools/SelectionTool/SelectionTool.cs
+++ Tools/SelectionTool/SelectionTool.cs
@@ -7,7 +7,7 @@ using UnityEngine.VR.Utilities;
 
 namespace UnityEngine.VR.Tools
 {
-	public class SelectionTool : MonoBehaviour, ITool, IRay, IRaycaster, ICustomActionMap, IHighlight, IMenuOrigins
+	public class SelectionTool : MonoBehaviour, ITool, IRay, IRaycaster, ICustomActionMap, IHighlight, IMenuOrigins, ILocking
 	{
 		private static HashSet<GameObject> s_SelectedObjects = new HashSet<GameObject>(); // Selection set is static because multiple selection tools can simulataneously add and remove objects from a shared selection
 
@@ -26,6 +26,8 @@ namespace UnityEngine.VR.Tools
 		public Action<GameObject, bool> setHighlight { private get; set; }
 		public Transform menuOrigin { get; set; }
 		public Node? node { private get; set; }
+		public Action<GameObject, bool> setLocked { get; set; }
+		public Func<GameObject, bool> getLocked { get; set; }
 
 		public ActionMapInput actionMapInput
 		{
@@ -73,6 +75,13 @@ namespace UnityEngine.VR.Tools
 				newPrefabRoot = PrefabUtility.FindPrefabRoot(newHoverGameObject);
 				if (newPrefabRoot != s_CurrentPrefabOpened)
 					newHoverGameObject = newPrefabRoot;
+			}
+
+			//LockTEST
+			if (getLocked(newHoverGameObject))
+			{
+				//check hover time
+				return;
 			}
 
 			// Handle changing highlight
