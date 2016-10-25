a8dfe7546fdf4e2fbbb1aaaa28926791b2ed71c1
Dylan Urquidi <dylanu@unity3d.com>
2016-10-24 17:58:01 -0700

    Perform structure changes, and add new animation support to DropDown controller

--- Scripts/Handles/UI/DropDown.cs
+++ Scripts/Handles/UI/DropDown.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.UI;
+using UnityEngine.VR.Extensions;
 using UnityEngine.VR.Handles;
 using UnityEngine.VR.Utilities;
 
@@ -9,6 +11,11 @@ namespace UnityEngine.VR.UI
 {
 	public class DropDown : MonoBehaviour
 	{
+		Coroutine m_ShowDropdownCoroutine;
+		Coroutine m_HideDropdownCoroutine;
+		float m_HiddenDropdownItemYSpacing;
+		float m_VisibleDropdownItemYSpacing;
+
 		public string[] options
 		{
 			get { return m_Options; }
@@ -32,13 +39,16 @@ namespace UnityEngine.VR.UI
 		RectTransform m_OptionsPanel;
 
 		[SerializeField]
-		LayoutGroup m_OptionsList;
+		GridLayoutGroup m_OptionsList;
 
 		[SerializeField]
 		GameObject m_TemplatePrefab;
 
 		[SerializeField]
 		GameObject m_MultiSelectTemplatePrefab;
+
+		[SerializeField]
+		CanvasGroup m_CanvasGroup;
 
 		public int value
 		{
@@ -72,6 +82,9 @@ namespace UnityEngine.VR.UI
 		void Awake()
 		{
 			SetupOptions();
+
+			m_HiddenDropdownItemYSpacing = -m_OptionsList.cellSize.y;
+			m_VisibleDropdownItemYSpacing = m_OptionsList.spacing.y;
 		}
 
 		void OnEnable()
@@ -104,6 +117,9 @@ namespace UnityEngine.VR.UI
 				{
 					var optionObject = (GameObject)Instantiate(template, listTransform.position, listTransform.rotation, listTransform);
 
+					// Zero out Z local position
+					optionObject.transform.localPosition = new Vector3(optionObject.transform.localPosition.x, optionObject.transform.localPosition.y, 0f);
+
 					var optionText = optionObject.GetComponentInChildren<Text>();
 					if (optionText)
 						optionText.text = m_Options[i];
@@ -131,12 +147,16 @@ namespace UnityEngine.VR.UI
 
 		public void OpenPanel()
 		{
-			m_OptionsPanel.gameObject.SetActive(true);
+			this.StopCoroutine(ref m_HideDropdownCoroutine);
+			this.StopCoroutine(ref m_ShowDropdownCoroutine);
+			m_ShowDropdownCoroutine = StartCoroutine(ShowDropDownContents());
 		}
 
 		public void ClosePanel()
 		{
-			m_OptionsPanel.gameObject.SetActive(false);
+			this.StopCoroutine(ref m_ShowDropdownCoroutine);
+			this.StopCoroutine(ref m_HideDropdownCoroutine);
+			m_HideDropdownCoroutine = StartCoroutine(HideDropDownContents());
 		}
 
 		public void LabelOverride(string text)
@@ -195,6 +215,52 @@ namespace UnityEngine.VR.UI
 				if(m_Value >= 0 && m_Value < m_Options.Length)
 					m_Label.text = m_Options[m_Value];
 			}
+		}
+
+		IEnumerator ShowDropDownContents()
+		{
+			m_OptionsPanel.gameObject.SetActive(true);
+
+			const float kTargetDuration = 0.5f;
+			var currentAlpha = m_CanvasGroup.alpha;
+			var kTargetAlpha = 1f;
+			var transitionAmount = 0f;
+			var velocity = 0f;
+			var currentDuration = 0f;
+			while (currentDuration < kTargetDuration)
+			{
+				currentDuration += Time.unscaledDeltaTime;
+				transitionAmount = U.Math.SmoothDamp(transitionAmount, 1f, ref velocity, kTargetDuration, Mathf.Infinity, Time.unscaledDeltaTime);
+				m_OptionsList.spacing = new Vector2(0f, Mathf.Lerp(m_HiddenDropdownItemYSpacing, m_VisibleDropdownItemYSpacing, transitionAmount));
+				m_CanvasGroup.alpha = Mathf.Lerp(currentAlpha, kTargetAlpha, transitionAmount);
+				yield return null;
+			}
+
+			m_OptionsList.spacing = new Vector2(0f, m_VisibleDropdownItemYSpacing);
+			m_CanvasGroup.alpha = 1f;
+			m_ShowDropdownCoroutine = null;
+		}
+
+		IEnumerator HideDropDownContents()
+		{
+			const float kTargetDuration = 0.25f;
+			var currentAlpha = m_CanvasGroup.alpha;
+			var kTargetAlpha = 0f;
+			var transitionAmount = 0f;
+			var currentSpacing = m_OptionsList.spacing.y;
+			var velocity = 0f;
+			var currentDuration = 0f;
+			while (currentDuration < kTargetDuration)
+			{
+				currentDuration += Time.unscaledDeltaTime;
+				transitionAmount = U.Math.SmoothDamp(transitionAmount, 1f, ref velocity, kTargetDuration, Mathf.Infinity, Time.unscaledDeltaTime);
+				m_OptionsList.spacing = new Vector2(0f, Mathf.Lerp(currentSpacing, m_HiddenDropdownItemYSpacing, transitionAmount));
+				m_CanvasGroup.alpha = Mathf.Lerp(currentAlpha, kTargetAlpha, transitionAmount);
+				yield return null;
+			}
+
+			m_OptionsPanel.gameObject.SetActive(false);
+			m_HideDropdownCoroutine = null;
 		}
 	}
 }
