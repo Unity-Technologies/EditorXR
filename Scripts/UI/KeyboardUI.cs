d6231b2c918da1f5a851212770c965465751a99e
Bradley Weiers <bradley@unity3d.com>
2016-10-26 15:57:09 -0700

    Update to latest Unity build

--- Scripts/UI/KeyboardUI.cs
+++ Scripts/UI/KeyboardUI.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.VR.Handles;
 using UnityEngine.VR.Utilities;
 
