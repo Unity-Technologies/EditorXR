23a6ff32d1978d601dc03cf7ce57fdfba99f8d37
Amir Ebrahimi <amir@unity3d.com>
2016-10-30 21:57:47 -0700

    Add speaker notes assets

---
+++ Scripts/Helpers/ProxyExtra.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.VR.Tools;
+
+/// <summary>
+/// Spawn additional objects around a proxy node
+/// </summary>
+public class ProxyExtra : MonoBehaviour
+{
+	/// <summary>
+	/// The proxy node to spawn this extra at
+	/// </summary>
+	public Node node;
+}
