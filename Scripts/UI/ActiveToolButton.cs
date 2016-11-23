e87cbaaadf37877d9e4439421109f2b1790a43ec
Dylan Urquidi <dylanu@unity3d.com>
2016-11-22 17:50:11 -0800

    Add new ActiveToolButton controller; used in EVR to manage the new tool button offset from the mainMenuActivator

---
+++ Scripts/UI/ActiveToolButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine.VR.UI;
+
+namespace UnityEngine.VR.Menus
+{
+	public class ActiveToolButton : MonoBehaviour
+	{
+		[SerializeField]
+		private VRButton m_VRButton;
+
+		public event Action<Transform> selected = delegate { };
+	}
+}
