5a8955b8254a983348e805b3f25381ade66d8210
Dylan Urquidi <dylanu@unity3d.com>
2016-11-23 00:24:07 -0800

    Add ISelectTool interface

---
+++ Scripts/Interfaces/ISelectTool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnityEngine.VR.Tools
+{
+	public interface ISelectTool
+	{
+		/// <summary>
+		/// Delegate used to select tools from the menu
+		/// Transform = ray origin
+		/// Type = type of tool
+		/// Returns whether the tool was successfully selected
+		/// </summary>
+		Func<Transform, Type, bool> selectTool { set; }
+	}
+}
