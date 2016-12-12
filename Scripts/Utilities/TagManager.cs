6f88c1068aa7924a8492396cf6646c7a54804f7b
Matt Schoen <mtschoen@gmail.com>
2016-12-12 15:08:08 -0800

    Move TagManager to UnityEngine.VR.Utilities namespace

--- Scripts/Utilities/TagManager.cs
+++ Scripts/Utilities/TagManager.cs
@@ -1,102 +1,104 @@
 ﻿using UnityEditor;
-using UnityEngine;
 
-[InitializeOnLoad]
-public static class TagManager
+namespace UnityEngine.VR.Utilities
 {
-	const int kMaxLayer = 31;
-	const int kMinLayer = 8;
-
-	static TagManager()
+	[InitializeOnLoad]
+	public static class TagManager
 	{
-		// Add EVR tags and layers if they don't exist
-		var evrTags = new[] { "VRPlayer", "ShowInMiniWorld" };
-		foreach (var tag in evrTags)
+		const int kMaxLayer = 31;
+		const int kMinLayer = 8;
+
+		static TagManager()
 		{
-			AddTag(tag);
+			// Add EVR tags and layers if they don't exist
+			var evrTags = new[] { "VRPlayer", "ShowInMiniWorld" };
+			foreach (var tag in evrTags)
+			{
+				AddTag(tag);
+			}
+
+			var evrLayers = new[] { "HMDOnly" };
+			foreach (var layer in evrLayers)
+			{
+				AddLayer(layer);
+			}
 		}
 
-		var evrLayers = new[] { "HMDOnly" };
-		foreach (var layer in evrLayers)
+		/// <summary>
+		/// Add a tag to the tag manager if it doesn't already exist
+		/// </summary>
+		/// <param name="tag">Tag to add</param>
+		public static void AddTag(string tag)
 		{
-			AddLayer(layer);
+			var asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+			if ((asset != null) && (asset.Length > 0))
+			{
+				var so = new SerializedObject(asset[0]);
+				var tags = so.FindProperty("tags");
+
+				var found = false;
+				for (var i = 0; i < tags.arraySize; i++)
+				{
+					if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+					{
+						found = true;
+						break;
+					}
+				}
+
+				if (!found)
+				{
+					var arraySize = tags.arraySize;
+					tags.InsertArrayElementAtIndex(arraySize);
+					tags.GetArrayElementAtIndex(arraySize - 1).stringValue = tag;
+				}
+				so.ApplyModifiedProperties();
+				so.Update();
+			}
 		}
-	}
 
-	/// <summary>
-	/// Add a tag to the tag manager if it doesn't already exist
-	/// </summary>
-	/// <param name="tag">Tag to add</param>
-	public static void AddTag(string tag)
-	{
-		var asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
-		if ((asset != null) && (asset.Length > 0))
+		/// <summary>
+		/// Add a layer to the tag manager if it doesn't already exist
+		/// Start at layer 31 (max) and work down
+		/// </summary>
+		/// <param name="layerName"></param>
+		public static void AddLayer(string layerName)
 		{
-			var so = new SerializedObject(asset[0]);
-			var tags = so.FindProperty("tags");
-
-			var found = false;
-			for (var i = 0; i < tags.arraySize; i++)
+			var asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+			if ((asset != null) && (asset.Length > 0))
 			{
-				if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+				var so = new SerializedObject(asset[0]);
+				var layers = so.FindProperty("layers");
+				var found = false;
+				for (var i = 0; i < layers.arraySize; i++)
 				{
-					found = true;
-					break;
-				}
-			}
-
-			if (!found)
-			{
-				var arraySize = tags.arraySize;
-				tags.InsertArrayElementAtIndex(arraySize);
-				tags.GetArrayElementAtIndex(arraySize - 1).stringValue = tag;
-			}
-			so.ApplyModifiedProperties();
-			so.Update();
-		}
-	}
-
-	/// <summary>
-	/// Add a layer to the tag manager if it doesn't already exist
-	/// Start at layer 31 (max) and work down
-	/// </summary>
-	/// <param name="layerName"></param>
-	public static void AddLayer(string layerName)
-	{
-		var asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
-		if ((asset != null) && (asset.Length > 0))
-		{
-			var so = new SerializedObject(asset[0]);
-			var layers = so.FindProperty("layers");
-			var found = false;
-			for (var i = 0; i < layers.arraySize; i++)
-			{
-				if (layers.GetArrayElementAtIndex(i).stringValue == layerName)
-				{
-					found = true;
-					break;
-				}
-			}
-
-			if (!found)
-			{
-				var added = false;
-				for (var i = kMaxLayer; i >= kMinLayer; i--)
-				{
-					var layer = layers.GetArrayElementAtIndex(i);
-					if (!string.IsNullOrEmpty(layer.stringValue))
-						continue;
-
-					layer.stringValue = layerName;
-					added = true;
-					break;
+					if (layers.GetArrayElementAtIndex(i).stringValue == layerName)
+					{
+						found = true;
+						break;
+					}
 				}
 
-				if (!added)
-					Debug.LogWarning("Could not add layer " + layerName + " because there are no free layers");
+				if (!found)
+				{
+					var added = false;
+					for (var i = kMaxLayer; i >= kMinLayer; i--)
+					{
+						var layer = layers.GetArrayElementAtIndex(i);
+						if (!string.IsNullOrEmpty(layer.stringValue))
+							continue;
+
+						layer.stringValue = layerName;
+						added = true;
+						break;
+					}
+
+					if (!added)
+						Debug.LogWarning("Could not add layer " + layerName + " because there are no free layers");
+				}
+				so.ApplyModifiedProperties();
+				so.Update();
 			}
-			so.ApplyModifiedProperties();
-			so.Update();
 		}
 	}
 }
