0a29ba4fdae40085144ee83a204a934825a430cf
Dylan Urquidi <dylanu@unity3d.com>
2016-12-14 00:10:48 -0800

    Add public summaries to additional EditorVR Ray interfaces

--- Scripts/Interfaces/UI/IRayEnterHandler.cs
+++ Scripts/Interfaces/UI/IRayEnterHandler.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine.EventSystems;
 
 namespace UnityEngine.VR.Modules
-{	
+{
+	/// <summary>
+	/// Decorates objects with functionality to detect RayEnter events
+	/// </summary>
 	public interface IRayEnterHandler : IEventSystemHandler
 	{
 		void OnRayEnter(RayEventData eventData);
