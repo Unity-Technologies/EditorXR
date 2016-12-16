89b3d1e5bef91cf70225e2d2b49564b65cbf66a3
Matt Schoen <mtschoen@gmail.com>
2016-12-16 11:38:36 -0800

    Revert null check

--- Scripts/Data/InspectorData.cs
+++ Scripts/Data/InspectorData.cs
@@ -9,7 +9,7 @@ public class InspectorData : ListViewItemNestedData<InspectorData>
 
 	public virtual int instanceID
 	{
-		get { return serializedObject == null ? serializedObject.targetObject.GetInstanceID() : 0; }
+		get { return serializedObject.targetObject.GetInstanceID(); }
 	}
 
 	public InspectorData(string template, SerializedObject serializedObject, List<InspectorData> children)
