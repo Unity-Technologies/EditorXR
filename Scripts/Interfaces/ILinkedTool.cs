f77b23c72891e5bc5ca26e61c8cc85d3ca08cb9c
Matt Schoen <schoen@unity3d.com>
2017-01-20 10:02:58 -0800

    Add documentation

--- Scripts/Interfaces/ILinkedTool.cs
+++ Scripts/Interfaces/ILinkedTool.cs
@@ -1,7 +1,17 @@
 ﻿using System.Collections.Generic;
 
+/// <summary>
+/// Provides access to other tools of the same type
+/// </summary>
 public interface ILinkedTool
 {
+	/// <summary>
+	/// List of other tools of the same type (not including this one)
+	/// </summary>
 	List<ILinkedTool> otherTools { get; }
+
+	/// <summary>
+	/// Whether this is the primary tool (the first to be created, can be either hand)
+	/// </summary>
 	bool primary { get; set; }
 }
--- Scripts/Interfaces/IUsesHandedRayOrigin.cs
+++ Scripts/Interfaces/IUsesHandedRayOrigin.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine.Experimental.EditorVR.Tools;
 
-namespace UnityEngine.Experimental.EditorVR {
+namespace UnityEngine.Experimental.EditorVR
+{
+	/// <summary>
+	/// Adds Node information to IUsesRayOrigin to determine which hand the tool is attached to
+	/// </summary>
 	public interface IUsesHandedRayOrigin : IUsesRayOrigin
 	{
+		/// <summary>
+		/// The node associated with rayOrigin
+		/// </summary>
 		Node node { set; }
 	}
 }
