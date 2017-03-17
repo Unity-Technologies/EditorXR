42617ce1d5f90bcb748744baf75222cb32c7c7a6
Matt Schoen <schoen@unity3d.com>
2017-03-16 23:00:53 -0700

    Fix comments in IGetFieldGrabOrigin

--- Scripts/Interfaces/IGetFieldGrabOrigin.cs
+++ Scripts/Interfaces/IGetFieldGrabOrigin.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 namespace UnityEditor.Experimental.EditorVR
 {
 	/// <summary>
-	/// Implementors receive a preview origin transform
+	/// Implementors receive a field grab origin transform
 	/// </summary>
 	public interface IGetFieldGrabOrigin
 	{
 		/// <summary>
-		/// Get the preview transform attached to the given rayOrigin
+		/// Get the field grab transform attached to the given rayOrigin
 		/// </summary>
 		Func<Transform, Transform> getFieldGrabOriginForRayOrigin { set; }
 	}
