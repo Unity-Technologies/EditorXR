e22583f9aa749891533218592c71513851514337
Dylan Urquidi <dylanu@unity3d.com>
2017-03-20 18:29:36 -0700

    Add new ISetCustomHighlightColor interface; decorates a class with the functionality to define a custom tooltip highlight color gradient

---
+++ Scripts/Interfaces/ISetCustomTooltipColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor.Experimental.EditorVR.Helpers;
+
+#if UNITY_EDITOR
+namespace UnityEditor.Experimental.EditorVR
+{
+	/// <summary>
+	/// Decorates classes which can specify custom tool tip colors
+	/// </summary>
+	public interface ISetCustomTooltipColor
+	{
+		/// <summary>
+		/// Custom tooltip highlight color
+		/// </summary>
+		GradientPair customToolTipHighlightColor { get; }
+	}
+}
+#endif
