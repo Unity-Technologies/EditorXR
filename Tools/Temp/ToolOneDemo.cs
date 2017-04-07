ae0792f306ca4c261b7742e347c9779250245053
Dylan Urquidi <dylanu@unity3d.com>
2017-04-06 20:50:27 -0700

    Add temporary demoTools in order to populate the MainMenu

---
+++ Tools/Temp/ToolOneDemo.cs
@@ -0,0 +1,39 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEngine.InputNew;
+
+namespace UnityEditor.Experimental.EditorVR.Tools
+{
+	[MainMenuItem("XTool1", "Create", "1 Extra Demo Tool")]
+	//[MainMenuItem(false)]
+	sealed class ToolOneDemo : MonoBehaviour, ITool, ICustomActionMap, IUsesRayOrigin, IUsesSpatialHash
+	{
+		public Transform rayOrigin { get; set; }
+
+		public ActionMap actionMap
+		{
+			get { return m_ActionMap; }
+			set { m_ActionMap = value; }
+		}
+
+		[SerializeField]
+		private ActionMap m_ActionMap;
+
+		public void ProcessInput(ActionMapInput input, ConsumeControlDelegate consumeControl)
+		{
+			var standardAlt = (StandardAlt)input;
+			if (standardAlt.action.wasJustPressed)
+			{
+				Transform sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+				if (rayOrigin)
+					sphere.position = rayOrigin.position + rayOrigin.forward * 5f;
+
+				this.AddToSpatialHash(sphere.gameObject);
+
+				consumeControl(standardAlt.action);
+			}
+		}
+	}
+}
+#endif
