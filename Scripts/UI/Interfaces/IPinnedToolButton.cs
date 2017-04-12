aa594c7117c2d99b0282ddb38ea1e6581a9803ed
Dylan Urquidi <dylanu@unity3d.com>
2017-04-12 16:09:19 -0700

    Add IPinnedToolButton interface; used in PinnedToolButtons, allowing for custom pinned tool button implementations that pair visually/functionally with custom alternate menu implementations

---
+++ Scripts/UI/Interfaces/IPinnedToolButton.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEditor.Experimental.EditorVR.Helpers;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+	/// <summary>
+	/// Gives decorated class Pinned Tool Button functionality
+	/// </summary>
+	public interface IPinnedToolButton
+	{
+		int menuButtonOrderPosition { get; }
+		int activeToolOrderPosition  { get; }
+		int order { get; set; }
+		int activeButtonCount { get; set; }
+		bool highlighted { set; }
+		bool moveToAlternatePosition { set; }
+		Type toolType { get; set; }
+		Type previewToolType { get; set; }
+		Vector3 toolButtonActivePosition { get; } // Shared active button offset from the alternate menu
+		GradientPair gradientPair { set; }
+
+		event Action<Transform> hoverEnter;
+		event Action<Transform> hoverExit;
+		event Action<Transform> selected;
+	}
+
+	public static class IPinnedToolButtonMethods
+	{
+	}
+}
+#endif
