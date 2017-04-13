3124ede8fd0c53616f5d4a669f87ce87edccdeb0
Dylan Urquidi <dylanu@unity3d.com>
2017-04-12 22:35:57 -0700

    Add new IMenuIcon interface, used in tools/objects that implement a sprite icon that can be utilized in menus

---
+++ Scripts/UI/Interfaces/IMenuIcon.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+	/// <summary>
+	/// Declares a class as a menu UI element that can be represented via an icon/sprite
+	/// </summary>
+	public interface IMenuIcon
+	{
+		/// <summary>
+		/// The icon representing this Action that can be displayed in menus
+		/// </summary>
+		Sprite icon { get; }
+	}
+}
+#endif
