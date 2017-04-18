f40b8a577aa6f3801af0b32c01e6dd1c9c03d2a6
Dylan Urquidi <dylanu@unity3d.com>
2017-04-18 15:16:27 -0700

    Fix exceptions in temporary MakeSphereTool, after converting to Standard action map input

--- Tools/Temp/MakeSphereTool.cs
+++ Tools/Temp/MakeSphereTool.cs
@@ -7,27 +7,18 @@ namespace UnityEditor.Experimental.EditorVR.Tools
 {
 	[MainMenuItem("Sphere", "Create", "Create spheres in the scene")]
 	//[MainMenuItem(false)]
-	sealed class MakeSphereTool : MonoBehaviour, ITool, ICustomActionMap, IUsesRayOrigin, IUsesSpatialHash, IMenuIcon
+	sealed class MakeSphereTool : MonoBehaviour, ITool, IStandardActionMap, IUsesRayOrigin, IUsesSpatialHash, IMenuIcon
 	{
-		[SerializeField]
-		private ActionMap m_ActionMap;
-
 		[SerializeField]
 		Sprite m_Icon;
 
 		public Transform rayOrigin { get; set; }
 		public Sprite icon { get { return m_Icon; } }
 
-		public ActionMap actionMap
-		{
-			get { return m_ActionMap; }
-			set { m_ActionMap = value; }
-		}
-
 		public void ProcessInput(ActionMapInput input, ConsumeControlDelegate consumeControl)
 		{
-			var standardAlt = (Standard)input;
-			if (standardAlt.action.wasJustPressed)
+			var standardInput = (Standard)input;
+			if (standardInput.action.wasJustPressed)
 			{
 				Transform sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
 				if (rayOrigin)
@@ -35,7 +26,7 @@ namespace UnityEditor.Experimental.EditorVR.Tools
 
 				this.AddToSpatialHash(sphere.gameObject);
 
-				consumeControl(standardAlt.action);
+				consumeControl(standardInput.action);
 			}
 		}
 	}
