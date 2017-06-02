2bdb4f13e7370855b53c7e0d775fd60c3144a951
Dylan Urquidi <dylanu@unity3d.com>
2017-06-01 17:47:41 -0700

    Add HapticsModuleConnector

---
+++ Scripts/Core/InterfaceConnectors/HapticsModuleConnector.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR && UNITY_EDITORVR
+using UnityEditor.Experimental.EditorVR.Modules;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+	partial class EditorVR
+	{
+		class HapticsModuleConnector : Nested, ILateBindInterfaceMethods<HapticsModule>
+		{
+			public void LateBindInterfaceMethods(HapticsModule provider)
+			{
+				IPerformHapticsMethods.performHaptics = provider.PerformHaptics;
+			}
+		}
+	}
+}
+#endif
