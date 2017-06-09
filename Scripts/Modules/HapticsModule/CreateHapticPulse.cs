c26f503937de81ce6c1d69e815c404c213b76928
Dylan Urquidi <dylanu@unity3d.com>
2017-06-08 17:03:18 -0700

    Add creation of haptic pulses via scriptable objects

---
+++ Scripts/Modules/HapticsModule/CreateHapticPulse.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+	public class CreateHapticPulse
+	{
+		[MenuItem("Assets/Create/EditorVR/HapticPulse")]
+		public static void CreateMyAsset()
+		{
+			HapticPulse asset = ScriptableObject.CreateInstance<HapticPulse>();
+
+			string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+			if (path == string.Empty)
+				path = "Assets";
+			else if (Path.GetExtension(path) != string.Empty)
+				path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), string.Empty);
+
+			string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/NewHapticPulse.asset");
+
+			AssetDatabase.CreateAsset(asset, assetPathAndName);
+			AssetDatabase.SaveAssets();
+			EditorUtility.FocusProjectWindow();
+			Selection.activeObject = asset;
+		}
+	}
+}
+#endif
---
+++ Scripts/Modules/HapticsModule/HapticPulse.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+	class HapticPulse : ScriptableObject
+	{
+		[SerializeField]
+		[Range(0.001f, 1f)]
+		float m_Duration = 0.25f;
+
+		[SerializeField]
+		[Range(0f, 1f)]
+		float m_Intensity = 1f;
+
+		[SerializeField]
+		bool m_FadeIn = false;
+	
+		[SerializeField]
+		bool m_FadeOut = false;
+
+		// Don't allow public setting of value; use inspector-set values
+		public float duration { get { return m_Duration; } }
+		public float intensity { get { return m_Intensity; } }
+		public bool fadeIn { get { return m_FadeIn; } }
+		public bool fadeOut { get { return m_FadeOut; } }
+	}
+}
+#endif
