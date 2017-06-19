43cb4954750b7fa55baa21a0383f081355633a47
Matt Schoen <schoen@unity3d.com>
2017-06-19 16:05:51 -0700

    Scale flying speed using primary trigger

--- Tools/LocomotionTool/Locomotion.cs
+++ Tools/LocomotionTool/Locomotion.cs
@@ -10,7 +10,7 @@ namespace UnityEngine.InputNew
 		public ButtonInputControl @blink { get { return (ButtonInputControl)this[0]; } }
 		public ButtonInputControl @grip { get { return (ButtonInputControl)this[1]; } }
 		public ButtonInputControl @thumb { get { return (ButtonInputControl)this[2]; } }
-		public ButtonInputControl @trigger { get { return (ButtonInputControl)this[3]; } }
+		public AxisInputControl @speed { get { return (AxisInputControl)this[3]; } }
 		public ButtonInputControl @reverse { get { return (ButtonInputControl)this[4]; } }
 		public ButtonInputControl @forward { get { return (ButtonInputControl)this[5]; } }
 	}
