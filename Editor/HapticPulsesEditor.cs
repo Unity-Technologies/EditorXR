bbb9bd6b48b8589895bc416b80760b0aec67bc87
Matt Schoen <schoen@unity3d.com>
2017-07-12 11:13:34 -0700

    Add pulse preview button; Add ctrl+w to close HapitcPulsesEditor and TooltipsEditor; Add reset/repaint on project change to reflect new/deleted assets

--- Editor/HapticPulsesEditor.cs
+++ Editor/HapticPulsesEditor.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEditor.Experimental.EditorVR.Core;
+using UnityEditor.Experimental.EditorVR.Modules;
+using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
 
 namespace UnityEditor.Experimental.EditorVR.UI
@@ -19,6 +21,8 @@ namespace UnityEditor.Experimental.EditorVR.UI
 		Vector2 m_Scroll;
 		float m_Multiplier = 1;
 
+		HapticsModule m_HapticsModule;
+
 		[MenuItem("Edit/Project Settings/EditorVR/Haptic Pulses")]
 		static void Init()
 		{
@@ -28,6 +32,10 @@ namespace UnityEditor.Experimental.EditorVR.UI
 		void OnEnable()
 		{
 			Reset();
+
+			m_HapticsModule = ObjectUtils.CreateGameObjectWithComponent<HapticsModule>();
+			m_HapticsModule.name = "Haptics Module";
+			m_HapticsModule.gameObject.hideFlags = HideFlags.HideAndDontSave;
 
 			Undo.undoRedoPerformed += OnUndoRedo;
 		}
@@ -55,13 +63,20 @@ namespace UnityEditor.Experimental.EditorVR.UI
 
 		void OnDisable()
 		{
+			ObjectUtils.Destroy(m_HapticsModule.gameObject);
 			Undo.undoRedoPerformed -= OnUndoRedo;
 		}
 
 		void OnGUI()
 		{
+			if (Event.current.Equals(Event.KeyboardEvent("^w"))) {
+				Close();
+				GUIUtility.ExitGUI();
+			}
+
 			const float nameColumnWidth = 250f;
 			const float durationColumnWidth = 60f;
+			const float previewColumnWidth = 80f;
 
 			m_Scroll = GUILayout.BeginScrollView(m_Scroll);
 
@@ -105,6 +120,9 @@ namespace UnityEditor.Experimental.EditorVR.UI
 					pulse.serializedObject.ApplyModifiedProperties();
 				}
 
+				if (GUILayout.Button("Preview", GUILayout.Width(previewColumnWidth)))
+					m_HapticsModule.Pulse(Node.LeftHand, pulse.pulse);
+
 				GUILayout.EndHorizontal();
 			}
 
@@ -112,6 +130,12 @@ namespace UnityEditor.Experimental.EditorVR.UI
 		}
 
 		void OnUndoRedo()
+		{
+			Reset();
+			Repaint();
+		}
+
+		void OnProjectChange()
 		{
 			Reset();
 			Repaint();
