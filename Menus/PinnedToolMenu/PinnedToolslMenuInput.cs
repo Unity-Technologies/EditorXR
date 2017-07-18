8069d69578900be95c3b56fb656814761f3455eb
Dylan Urquidi <dylanu@unity3d.com>
2017-07-17 20:06:44 -0700

    Add cancel button to PinnedToolsMenuInput

--- Menus/PinnedToolMenu/PinnedToolslMenuInput.cs
+++ Menus/PinnedToolMenu/PinnedToolslMenuInput.cs
@@ -9,5 +9,6 @@ namespace UnityEngine.InputNew
 		
 		public ButtonInputControl @show { get { return (ButtonInputControl)this[0]; } }
 		public ButtonInputControl @select { get { return (ButtonInputControl)this[1]; } }
+		public ButtonInputControl @cancel { get { return (ButtonInputControl)this[2]; } }
 	}
 }
