967ba829772c451de53a5c920ca7a1a926d4bcc2
Dylan Urquidi <dylanu@unity3d.com>
2017-07-18 18:40:19 -0700

    Set HintLine radius when performing pulse

--- Menus/PinnedToolMenu/Scripts/HintLine.cs
+++ Menus/PinnedToolMenu/Scripts/HintLine.cs
@@ -2,13 +2,13 @@
 using UnityEditor.Experimental.EditorVR.Extensions;
 using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
-using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
 namespace UnityEditor.Experimental.EditorVR.Menus
 {
 	public class HintLine : MonoBehaviour
 	{
+		const string k_ShaderLineRadiusPropertyName = "_lineRadius";
+
 		[SerializeField]
 		bool m_HideOnInitialize = true;
 
@@ -30,6 +30,9 @@ namespace UnityEditor.Experimental.EditorVR.Menus
 		[SerializeField]
 		Color m_PulseColor = Color.white;
 
+		[SerializeField]
+		MeshRenderer m_MeshRenderer;
+
 		readonly Vector3 k_HiddenScale = Vector3.zero;
 
 		Transform m_IconTransform;
@@ -37,6 +40,7 @@ namespace UnityEditor.Experimental.EditorVR.Menus
 		Coroutine m_VisibilityCoroutine;
 		Coroutine m_ScrollArrowPulseCoroutine;
 		float m_PulseDuration;
+		Material m_HintLineMaterial;
 
 		public float LineWidth { set { m_ScrollLineRenderer.SetWidth(value, value); } }
 		public Vector3[] Positions { set { m_ScrollLineRenderer.SetPositions(value) ; } }
@@ -77,6 +81,12 @@ namespace UnityEditor.Experimental.EditorVR.Menus
 			m_ScrollLineRenderer.SetVertexCount(4);
 			m_ScrollLineRenderer.useWorldSpace = true;
 			m_ScrollLineRenderer.SetWidth(0f, 0f);
+			m_HintLineMaterial = MaterialUtils.GetMaterialClone(m_MeshRenderer);
+		}
+
+		void OnDestroy()
+		{
+			ObjectUtils.Destroy(m_HintLineMaterial);
 		}
 
 		/*
@@ -140,24 +150,28 @@ namespace UnityEditor.Experimental.EditorVR.Menus
 			//Debug.LogError("Pulsing color of hint arrow : " + gameObject.name);
 			const float kTargetDuration = 1f;
 			m_PulseDuration = 0f;
+			var maxShaderLineRadius = new Vector3 (0.03f, 0f, 100f);
+			var minShaderLineRadius = new Vector3 (0.005f, 0f, 100f);
+			var currentVector3ShaderLineRadius = m_HintLineMaterial.GetVector(k_ShaderLineRadiusPropertyName);
 			var currentColor = m_ScrollLineRenderer.Colors[0]; // The line stand & end colors are the same; fetch only one of them
+			var currentShaderLineRadius = m_HintLineMaterial.GetFloat(k_ShaderLineRadiusPropertyName);
 			while (m_PulseDuration < kTargetDuration)
 			{
 				var shapedDuration = MathUtilsExt.SmoothInOutLerpFloat(m_PulseDuration / kTargetDuration);
 				var newColor = Color.Lerp(currentColor, m_PulseColor, shapedDuration);
 				m_ScrollLineRenderer.SetColors(newColor, newColor);
 				m_PulseDuration += Time.unscaledDeltaTime * 5;
+				m_HintLineMaterial.SetVector(k_ShaderLineRadiusPropertyName, Vector3.Lerp(currentVector3ShaderLineRadius, maxShaderLineRadius, shapedDuration));
 				yield return null;
 			}
-
-			
 
 			while (m_PulseDuration > 0f)
 			{
 				var shapedDuration = MathUtilsExt.SmoothInOutLerpFloat(m_PulseDuration / kTargetDuration);
 				var newColor = Color.Lerp(m_VisibleColor, m_PulseColor, shapedDuration); 
 				m_ScrollLineRenderer.SetColors(newColor, newColor);
-				m_PulseDuration -= Time.unscaledDeltaTime * 2;
+				m_PulseDuration -= Time.unscaledDeltaTime * 1.5f;
+				m_HintLineMaterial.SetVector(k_ShaderLineRadiusPropertyName, Vector3.Lerp(minShaderLineRadius, maxShaderLineRadius, shapedDuration));
 				yield return null;
 			}
 
