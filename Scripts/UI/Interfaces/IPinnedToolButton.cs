36f7cb4619b08837c036b54c819b64fcc834054c
Dylan Urquidi <dylanu@unity3d.com>
2017-07-19 19:38:16 -0700

    Cleanup IPinnedToolButton interface structure

--- Scripts/UI/Interfaces/IPinnedToolButton.cs
+++ Scripts/UI/Interfaces/IPinnedToolButton.cs
@@ -10,45 +10,22 @@ namespace UnityEditor.Experimental.EditorVR
 	public interface IPinnedToolButton
 	{
 		Type previewToolType { set; }
-		int order { get; set; }
 		Type toolType { get; set; }
+		int order { get; set; }
+		int maxButtonCount { get; set; }
 		bool highlighted { get; set; }
 		bool secondaryButtonHighlighted { get; }
-		int maxButtonCount { get; set; }
-		Func<int> visibileButtonCount { get; set; }
 		bool activeTool { get; set; }
-		Action<Type> selectTool { get; set; }
-		Func<bool> closeButton { get; set; }
 		bool toolTipVisible { set; }
-		Action destroy { get; }
 		bool implementsSecondaryButton { get; set; }
+		Vector3 primaryUIContentContainerLocalScale { get; set; }
+		Action destroy { get; }
+		Action<Type> selectTool { get; set; }
+		Func<int> visibileButtonCount { get; set; }
+		Func<bool> closeButton { get; set; }
 		Action<IPinnedToolButton> showAllButtons { get; set; }
 		Action hoverExit { get; set; }
 		event Action hovered;
-
-		/*
-		int menuButtonOrderPosition { get; }
-		int activeToolOrderPosition  { get; }
-		int activeButtonCount { set; }
-		bool revealed { set; }
-		bool moveToAlternatePosition { get; set; }
-		Transform rayOrigin { get; set; }
-		Vector3 toolButtonActivePosition { get; } // Shared active button offset from the alternate menu
-		Sprite icon { set; }
-		Node node { set; }
-		GradientPair gradientPair { get; }
-
-		event Action<Transform> hoverEnter;
-		event Action<Transform> hoverExit;
-		event Action<Transform> selected;
-
-		Action<Transform, Type> selectTool { set; }
-		Action<Transform, IPinnedToolButton> deletePinnedToolButton { set; }
-		Action<Transform, bool> revealAllToolButtons { set; }
-		Action<Transform, int, bool> dighlightSingleButton { set; }
-		Action<Transform> selectHighlightedButton { set; }
-		Action<Transform> deleteHighlightedButton { set; }
-		*/
 	}
 
 	public static class IPinnedToolButtonMethods
