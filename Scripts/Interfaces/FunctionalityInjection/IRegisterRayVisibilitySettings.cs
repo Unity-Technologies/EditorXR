bf413323a4b1285918af4aa20368990aaec833dc
Matt Schoen <schoen@unity3d.com>
2017-07-19 20:54:50 -0700

    WIP

--- Scripts/Interfaces/FunctionalityInjection/IRegisterRayVisibilitySettings.cs
+++ Scripts/Interfaces/FunctionalityInjection/IRegisterRayVisibilitySettings.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 namespace UnityEditor.Experimental.EditorVR
 {
+	public delegate void RegisterRayVisibilitySettingsDelegate<T>(Transform rayOrigin, object caller, T settings, int priority = 0);
 	/// <summary>
 	/// Implementors can show & hide the default ray
 	/// </summary>
-	public interface IRegisterRayVisibilitySettings
+	public interface IRegisterRayVisibilitySettings<T>
 	{
+		RegisterRayVisibilitySettingsDelegate<T> registerRayVisibilitySettings { set; }
 	}
 
 	public static class IRegisterRayVisibilitySettingsMethods
 	{
-		internal delegate void RegisterRayVisibilitySettingsDelegate(Transform rayOrigin, object caller, bool rayVisible, bool coneVisible, int priority = 0);
-
-		internal static RegisterRayVisibilitySettingsDelegate registerRayVisibilitySettings { get; set; }
 		internal static Action<Transform, object> unregisterRayVisibilitySettings { get; set; }
 
 		/// <summary>
@@ -26,9 +25,9 @@ namespace UnityEditor.Experimental.EditorVR
 		/// <param name="rayVisible">Show or hide</param>
 		/// <param name="coneVisible">An optional parameter to hide or show only the ray and not the cone</param>
 		/// <param name="priority">The priority level of this request</param>
-		public static void RegisterRayVisibilitySettings(this IRegisterRayVisibilitySettings customRay, Transform rayOrigin, object caller, bool rayVisible, bool coneVisible, int priority = 0)
+		public static void RegisterRayVisibilitySettings<T>(this IRegisterRayVisibilitySettings<T> customRay, Transform rayOrigin, object caller, T settings, int priority = 0)
 		{
-			registerRayVisibilitySettings(rayOrigin, caller, rayVisible, coneVisible, priority);
+			customRay.registerRayVisibilitySettings(rayOrigin, caller, settings, priority);
 		}
 
 		/// <summary>
@@ -36,7 +35,7 @@ namespace UnityEditor.Experimental.EditorVR
 		/// </summary>
 		/// <param name="rayOrigin">The ray to hide or show</param>
 		/// <param name="caller">The object which has locked the ray</param>
-		public static void UnregisterRayVisibilitySettings(this IRegisterRayVisibilitySettings customRay, Transform rayOrigin, object caller)
+		public static void UnregisterRayVisibilitySettings<TVisibilitySettings>(this IRegisterRayVisibilitySettings customRay, Transform rayOrigin, object caller)
 		{
 			unregisterRayVisibilitySettings(rayOrigin, caller);
 		}
