8c441801df5fe4d5191d9a841381e092367c1a98
Dylan Urquidi <dylanu@unity3d.com>
2017-08-16 18:05:59 -0700

    Add summaries to IControlSpatialScrolling

--- Scripts/Interfaces/FunctionalityInjection/IControlSpatialScrolling.cs
+++ Scripts/Interfaces/FunctionalityInjection/IControlSpatialScrolling.cs
@@ -13,13 +13,23 @@ namespace UnityEditor.Experimental.EditorVR
 
 	public static class IControlSpatialScrollingMethods
 	{
-		internal delegate SpatialScrollModule.SpatialScrollData PerformSpatialScrollDelegate (object caller, Node? node, Vector3 startingPosition, Vector3 currentPosition, float repeatingScrollLengthRange, int scrollableItemCount, int maxItemCount = -1);
+		internal delegate SpatialScrollModule.SpatialScrollData PerformSpatialScrollDelegate (object caller, Node? node, Vector3 startingPosition,
+			Vector3 currentPosition, float repeatingScrollLengthRange, int scrollableItemCount, int maxItemCount = -1);
 		internal static PerformSpatialScrollDelegate performSpatialScroll { private get; set; }
 
 		/// <summary>
-		/// 
+		/// Perform a spatial scroll action
 		/// </summary>
-		public static SpatialScrollModule.SpatialScrollData PerformSpatialScroll(this IControlSpatialHinting obj, object caller, Node? node, Vector3 startingPosition, Vector3 currentPosition, float repeatingScrollLengthRange, int scrollableItemCount, int maxItemCount = -1)
+		/// <param name="caller">The object requesting the performance of a spatial scroll action</param>
+		/// <param name="node">The node on which to display & perform the spatial scroll</param>
+		/// <param name="startingPosition">The initial position of the spatial scroll</param>
+		/// <param name="currentPosition">The current/updated position of the spatial scroll</param>
+		/// <param name="repeatingScrollLengthRange">The length at which a scroll action will return a repeating/looping value</param>
+		/// <param name="scrollableItemCount">The number of items being scrolled through with this action</param>
+		/// <param name="maxItemCount">The maximum number of items that can be scrolled through for this action</param>
+		/// <returns>The spatial scroll data for a single scroll action, but an individual caller object</returns>
+		public static SpatialScrollModule.SpatialScrollData PerformSpatialScroll(this IControlSpatialHinting obj, object caller, Node? node,
+			Vector3 startingPosition, Vector3 currentPosition, float repeatingScrollLengthRange, int scrollableItemCount, int maxItemCount = -1)
 		{
 			return performSpatialScroll(caller, node, startingPosition, currentPosition, repeatingScrollLengthRange, scrollableItemCount, maxItemCount);
 		}
@@ -28,8 +38,9 @@ namespace UnityEditor.Experimental.EditorVR
 		internal static EndSpatialScrollDelegate endSpatialScroll { private get; set; }
 
 		/// <summary>
-		/// 
+		/// End a spatial scrolling action for a given caller
 		/// </summary>
+		/// <param name="caller">The caller whose spatial scroll action will be ended</param>
 		public static void EndSpatialScroll(this IControlSpatialHinting obj, object caller)
 		{
 			endSpatialScroll(caller);
