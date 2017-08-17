7c13e757445aaa657f94e4368c438c8eb94df954
Dylan Urquidi <dylanu@unity3d.com>
2017-08-16 18:07:13 -0700

    Reformat IPinnedToolButton structure

--- Scripts/Core/Interfaces/IPinnedToolButton.cs
+++ Scripts/Core/Interfaces/IPinnedToolButton.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 namespace UnityEditor.Experimental.EditorVR
 {
 	/// <summary>
-	/// Gives decorated class Pinned Tool Button functionality
+	/// Provides an interface for handling Pinned Tool Button functionality
 	/// </summary>
 	public interface IPinnedToolButton
 	{
 		Type previewToolType { set; }
-		Type toolType { get; set; }
+		Type toolType { get; }
+
 		int order { get; set; }
-		int maxButtonCount { get; set; }
+		int maxButtonCount { set; }
 		float iconHighlightedLocalZOffset { set; }
 		bool highlighted { get; set; }
 		bool secondaryButtonHighlighted { get; }
-		bool isActiveTool { get; set; }
+		bool isActiveTool { set; }
 		bool toolTipVisible { set; }
-		bool implementsSecondaryButton { get; set; }
+		bool implementsSecondaryButton { set; }
 		Vector3 primaryUIContentContainerLocalScale { get; set; }
-		Transform tooltipTarget { get; set; }
+		Transform tooltipTarget { set; }
 		string previewToolDescription { set; }
+
 		Action destroy { get; }
 		Action<Type> selectTool { set; }
-		Func<Type, int> visibileButtonCount { set; }
-		Func<bool> closeButton { set; }
 		Action<IPinnedToolButton> showAllButtons { set; }
 		Action hoverExit { set; }
-		event Action hovered;
-	}
 
-	public static class IPinnedToolButtonMethods
-	{
+		Func<Type, int> visibileButtonCount { set; }
+		Func<bool> closeButton { set; }
+
+		event Action hovered;
 	}
 }
 #endif
