0b08627f7842e602a12d2e7e7fcfe57dcf255bae
Dylan Urquidi <dylanu@unity3d.com>
2017-09-02 05:47:07 -0700

    Replace ToolsMenuInput ActionMap; fixes serialized naming issues with control scheme

--- Menus/ToolsMenu/ToolsMenuInput.cs
+++ Menus/ToolsMenu/ToolsMenuInput.cs
@@ -4,8 +4,8 @@ using UnityEngine.InputNew;
 // GENERATED FILE - DO NOT EDIT MANUALLY
 namespace UnityEngine.InputNew
 {
-	public class ToolslMenuInput : ActionMapInput {
-		public ToolslMenuInput (ActionMap actionMap) : base (actionMap) { }
+	public class ToolsMenuInput : ActionMapInput {
+		public ToolsMenuInput (ActionMap actionMap) : base (actionMap) { }
 		
 		public ButtonInputControl @show { get { return (ButtonInputControl)this[0]; } }
 		public ButtonInputControl @select { get { return (ButtonInputControl)this[1]; } }
