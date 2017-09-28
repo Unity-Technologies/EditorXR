56d7147a0f1cabc02ffa1bb84a19335561dc9835
Dylan Urquidi <dylanu@unity3d.com>
2017-09-28 02:14:26 -0700

    Add ProxyAffordanceMap; scriptable object housing control-based details for ProxyUI

---
+++ Scripts/Helpers/ProxyAffordanceMap.cs
@@ -0,0 +1,88 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputNew;
+using UnityEngine.Serialization;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+	[CreateAssetMenu(menuName = "EditorVR/EXR Proxy Affordance Map", fileName = "NewProxyAffordanceMap.asset")]
+	public class ProxyAffordanceMap : ScriptableObject
+	{
+		// TODO REMOVE - 5.6 HACK for items not appearing in the create menu
+#if UNITY_EDITOR
+		[MenuItem("Assets/Create/EditorVR/EditorVR Proxy Affordance Map")]
+		public static void Create()
+		{
+			var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+			if (string.IsNullOrEmpty(path))
+				path = "Assets";
+
+			if (!Directory.Exists(path))
+				path = Path.GetDirectoryName(path);
+
+			var affordanceMap = ScriptableObject.CreateInstance<ProxyAffordanceMap>();
+			path = AssetDatabase.GenerateUniqueAssetPath(path + "/NewProxyAffordanceMap.asset");
+			AssetDatabase.CreateAsset(affordanceMap, path);
+		}
+#endif
+
+		public enum VisibilityControlType
+		{
+			colorProperty,
+			alphaProperty,
+			materialSwap
+		}
+
+		[Serializable]
+		public class ButtonObject
+		{
+			[SerializeField]
+			VRInputDevice.VRControl m_Control;
+
+			[SerializeField]
+			VisibilityControlType m_VisibilityType;
+
+			//TODO Expose each visibility control type field set based on the selected visibility control type
+
+			[SerializeField]
+			string m_ColorVisibilityProperty; // Consider custom inspector that only displays this if "shaderProperty" is chosen
+
+			[SerializeField]
+			string m_AlphaVisibilityProperty; // Consider custom inspector that only displays this if "shaderProperty" is chosen
+
+			[SerializeField]
+			Material m_SwapMaterial;
+
+			//[SerializeField]
+			//float m_PropertyHiddenValue;
+
+			public VRInputDevice.VRControl control { get { return m_Control; } }
+			public VisibilityControlType visibilityControlType { get { return m_VisibilityType; } }
+			public string colorVisibilityProperty { get { return m_ColorVisibilityProperty; } }
+			public string alphaVisibilityProperty { get { return m_AlphaVisibilityProperty; } }
+			public Material swapMaterial { get { return m_SwapMaterial; } }
+		}
+
+		[SerializeField]
+		VisibilityControlType m_BodyVisibilityType;
+
+		[SerializeField]
+		[FormerlySerializedAs("m_Buttons")]
+		ButtonObject[] m_AffordanceDefinitions;
+
+		public ButtonObject[] AffordanceDefinitions { get { return m_AffordanceDefinitions; } }
+		public VisibilityControlType BodyVisibilityControlType { get { return m_BodyVisibilityType; } }
+
+		/*
+		// Don't allow public setting of value; use inspector-set values
+		public float duration { get { return m_Duration; } internal set { m_Duration = value; } }
+		public float intensity { get { return m_Intensity; } internal set { m_Intensity = value; } }
+		public bool fadeIn { get { return m_FadeIn; } }
+		public bool fadeOut { get { return m_FadeOut; } }
+		*/
+	}
+}
+#endif
