ff12f41ac7a0441796e96f00740815071a00438b
Dylan Urquidi <dylanu@unity3d.com>
2017-09-29 19:46:14 -0700

    Refine support for handling color blending, alpha fading, and material swaps for when setting proxy visibility in ProxyUI

--- Scripts/Helpers/ProxyUI.cs
+++ Scripts/Helpers/ProxyUI.cs
@@ -1,5 +1,4 @@
 ﻿#if UNITY_EDITOR
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +7,7 @@ using UnityEditor.Experimental.EditorVR.Extensions;
 using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
 using UnityEngine.InputNew;
+using VisibilityControlType = UnityEditor.Experimental.EditorVR.Core.ProxyAffordanceMap.VisibilityControlType;
 
 namespace UnityEditor.Experimental.EditorVR.Proxies
 {
@@ -16,12 +16,13 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 		List<Renderer> m_BodyRenderers; // Renderers not associated with controls, & will be hidden when displaying feedback/tooltips
 		bool m_BodyRenderersVisible = true; // Renderers default to visible/true
 		AffordanceObject[] m_Affordances;
-		List<Material> m_BodyMaterials; // Collection of unique body MeshRenderer materials
+		//List<Material> m_BodyMaterials; // Collection of unique body MeshRenderer materials // TODO : delete, no longer used?
 		Coroutine m_BodyVisibilityCoroutine;
 
 		// Map of unique body materials to their original Colors (used for affordances with the "color" visibility control type)
 		// The second param, ColorPair, houses the original cached color, and a value, representing the color to lerp FROM when animating visibility
-		Dictionary<Material, ColorPair> m_BodyMaterialOriginalColorMap = new Dictionary<Material, ColorPair>();
+		Dictionary<Material, affordancePropertyTuple<Color>> m_BodyMaterialOriginalColorMap = new Dictionary<Material, affordancePropertyTuple<Color>>();
+		Dictionary<Material, affordancePropertyTuple<float>> m_BodyMaterialOriginalAlphaMap = new Dictionary<Material, affordancePropertyTuple<float>>();
 
 		// Used to draw visual attention to individual affordances
 		Dictionary<VRInputDevice.VRControl, Dictionary<Material, Color>> m_AffordanceMaterialOriginalColorMap = new Dictionary<VRInputDevice.VRControl, Dictionary<Material, Color>>();
@@ -29,22 +30,21 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 		[SerializeField]
 		ProxyAffordanceMap m_AffordanceMap;
 
-		private class ColorPair
+		/// <summary>
+		/// Model containing original value, and values to "animate from", unique to each body MeshRenderer material.
+		/// Visibility of all objects in the proxy body are driven by a single AffordanceVisibilityDefinition,
+		/// as opposed to individual interactable affordances, which each have their own AffordanceVisibilityDefinition, which contains their unique value data.
+		/// This is a lightweight class to store that data, alleviating the need to duplicate an affordance definition for each body renderer as well.
+		/// </summary>
+		private class affordancePropertyTuple<T>
 		{
-			/// <summary>
-			/// The original/cached color of the material
-			/// </summary>
-			public Color originalColor { get; set; }
+			public T originalValue { get; private set; }
+			public T animateFromValue { get; set; }
 
-			/// <summary>
-			/// The color to lerp FROM as the current color when animating visibility
-			/// </summary>
-			public Color animateFromColor { get; set; }
-
-			public ColorPair(Color originalColor, Color animateFromColor)
+			public affordancePropertyTuple(T originalValue, T animateFromValue)
 			{
-				this.originalColor = originalColor;
-				this.animateFromColor = animateFromColor;
+				this.originalValue = originalValue;
+				this.animateFromValue = animateFromValue;
 			}
 		}
 
@@ -89,16 +89,36 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 				}
 
 				// Collect renderers not associated with affordances
+				// Material swaps don't need to cache original values, only alpha & color
+				var bodyVisibilityDefinition = m_AffordanceMap.bodyVisibilityDefinition;
 				m_BodyRenderers = GetComponentsInChildren<Renderer>(true).Where(x => !affordanceRenderers.Contains(x)).ToList();
-				foreach (var renderer in m_BodyRenderers)
+				Debug.LogWarning("Don't collect renderers from the menu origins!!!");
+				switch (m_AffordanceMap.bodyVisibilityDefinition.visibilityType)
 				{
-					// TODO: support for skipping the cloning of materials in the body that are shared between objects, to reduce draw calls
-					var materialClone = MaterialUtils.GetMaterialClone(renderer); // TODO: support multiple materials per-renderer
-					if (materialClone != null)
-					{
-						var originalColor = materialClone.color;
-						m_BodyMaterialOriginalColorMap[materialClone] = new ColorPair(originalColor, originalColor);
-					}
+					case VisibilityControlType.colorProperty:
+						foreach (var renderer in m_BodyRenderers)
+						{
+							// TODO: support for skipping the cloning of materials in the body that are shared between objects, to reduce draw calls
+							var materialClone = MaterialUtils.GetMaterialClone(renderer); // TODO: support multiple materials per-renderer
+							if (materialClone != null)
+							{
+								var originalColor = materialClone.color;
+								m_BodyMaterialOriginalColorMap[materialClone] = new affordancePropertyTuple<Color>(originalColor, originalColor);
+							}
+						}
+						break;
+					case VisibilityControlType.alphaProperty:
+						string shaderAlphaPropety = bodyVisibilityDefinition.alphaProperty;
+						foreach (var renderer in m_BodyRenderers)
+						{
+							var materialClone = MaterialUtils.GetMaterialClone(renderer); // TODO: support multiple materials per-renderer
+							if (materialClone != null)
+							{
+								var originalAlpha = materialClone.GetFloat(shaderAlphaPropety);
+								m_BodyMaterialOriginalAlphaMap[materialClone] = new affordancePropertyTuple<float>(originalAlpha, originalAlpha);
+							}
+						}
+						break;
 				}
 			}
 		}
@@ -114,20 +134,33 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 					return;
 
 				m_BodyRenderersVisible = value;
-				this.RestartCoroutine(ref m_BodyVisibilityCoroutine, AnimateBodyVisibility(value));
+				var bodyVisibilityDefinition = m_AffordanceMap.bodyVisibilityDefinition;
+				switch (bodyVisibilityDefinition.visibilityType)
+				{
+					case VisibilityControlType.colorProperty:
+						this.RestartCoroutine(ref m_BodyVisibilityCoroutine, AnimateColorBodyVisibility(value));
+						break;
+					case VisibilityControlType.alphaProperty:
+						this.RestartCoroutine(ref m_BodyVisibilityCoroutine, AnimateAlphaBodyVisibility(value));
+						break;
+					case VisibilityControlType.materialSwap:
+						SwapToHiddenMaterial(value);
+						break;
+				}
 			}
 		}
 
-		IEnumerator AnimateBodyVisibility(bool isVisible)
+		IEnumerator AnimateColorBodyVisibility(bool isVisible)
 		{
-			// Cache current "from" colors
+			var bodyVisibilityDefinition = m_AffordanceMap.bodyVisibilityDefinition;
 			foreach (var kvp in m_BodyMaterialOriginalColorMap)
 			{
-				kvp.Value.animateFromColor = kvp.Key.GetColor("_Color");
+				kvp.Value.animateFromValue = kvp.Key.GetColor(bodyVisibilityDefinition.colorProperty);
 			}
 
 			const float kSpeedScalar = 2f;
 			const float kTargetAmount = 1f;
+			const float kHiddenValue = 0.25f;
 			var currentAmount = 0f;
 			while (currentAmount < kTargetAmount)
 			{
@@ -136,13 +169,52 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 				foreach (var kvp in m_BodyMaterialOriginalColorMap)
 				{
 					// Set original cached color when visible, transparent when hidden
-					var colorFrom = kvp.Value.animateFromColor;
-					var colorTo = isVisible ? kvp.Value.originalColor : new Color(colorFrom.r, colorFrom.g, colorFrom.b, 0.25f);
-					var currentColor = Color.Lerp(colorFrom, colorTo, smoothedAmount);
+					var valueFrom = kvp.Value.animateFromValue;
+					var valueTo = isVisible ? kvp.Value.originalValue : new Color(valueFrom.r, valueFrom.g, valueFrom.b, kHiddenValue);
+					var currentColor = Color.Lerp(valueFrom, valueTo, smoothedAmount);
 					kvp.Key.color = currentColor;
 				}
 
 				yield return null;
+			}
+		}
+
+		IEnumerator AnimateAlphaBodyVisibility(bool isVisible)
+		{
+			var bodyVisibilityDefinition = m_AffordanceMap.bodyVisibilityDefinition;
+			foreach (var kvp in m_BodyMaterialOriginalAlphaMap)
+			{
+				kvp.Value.animateFromValue = kvp.Key.GetFloat(bodyVisibilityDefinition.alphaProperty);
+			}
+
+			const float kSpeedScalar = 2f;
+			const float kTargetAmount = 1f;
+			const float kHiddenValue = 0.25f;
+			string shaderAlphaPropety = bodyVisibilityDefinition.alphaProperty;
+			var currentAmount = 0f;
+			while (currentAmount < kTargetAmount)
+			{
+				currentAmount += Time.unscaledDeltaTime * kSpeedScalar;
+				var smoothedAmount = MathUtilsExt.SmoothInOutLerpFloat(currentAmount);
+				foreach (var kvp in m_BodyMaterialOriginalAlphaMap)
+				{
+					var valueFrom = kvp.Value.animateFromValue;
+					var valueTo = isVisible ? kvp.Value.originalValue : kHiddenValue;
+					var currentAlpha = Mathf.Lerp(valueFrom, valueTo, smoothedAmount);
+					kvp.Key.SetFloat(shaderAlphaPropety, currentAlpha);
+				}
+
+				yield return null;
+			}
+		}
+
+		void SwapToHiddenMaterial(bool swapToHiddenMaterial)
+		{
+			var bodyVisibilityDefinition = m_AffordanceMap.bodyVisibilityDefinition;
+			var swapMaterial = swapToHiddenMaterial ? bodyVisibilityDefinition.hiddenMaterial : bodyVisibilityDefinition.originalMaterial;
+			foreach (var renderer in m_BodyRenderers)
+			{
+				renderer.material = swapMaterial;
 			}
 		}
 	}
