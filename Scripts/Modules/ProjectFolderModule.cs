c1e29dff7192b721a78439d72ffaf52c56ec69b0
Matt Schoen <schoen@unity3d.com>
2017-10-03 17:37:11 -0700

    Fix refactor issue that broke ProjectFolderModule

--- Scripts/Modules/ProjectFolderModule.cs
+++ Scripts/Modules/ProjectFolderModule.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Experimental.EditorVR.Core;
 using UnityEditor.Experimental.EditorVR.Data;
 using UnityEngine;
 
 namespace UnityEditor.Experimental.EditorVR.Modules
 {
-	sealed class ProjectFolderModule : MonoBehaviour, IConnectInterfaces
+	sealed class ProjectFolderModule : MonoBehaviour, IInterfaceConnector
 	{
 		// Maximum time (in ms) before yielding in CreateFolderData: should be target frame time
 		const float k_MaxFrameTime = 0.01f;
