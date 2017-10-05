d848ae8f8c16d477d0e9e816f8ea95a25ddb18b0
Matt Schoen <schoen@unity3d.com>
2017-10-05 13:47:46 -0700

    Begin implementing Vive controller prefab

--- Scripts/Proxies/ViveProxy.cs
+++ Scripts/Proxies/ViveProxy.cs
@@ -16,11 +16,6 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 		[SerializeField]
 		GameObject m_RightHandTouchProxyPrefab;
 
-#if ENABLE_STEAMVR_INPUT
-		SteamVR_RenderModel m_LeftModel;
-		SteamVR_RenderModel m_RightModel;
-#endif
-
 		public override void Awake()
 		{
 			if (VRDevice.model.IndexOf("oculus", StringComparison.OrdinalIgnoreCase) >= 0)
@@ -45,34 +40,7 @@ namespace UnityEditor.Experimental.EditorVR.Proxies
 			while (!active)
 				yield return null;
 
-			m_LeftModel = m_LeftHand.GetComponentInChildren<SteamVR_RenderModel>(true);
-			m_LeftModel.enabled = true;
-			m_RightModel = m_RightHand.GetComponentInChildren<SteamVR_RenderModel>(true);
-			m_RightModel.enabled = true;
-
 			yield return base.Start();
-		}
-
-		public override void Update()
-		{
-			if (active && m_LeftModel && m_RightModel)
-			{
-				var viveInputToEvents = (ViveInputToEvents)m_InputToEvents;
-
-				//If proxy is not mapped to a physical input device, check if one has been assigned
-				if ((int) m_LeftModel.index == -1 && viveInputToEvents.steamDevice[0] != -1)
-				{
-					// HACK set device index individually instead of calling SetDeviceIndex because loading device mesh dynamically does not work in editor. Prefab has Model Override set and mesh generated, calling SetDeviceIndex clears the model.
-					m_LeftModel.index = (SteamVR_TrackedObject.EIndex)viveInputToEvents.steamDevice[0];
-				}
-
-				if ((int) m_RightModel.index == -1 && viveInputToEvents.steamDevice[1] != -1)
-				{
-					m_RightModel.index = (SteamVR_TrackedObject.EIndex)viveInputToEvents.steamDevice[1];
-				}
-			}
-
-			base.Update();
 		}
 #endif
 	}
