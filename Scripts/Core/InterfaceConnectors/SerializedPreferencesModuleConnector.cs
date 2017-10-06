52c96b07d6b065957e46e8d2a27829f1033fe627
Matt Schoen <schoen@unity3d.com>
2017-10-06 16:39:01 -0700

    Add missing ModuleConnectors; Serialization change on exit

---
+++ Scripts/Core/InterfaceConnectors/SerializedPreferencesModuleConnector.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR && UNITY_EDITORVR
+using UnityEditor.Experimental.EditorVR.Modules;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+	partial class EditorVR
+	{
+		class SerializedPreferencesModuleConnector : Nested, IInterfaceConnector
+		{
+			public void ConnectInterface(object @object, object userData = null)
+			{
+				var serializePreferences = @object as ISerializePreferences;
+				if (serializePreferences != null)
+					evr.GetModule<SerializedPreferencesModule>().AddSerializer(serializePreferences);
+			}
+
+			public void DisconnectInterface(object @object, object userData = null)
+			{
+				var serializePreferences = @object as ISerializePreferences;
+				if (serializePreferences != null)
+					evr.GetModule<SerializedPreferencesModule>().RemoveSerializer(serializePreferences);
+			}
+		}
+	}
+}
+#endif
