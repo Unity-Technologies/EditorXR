39011fdef006053e29c498c955665278ac7c458b
Dylan Urquidi <dylanu@unity3d.com>
2017-10-12 15:08:11 -0700

    Extract AffordanceObjects from the ProxyHelper; add to Scripts\Data

--- Scripts/Helpers/ProxyHelper.cs
+++ Scripts/Helpers/ProxyHelper.cs
@@ -1,52 +1,9 @@
 ﻿#if UNITY_EDITOR
-using System;
 using System.Collections.Generic;
-using UnityEditor.Experimental.EditorVR.UI;
 using UnityEngine;
-using UnityEngine.InputNew;
-using UnityEngine.Serialization;
 
 namespace UnityEditor.Experimental.EditorVR.Proxies
 {
-    [Serializable]
-    public class AffordanceObject
-    {
-        [SerializeField]
-        VRInputDevice.VRControl m_Control;
-
-        [SerializeField]
-        Transform m_Transform;
-
-        [SerializeField]
-        Tooltip[] m_Tooltips;
-
-        [SerializeField]
-        Renderer m_Renderer;
-
-        [FlagsProperty]
-        [SerializeField]
-        AxisFlags m_TranslateAxes;
-
-        [FlagsProperty]
-        [SerializeField]
-        AxisFlags m_RotateAxes;
-
-        [SerializeField]
-        float m_Min;
-
-        [SerializeField]
-        float m_Max;
-
-        public VRInputDevice.VRControl control { get { return m_Control; } }
-        public Transform transform { get { return m_Transform; } }
-        public Renderer renderer { get { return m_Renderer; } }
-	    public Tooltip[] tooltips { get { return m_Tooltips; } }
-		public AxisFlags translateAxes { get { return m_TranslateAxes; } }
-        public AxisFlags rotateAxes { get { return m_RotateAxes; } }
-        public float min { get { return m_Min; } }
-        public float max { get { return m_Max; } }
-    }
-
     /// <summary>
     /// Reference container for additional content origins on a device
     /// </summary>
