0e7bd6a977daed3e78649937a6c7dd2b79349419
Matt Schoen <schoen@unity3d.com>
2017-11-04 00:21:01 -0700

    Remove reference to GizmoModule

--- Scripts/Core/EditorVR.cs
+++ Scripts/Core/EditorVR.cs
@@ -234,8 +234,6 @@ namespace UnityEditor.Experimental.EditorVR.Core
 
             AddModule<FeedbackModule>();
 
-            AddModule<GizmoModule>();
-
             viewer.AddPlayerModel();
 
             GetNestedModule<Rays>().CreateAllProxies();
