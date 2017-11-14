1bab72d8cc2d14969c8291692a7829414f485f82
Matt Schoen <mtschoen@gmail.com>
2017-11-13 21:05:59 -0800

    Update usings in IBlocks

--- Scripts/Interfaces/FunctionalityInjection/IBlocks.cs
+++ Scripts/Interfaces/FunctionalityInjection/IBlocks.cs
@@ -1,7 +1,7 @@
 #if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
-using UnityEngine.Networking;
+using UnityEditor.Experimental.EditorVR.Workspaces;
 
 namespace UnityEditor.Experimental.EditorVR
 {
