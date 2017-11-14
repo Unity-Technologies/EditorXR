d64d5285d0a6f2176937cc21c71431690fc03a0a
Matt Schoen <mtschoen@gmail.com>
2017-11-13 22:10:40 -0800

    Import TextMeshPro; Implement infinite scroll and click-to-import

--- Scripts/Interfaces/FunctionalityInjection/IBlocks.cs
+++ Scripts/Interfaces/FunctionalityInjection/IBlocks.cs
@@ -8,15 +8,17 @@ namespace UnityEditor.Experimental.EditorVR
     /// <summary>
     /// Provides access to the Web Module
     /// </summary>
-    public interface IBlocks { }
+    public interface IBlocks
+    {
+    }
 
     public static class IBlocksMethods
     {
-        internal static Action<List<BlocksAsset>> getFeaturedModels;
+        internal static Action<List<BlocksAsset>, Action<string>, string> getFeaturedModels;
 
-        public static void GetFeaturedModels(this IBlocks obj, List<BlocksAsset> assets)
+        public static void GetFeaturedModels(this IBlocks obj, List<BlocksAsset> assets, Action<string> listCallback, string nextPageToken = null)
         {
-            getFeaturedModels(assets);
+            getFeaturedModels(assets, listCallback, nextPageToken);
         }
     }
 }
