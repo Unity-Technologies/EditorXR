d9a9ad3992b96f704d88dbb8e90dcdb0ec0572e2
Matt Schoen <schoen@unity3d.com>
2017-11-18 00:11:45 -0800

    Add documentation to IWeb

--- Scripts/Interfaces/FunctionalityInjection/IWeb.cs
+++ Scripts/Interfaces/FunctionalityInjection/IWeb.cs
@@ -18,16 +18,32 @@ namespace UnityEditor.Experimental.EditorVR
         internal static Action<string, Action<DownloadHandlerTexture>> downloadTexture;
         internal static Action<string, string, Action> downloadToDisk;
 
+        /// <summary>
+        /// Download the given URL
+        /// </summary>
+        /// <param name="url">The URL to request</param>
+        /// <param name="completed">A method to be called on completion</param>
         public static void Download(this IWeb obj, string url, Action<DownloadHandler> completed)
         {
             download(url, completed);
         }
 
+        /// <summary>
+        /// Download the given URL using a DownloadTextureHandler
+        /// </summary>
+        /// <param name="url">The URL to request</param>
+        /// <param name="completed">A method to be called on completion</param>
         public static void DownloadTexture(this IWeb obj, string url, Action<DownloadHandlerTexture> completed)
         {
             downloadTexture(url, completed);
         }
 
+        /// <summary>
+        /// Download the given URL to a file on disk
+        /// </summary>
+        /// <param name="url">The URL to request</param>
+        /// <param name="destination">The file in which to store the results</param>
+        /// <param name="completed">A method to be called on completion</param>
         public static void Download(this IWeb obj, string url, string destination, Action completed)
         {
             downloadToDisk(url, destination, completed);
