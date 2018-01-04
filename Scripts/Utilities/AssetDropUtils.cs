e24430fc9b7a0058e2f7c24812c5b374bd3a12a5
Stella Cannefax <stella@unity3d.com>
2018-01-04 00:32:11 -0800

    standardize asset drop undo labels and code style

--- Scripts/Utilities/AssetDropUtils.cs
+++ Scripts/Utilities/AssetDropUtils.cs
@@ -7,14 +7,14 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
 {
     internal static class AssetDropUtils
     {
-        const string k_AudioClipAttachUndoLabel = "Add Audio Clip";
-        const string k_AssignAnimationClipUndoLabel = "Add Animation Clip";
-        const string k_AssignVideoClipUndoLabel = "Assign Video Clip";
-        const string k_AssignFontUndoLabel = "Assign Font";
-        const string k_AttachScriptUndoLabel = "Add Script";
-        const string k_AssignMaterialUndoLabel = "Assign Material";
-        const string k_AssignPhysicMaterialUndoLabel = "Assign Physic Material";
-        const string k_AssignMaterialShaderUndoLabel = "Assign Material Shader";
+        const string k_AssignAudioClipUndo = "Assign Audio Clip";
+        const string k_AssignAnimationClipUndo = "Assign Animation Clip";
+        const string k_AssignVideoClipUndo = "Assign Video Clip";
+        const string k_AssignFontUndo = "Assign Font";
+        const string k_AttachScriptUndo = "Assign Script";
+        const string k_AssignMaterialUndo = "Assign Material";
+        const string k_AssignPhysicMaterialUndo = "Assign Physic Material";
+        const string k_AssignMaterialShaderUndo = "Assign Material Shader";
 
         internal static AnimationClip AttachAnimationClip(GameObject go, AssetData data)
         {
@@ -22,7 +22,7 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
             if (animation == null)
                 animation = Undo.AddComponent<Animation>(go);
 
-            Undo.RecordObject(animation, k_AssignAnimationClipUndoLabel);
+            Undo.RecordObject(animation, k_AssignAnimationClipUndo);
             animation.clip = (AnimationClip)data.asset;
 
             Undo.IncrementCurrentGroup();
@@ -34,7 +34,8 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
             var source = go.GetComponent<AudioSource>();
             if (source == null)
                 source = Undo.AddComponent<AudioSource>(go);
-            
+
+            Undo.RecordObject(source, k_AssignAudioClipUndo);
             source.clip = (AudioClip)data.asset;
 
             Undo.IncrementCurrentGroup();
@@ -47,7 +48,7 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
             if (player == null)
                 player = Undo.AddComponent<VideoPlayer>(go);
 
-            Undo.RecordObject(player, k_AssignVideoClipUndoLabel);
+            Undo.RecordObject(player, k_AssignVideoClipUndo);
             player.clip = (VideoClip)data.asset;
 
             Undo.IncrementCurrentGroup();
@@ -68,9 +69,10 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
 
             if (renderer != null)
             {
-                Undo.RecordObject(go, k_AssignMaterialUndoLabel);
+                Undo.RecordObject(go, k_AssignMaterialUndo);
                 renderer.sharedMaterial = (Material)data.asset;
                 Undo.IncrementCurrentGroup();
+
                 return renderer.sharedMaterial;
             }
 
@@ -83,7 +85,7 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
 
             if (renderer != null)
             {
-                Undo.RecordObject(go, k_AssignMaterialUndoLabel);
+                Undo.RecordObject(go, k_AssignMaterialUndo);
                 // this warns that we're leaking materials into the scene,
                 // and creates a new instance, but we don't want to change
                 // the shader on the shared material here.
@@ -102,7 +104,7 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
 
             if(collider != null)
             {
-                Undo.RecordObject(go, k_AssignPhysicMaterialUndoLabel);
+                Undo.RecordObject(go, k_AssignPhysicMaterialUndo);
                 collider.material = (PhysicMaterial)data.asset;
                 Undo.IncrementCurrentGroup();
 
@@ -119,7 +121,7 @@ namespace UnityEditor.Experimental.EditorVR.Utilities
             if (text != null)
             {
                 var font = (Font)data.asset;
-                Undo.RecordObject(go, k_AssignFontUndoLabel);
+                Undo.RecordObject(go, k_AssignFontUndo);
                 text.font = font;
                 Undo.IncrementCurrentGroup();
                 return font;
