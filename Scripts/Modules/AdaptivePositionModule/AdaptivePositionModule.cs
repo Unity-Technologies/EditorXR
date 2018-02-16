2128b9c002438f7ffed87d0355f75c95b24f37ac
Dylan Urquidi <dylanu@unity3d.com>
2018-02-15 16:29:48 -0800

    Add AdaptivePositionData-related fields and props to AdaptivePositionModule

--- Scripts/Modules/AdaptivePositionModule/AdaptivePositionModule.cs
+++ Scripts/Modules/AdaptivePositionModule/AdaptivePositionModule.cs
@@ -1,10 +1,120 @@
 ﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor.Experimental.EditorVR.Core;
+using UnityEditor.Experimental.EditorVR.SpatialUI;
 using UnityEngine;
 
 namespace UnityEditor.Experimental.EditorVR.Modules
 {
     sealed class AdaptivePositionModule : MonoBehaviour
     {
+        /// <summary>
+        /// Distance beyond which content will be re-positioned at the ideal distance from the user's gaze/hmd
+        /// </summary>
+        const float k_AllowedDistanceDivergence = 0.1f;
+
+        [SerializeField]
+        HapticPulse m_MovementStartPulse; // The pulse performed when beginning to move an element to a new destination
+
+        [SerializeField]
+        HapticPulse m_MovingPulse; // The pulse performed while moving an element to a new target position in the user's FOV
+
+        [SerializeField]
+        HapticPulse m_MovementEndPulse; // The pulse performed when movement of an element has ended, the element has bee repositioned in the user's FOV
+
+        // Collection housing objects whose position is controlled by this module
+        readonly List<IAdaptPosition> m_AdaptivePositionElements = new List<IAdaptPosition>();
+
+        public class AdaptivePositionData : INodeToRay
+        {
+            public AdaptivePositionData(IAdaptPosition caller, Node node, Vector3 startingPosition, Vector3 currentPosition, float repeatingScrollLengthRange, int scrollableItemCount, int maxItemCount = -1, bool centerVisuals = true)
+            {
+                this.caller = caller;
+                this.node = node;
+                this.startingPosition = startingPosition;
+                this.currentPosition = currentPosition;
+                this.repeatingScrollLengthRange = repeatingScrollLengthRange;
+                this.scrollableItemCount = scrollableItemCount;
+                this.maxItemCount = maxItemCount;
+                this.centerVisuals = centerVisuals;
+                spatialDirection = null;
+                rayOrigin = this.RequestRayOriginFromNode(node);
+            }
+
+            // Below is Data assigned by calling object requesting spatial scroll processing
+
+            /// <summary>
+            /// The object/caller initiating this particular spatial scroll action
+            /// </summary>
+            public IAdaptPosition caller { get; set; }
+
+            /// <summary>
+            /// The node on which this spatial scroll is being processed
+            /// </summary>
+            public Node node { get; set; }
+
+            /// <summary>
+            /// The ray origin on which this spatial scroll is being processed
+            /// </summary>
+            public Transform rayOrigin { get; set; }
+
+            /// <summary>
+            /// The origin/starting position of the scroll
+            /// </summary>
+            public Vector3 startingPosition { get; set; }
+
+            /// <summary>
+            /// The current scroll position
+            /// </summary>
+            public Vector3 currentPosition { get; set; }
+
+            /// <summary>
+            /// The magnitude at which a scroll will repeat/reset to its original scroll starting value
+            /// </summary>
+            public float repeatingScrollLengthRange { get; set; }
+
+            /// <summary>
+            /// Number of items being scrolled through
+            /// </summary>
+            public int scrollableItemCount { get; set; }
+
+            /// <summary>
+            /// Maximum number of items (to be scrolled through) that will be allowed
+            /// </summary>
+            public int maxItemCount { get; set; }
+
+            /// <summary>
+            /// If true, expand scroll visuals out from the center of the trigger/origin/start position
+            /// </summary>
+            public bool centerVisuals { get; set; }
+
+            // The Values below are populated by scroll processing
+
+            /// <summary>
+            /// The vector defining the spatial scroll direction
+            /// </summary>
+            public Vector3? spatialDirection { get; set; }
+
+            /// <summary>
+            /// 0-1 offset/magnitude of current scroll position, relative to the trigger/origin/start point, and the repeatingScrollLengthRange
+            /// </summary>
+            public float normalizedLoopingPosition { get; set; }
+
+            /// <summary>
+            /// Value representing how much of the pre-scroll drag amount has occurred
+            /// </summary>
+            public float dragDistance { get; set; }
+
+            /// <summary>
+            /// Bool denoting that the scroll trigger magnitude has been exceeded
+            /// </summary>
+            public bool passedMinDragActivationThreshold { get { return spatialDirection != null; } }
+
+            public void UpdateExistingScrollData(Vector3 newPosition)
+            {
+                currentPosition = newPosition;
+            }
+        }
     }
 }
 #endif
