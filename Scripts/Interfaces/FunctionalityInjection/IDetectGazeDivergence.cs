13d282003598c43fc361467a039b18feea98048b
Dylan Urquidi <dylanu@unity3d.com>
2018-02-16 16:48:10 -0800

    Add initial implementation of GazeDivergenceModule/Connector & IDetectGazeDivergence

---
+++ Scripts/Interfaces/FunctionalityInjection/IDetectGazeDivergence.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEditor.Experimental.EditorVR.Modules;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    /// Gives decorated class ability to detect gaze divergence above an defined threshold, for a given transform's forward vector
+    /// 
+    /// Spatially scrolling allows for directional input-device movement to drive changes/progression of UI
+    /// element selection, without the need for additional input beyond the movement of an input-device.
+    /// </summary>
+    public interface IDetectGazeDivergence
+    {
+        /// <summary>
+        /// The data defining an element whose worldspace vector will be tested against the gaze-forward-vector
+        /// </summary>
+        GazeDivergenceModule.GazeDivergenceData gazeDivergenceData { get; set; }
+    }
+
+    public static class IDetectGazeDivergenceMethods
+    {
+        internal delegate bool IsAboveDivergenceThresholdDelegate(IDetectGazeDivergence obj, GazeDivergenceModule.GazeDivergenceData data);
+
+        internal static IsAboveDivergenceThresholdDelegate isAboveDivergenceThreshold { private get; set; }
+
+        public static bool IsAboveDivergenceThreshold(this IDetectGazeDivergence obj, GazeDivergenceModule.GazeDivergenceData data)
+        {
+            return isAboveDivergenceThreshold(obj, data);
+        }
+    }
+}
+#endif
---
+++ Scripts/Modules/GazeDivergenceModule/GazeDivergenceModule.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using UnityEditor.Experimental.EditorVR.Utilities;
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR.Modules
+{
+    public sealed class GazeDivergenceModule : MonoBehaviour
+    {
+        Transform m_GazeSourceTransform;
+
+        public class GazeDivergenceData
+        {
+            public GazeDivergenceData(Vector3 directionToTest, float divergenceThreshold)
+            {
+                this.directionToTest = directionToTest;
+                this.divergenceThreshold = divergenceThreshold;
+            }
+
+            // Below is Data assigned by calling object requesting a divergence delta test
+
+            /// <summary>
+            /// The vector, whose divergence will be tested against the gaze source's forward vector
+            /// </summary>
+            public Vector3 directionToTest { get; set; }
+
+            /// <summary>
+            /// Degree, beyond which the divergence threshold will return TRUE if passed
+            /// </summary>
+            public float divergenceThreshold { get; set; }
+        }
+
+        void Awake()
+        {
+            m_GazeSourceTransform = CameraUtils.GetMainCamera().transform;
+        }
+
+        public bool IsAboveDivergenceThreshold(GazeDivergenceData data)
+        {
+            var isAbove = Vector3.Dot(data.directionToTest, m_GazeSourceTransform.forward) >= Mathf.Abs(data.divergenceThreshold);
+            return isAbove;
+        }
+    }
+}
+#endif
