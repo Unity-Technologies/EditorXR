978bea63641cee4e901779b59cd94771eb5f8709
Dylan Urquidi <dylanu@unity3d.com>
2018-02-22 17:02:06 -0800

    Add new SpatialUI input action map

---
+++ Menus/SpatialUI/SpatialUIInput.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputNew;
+
+// GENERATED FILE - DO NOT EDIT MANUALLY
+namespace UnityEngine.InputNew
+{
+	public class SpatialUIInput : ActionMapInput {
+		public SpatialUIInput (ActionMap actionMap) : base (actionMap) { }
+		
+		public AxisInputControl @show { get { return (AxisInputControl)this[0]; } }
+		public ButtonInputControl @select { get { return (ButtonInputControl)this[1]; } }
+		public ButtonInputControl @cancel { get { return (ButtonInputControl)this[2]; } }
+	}
+}
