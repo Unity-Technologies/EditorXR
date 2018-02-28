3c22746d18a1751f79743ffcbb9c14c3f9ece93e
Dylan Urquidi <dylanu@unity3d.com>
2018-02-27 17:52:02 -0800

    Remove Linq using from DefaultProxyRay

--- Scripts/Proxies/DefaultProxyRay.cs
+++ Scripts/Proxies/DefaultProxyRay.cs
@@ -1,7 +1,6 @@
 #if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor.Experimental.EditorVR.Extensions;
 using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
