a2c74ad94437d95f2a6ae368c3c905c09ac91ed3
Dylan Urquidi <dylanu@unity3d.com>
2018-02-27 20:42:31 -0800

    Add new ISpatialMenuProvider interface; used by menus that can provide a sub-menu in the SpatialUI

---
+++ Scripts/Interfaces/Capability/ISpatialMenuProvider.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    /// Decorates types that can provide a sub-menu in the SpatialUI
+    /// </summary>
+    public interface ISpatialMenuProvider
+    {
+        /// <summary>
+        /// Name of the menu whose contents will be added to the menu
+        /// </summary>
+        string menuName { get; }
+
+        /// <summary>
+        /// Description of the menu whose contents will be added to the menu
+        /// </summary>
+        string description { get; }
+
+        /// <summary>
+        /// Bool denoting that this menu's contents are being displayed via the SpatialUI
+        /// </summary>
+        bool displayingSpatially { get; }
+    }
+}
+#endif
