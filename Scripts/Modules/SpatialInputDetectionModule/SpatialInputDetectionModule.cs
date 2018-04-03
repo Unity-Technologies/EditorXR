bc37992774cd65743a58d3c54c3533ec9b9c42c3
Dylan Urquidi <dylanu@unity3d.com>
2018-04-03 13:26:58 -0700

    Add initial SpatialInputData structure to SpatialInputDetectionModule; add yaw/pitch/roll flag values to SpatialInputType enum

--- Scripts/Modules/SpatialInputDetectionModule/SpatialInputDetectionModule.cs
+++ Scripts/Modules/SpatialInputDetectionModule/SpatialInputDetectionModule.cs
@@ -18,11 +18,79 @@ namespace UnityEditor.Experimental.EditorVR.Modules
         Z = 1 << 2, // 4
         DragTranslation = 1 << 3, // 8
         SingleAxisRotation = (X ^ Y ^ Z), // Validate that only one axis is being rotated
-        FreeRotation = (X & Y) | (Z & Y) | (Z & X), // Detect at least two axis' crossing their local rotation threshold
+        FreeRotation = (X & Y) | (Z & Y) | (Z & X), // Detect at least two axis' crossing their local rotation threshold, triggers ray-based selection
+        YawLeft = 1 << 4,
+        YawRight = 1 << 5,
+        PitchForward = 1 << 6,
+        PitchBackward = 1 << 7,
+        RollLeft = 1 << 8,
+        RollRight = 1 << 9
     }
 
     public sealed class SpatialInputDetectionModule : MonoBehaviour
     {
+        public class SpatialInputData : INodeToRay
+        {
+            public SpatialInputData(Node node, Vector3 startingPosition, Vector3 currentPosition,
+                Quaternion startingRotation, Quaternion currentRotation)
+            {
+                this.node = node;
+                this.startingPosition = startingPosition;
+                this.currentPosition = currentPosition;
+                this.startingRotation = startingRotation;
+                this.currentRotation = currentRotation;
+                rayOrigin = this.RequestRayOriginFromNode(node);
+            }
+
+            /// <summary>
+            /// The node on which this spatial scroll is being processed
+            /// </summary>
+            public Node node { get; set; }
+
+            /// <summary>
+            /// The ray origin on which this spatial scroll is being processed
+            /// </summary>
+            public Transform rayOrigin { get; set; }
+
+            public SpatialInputType spatialInputType { get; set; }
+
+            /// <summary>
+            /// Signed starting-position-dependent direction
+            /// Each time an input type change occurs, this value is then recalculated from the new local input direction type
+            /// </summary>
+            public float signedDirection { get; set; }
+
+            /// <summary>
+            /// The origin/starting input position
+            /// </summary>
+            public Vector3 startingPosition { get; set; }
+
+            /// <summary>
+            /// The current input position
+            /// </summary>
+            public Vector3 currentPosition { get; set; }
+
+            /// <summary>
+            /// The origin/starting input rotation
+            /// </summary>
+            public Quaternion startingRotation { get; set; }
+
+            /// <summary>
+            /// The current input rotation
+            /// </summary>
+            public Quaternion currentRotation { get; set; }
+
+            /// <summary>
+            /// Value representing how much of the pre-scroll drag amount has occurred
+            /// </summary>
+            public float dragDistance { get; set; }
+
+            public void UpdateExistingScrollData(Vector3 newPosition)
+            {
+                currentPosition = newPosition;
+            }
+        }
+
         [SerializeField]
         HapticPulse m_TranslationPulse; // The pulse performed on a node performing a spatial scroll while only in translation mode
         
