655965422902e0245631aaa014a6a2b44a7a4c46
Dylan Urquidi <dylanu@unity3d.com>
2018-04-16 14:18:00 -0700

    Add new IPerformSpatialRayInteraction interface; implemented by callers wanting to control the P/R/S of the spatial ray, like the Spatial UI, when in ray-based interaction mode

---
+++ Scripts/Interfaces/FunctionalityInjection/IPerformSpatialRayInteraction.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public interface IPerformSpatialRayInteraction
+    {
+        Transform spatialProxyRayDriverTransform { get; set; }
+    }
+
+    public static class IPerformSpatialRayInteractionMethods
+    {
+        internal delegate void UpdateSpatialRayDelegate(IPerformSpatialRayInteraction caller);
+
+        internal static UpdateSpatialRayDelegate updateSpatialRay { private get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static void UpdateSpatialRay(this IPerformSpatialRayInteraction obj)
+        {
+            updateSpatialRay(obj);
+        }
+    }
+}
+#endif
