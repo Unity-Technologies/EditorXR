652664f8d67d2dadb18e07f1546b814706fbaed4
Dylan Urquidi <dylanu@unity3d.com>
2018-04-16 14:20:47 -0700

    Add new ISpatialProxyRay interface; implementers can control the spatial proxy ray's length and appearance; The SpatialInputModule utilizes this, in order to control the visuals of the single Spatial proxy ray; spatial UI can control the P/R/S of the ray via IPerformSpatialRayInteraction

---
+++ Scripts/Interfaces/Entity/ISpatialProxyRay.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor.Experimental.EditorVR.Proxies;
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public interface ISpatialProxyRay
+    {
+        Transform spatialProxyRayOrigin { get; set; }
+        DefaultProxyRay spatialProxyRay { get; set; }
+    }
+
+    public static class ISpatialProxyRayMethods
+    {
+        internal delegate void UpdateSpatialProxyRayDelegate(ISpatialProxyRay caller);
+
+        internal static UpdateSpatialProxyRayDelegate updateSpatialProxyRay { private get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static void UpdateSpatialProxyRay(this ISpatialProxyRay obj)
+        {
+            updateSpatialProxyRay(obj);
+        }
+    }
+}
+#endif
