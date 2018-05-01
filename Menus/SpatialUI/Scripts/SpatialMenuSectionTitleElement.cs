b26e919c967f302b5c2b72f30a95052b6f33fb62
Dylan Urquidi <dylanu@unity3d.com>
2018-05-01 15:32:12 -0700

    Add new controllers for Spatial Menu setction title & sub-menu elements

---
+++ Menus/SpatialUI/Scripts/SpatialMenuSectionTitleElement.cs
@@ -0,0 +1,180 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using TMPro;
+using UnityEditor.Experimental.EditorVR.Extensions;
+using UnityEditor.Experimental.EditorVR.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEditor.Experimental.EditorVR
+{
+    public class SpatialMenuSectionTitleElement : MonoBehaviour, ISpatialMenuElement
+    {
+        [SerializeField]
+        TextMeshProUGUI m_Text;
+
+        [SerializeField]
+        Image m_Icon;
+
+        [SerializeField]
+        CanvasGroup m_CanvasGroup;
+
+        [SerializeField]
+        float m_TransitionDuration = 0.75f;
+
+        [SerializeField]
+        float m_FadeInZOffset = 0.05f;
+
+        [SerializeField]
+        float m_HighlightedZOffset = -0.005f;
+
+        //[SerializeField]
+        //Image m_BackgroundImage;
+
+        RectTransform m_RectTransform;
+        Action m_SelectedAction;
+        Vector2 m_OriginalSize;
+        Vector2 m_ExpandedTooltipDisplaySize;
+        Coroutine m_VisibilityCoroutine;
+        Coroutine m_TooltipVisualsVisibilityCoroutine;
+        Vector3 m_TextOriginalLocalPosition;
+        bool m_Highlighted;
+        Vector3 m_OriginalBordersLocalScale;
+        float m_BordersOriginalAlpha;
+
+        public Transform elementTransform { get { return transform; } }
+        public Action selectedAction { get { return m_SelectedAction; } }
+
+        public bool highlighted
+        {
+            set
+            {
+                if (m_Highlighted == value)
+                    return;
+
+                m_Highlighted = value;
+                this.RestartCoroutine(ref m_VisibilityCoroutine, AnimateHighlight(m_Highlighted));
+            }
+        }
+
+        public Action<Transform, Action, string, string> Setup { get; set; }
+
+        void Awake()
+        {
+            Setup = SetupInternal;
+        }
+
+        public void SetupInternal(Transform parentTransform, Action selectedAction, String displayedText = null, string toolTipText = null)
+        {
+            if (selectedAction == null)
+            {
+                Debug.LogWarning("Cannot setup SpatialUIMenuElement without an assigned action.");
+                ObjectUtils.Destroy(gameObject);
+                return;
+            }
+
+            m_SelectedAction = selectedAction;
+            m_RectTransform = (RectTransform)transform;
+            m_OriginalSize = m_RectTransform.sizeDelta;
+
+            Sprite sprite = null;
+            if (sprite != null) // Displaying a sprite icon instead of text
+            {
+                m_Icon.gameObject.SetActive(true);
+                m_Text.gameObject.SetActive(false);
+                m_Icon.sprite = sprite;
+            }
+            else // Displaying text instead of a sprite icon
+            {
+                m_Icon.gameObject.SetActive(false);
+                m_Text.gameObject.SetActive(true);
+                m_Text.text = displayedText;
+            }
+
+            transform.SetParent(parentTransform);
+            transform.localRotation = Quaternion.identity;
+            transform.localPosition = Vector3.zero;
+            transform.localScale = Vector3.one;
+
+            if (Mathf.Approximately(m_TransitionDuration, 0f))
+                m_TransitionDuration = 0.001f;
+        }
+
+        void OnEnable()
+        {
+            // Cacheing position here, as layout groups were altering the position when originally cacheing in Start()
+            m_TextOriginalLocalPosition = m_Text.transform.localPosition;
+
+            if (m_CanvasGroup != null)
+                this.RestartCoroutine(ref m_VisibilityCoroutine, AnimateVisibility(true));
+        }
+
+        void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+
+        IEnumerator AnimateVisibility(bool fadeIn)
+        {
+            var currentAlpha = fadeIn ? 0f : m_CanvasGroup.alpha;
+            var targetAlpha = fadeIn ? 1f : 0f;
+            var alphaTransitionAmount = 0f;
+            var textTransform = m_Text.transform;
+            var textCurrentLocalPosition = textTransform.localPosition;
+            textCurrentLocalPosition = fadeIn ? new Vector3(m_TextOriginalLocalPosition.x, m_TextOriginalLocalPosition.y, m_FadeInZOffset) : textCurrentLocalPosition;
+            var textTargetLocalPosition = m_TextOriginalLocalPosition;
+            var positionTransitionAmount = 0f;
+            var transitionSubtractMultiplier = 1f / m_TransitionDuration;
+            while (alphaTransitionAmount < 1f)
+            {
+                var alphaSmoothTransition = MathUtilsExt.SmoothInOutLerpFloat(alphaTransitionAmount);
+                var positionSmoothTransition = MathUtilsExt.SmoothInOutLerpFloat(positionTransitionAmount);
+                m_CanvasGroup.alpha = Mathf.Lerp(currentAlpha, targetAlpha, alphaSmoothTransition);
+                textTransform.localPosition = Vector3.Lerp(textCurrentLocalPosition, textTargetLocalPosition, positionSmoothTransition);
+                alphaTransitionAmount += Time.deltaTime * transitionSubtractMultiplier;
+                positionTransitionAmount += alphaTransitionAmount * 1.35f;
+                yield return null;
+            }
+
+            textTransform.localPosition = textTargetLocalPosition;
+            m_CanvasGroup.alpha = targetAlpha;
+            m_VisibilityCoroutine = null;
+        }
+
+        IEnumerator AnimateHighlight(bool isHighlighted)
+        {
+            var currentAlpha = m_CanvasGroup.alpha;
+            var targetAlpha = 1f;
+            var alphaTransitionAmount = 0f;
+            var textTransform = m_Text.transform;
+            var textCurrentLocalPosition = textTransform.localPosition;
+            var textTargetLocalPosition = isHighlighted ? new Vector3(m_TextOriginalLocalPosition.x, m_TextOriginalLocalPosition.y, m_HighlightedZOffset) : m_TextOriginalLocalPosition;
+            var positionTransitionAmount = 0f;
+            var currentTextLocalScale = textTransform.localScale;
+            var targetTextLocalScale = isHighlighted ? Vector3.one * 1.15f : Vector3.one;
+            //var currentBackgroundColor = m_BackgroundImage.color;
+            var targetBackgroundColor = isHighlighted ? Color.black : Color.clear;
+            var speedMultiplier = isHighlighted ? 3f : 6f;
+            while (alphaTransitionAmount < 1f)
+            {
+                var alphaSmoothTransition = MathUtilsExt.SmoothInOutLerpFloat(alphaTransitionAmount);
+                var positionSmoothTransition = MathUtilsExt.SmoothInOutLerpFloat(positionTransitionAmount);
+                m_CanvasGroup.alpha = Mathf.Lerp(currentAlpha, targetAlpha, alphaSmoothTransition);
+                textTransform.localPosition = Vector3.Lerp(textCurrentLocalPosition, textTargetLocalPosition, positionSmoothTransition);
+                textTransform.localScale = Vector3.Lerp(currentTextLocalScale, targetTextLocalScale, alphaSmoothTransition);
+                alphaTransitionAmount += Time.deltaTime * speedMultiplier;
+                positionTransitionAmount += alphaTransitionAmount * 1.35f;
+                //m_BackgroundImage.color = Color.Lerp(currentBackgroundColor, targetBackgroundColor, alphaSmoothTransition * 4);
+                yield return null;
+            }
+
+            textTransform.localPosition = textTargetLocalPosition;
+            textTransform.localScale = targetTextLocalScale;
+            //m_BackgroundImage.color = targetBackgroundColor;
+            m_CanvasGroup.alpha = targetAlpha;
+            m_VisibilityCoroutine = null;
+        }
+    }
+}
+#endif
