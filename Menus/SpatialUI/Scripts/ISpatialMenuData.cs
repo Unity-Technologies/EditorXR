f23137b89966baa2ba3917457b710cea10e12d04
Dylan Urquidi <dylanu@unity3d.com>
2018-05-01 18:57:13 -0700

    Add new ISpatialMenuData interface to project; the core of what will be utilized by elements without already existing string-based description properties; currently implemented by tools that will be shown in the main menu, and the SpatialMenu

---
+++ Menus/SpatialUI/Scripts/ISpatialMenuData.cs
@@ -0,0 +1,12 @@
+﻿#if UNITY_EDITOR
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    /// Declares a class as one which contains data relevant to display in the Spatial Menu system
+    /// </summary>
+    public interface ISpatialMenuData
+    {
+        string spatialMenuDescription { get; }
+    }
+}
+#endif
