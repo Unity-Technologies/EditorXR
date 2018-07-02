1a93b1eef83d6c0a79020cd5bd48d2833f2c4161
Dylan Urquidi <dylanu@unity3d.com>
2018-07-02 14:35:41 -0700

    Adding support for animating the visibility of the player floor, according to the gaze divergence threshold (with the HMD)

--- Scripts/Helpers/PlayerFloor.cs
+++ Scripts/Helpers/PlayerFloor.cs
@@ -1,4 +1,5 @@
 ﻿#if UNITY_EDITOR
+using System.Collections;
 using UnityEditor.Experimental.EditorVR.Core;
 using UnityEditor.Experimental.EditorVR.Extensions;
 using UnityEditor.Experimental.EditorVR.Utilities;
@@ -7,10 +8,13 @@ using UnityEngine;
 namespace UnityEditor.Experimental.EditorVR.Helpers
 {
     [ExecuteInEditMode]
-    public class PlayerFloor : MonoBehaviour, IUsesViewerScale
+    public class PlayerFloor : MonoBehaviour, IUsesViewerScale, IDetectGazeDivergence
     {
         const float k_XOffset = 0.05f;
         const float k_ZOffset = 0.2f;
+
+        [SerializeField]
+        CanvasGroup m_CanvasGroup;
 
         Vector3 m_FloorPosition;
         Transform m_Camera;
@@ -18,6 +22,9 @@ namespace UnityEditor.Experimental.EditorVR.Helpers
 
         Vector3 m_CameraForwardCurrent;
         Vector3 m_CameraForwardTarget;
+
+        bool m_Visible;
+        Coroutine m_AnimationVisibilityCoroutine;
 
         void Awake()
         {
@@ -37,6 +44,32 @@ namespace UnityEditor.Experimental.EditorVR.Helpers
             m_CameraForwardTarget = m_Camera.transform.XZForward();
             m_CameraForwardCurrent = Vector3.Lerp(m_CameraForwardCurrent, m_CameraForwardTarget, Time.unscaledDeltaTime * kLerpMultiplier);
             transform.forward = m_CameraForwardCurrent;
+
+            const float kAllowedDegreeOfGazeDivergence = 10f;
+            var visible = this.IsAboveDivergenceThreshold(transform, kAllowedDegreeOfGazeDivergence);
+            //Debug.LogWarning("<color=yellow> Out of FOV : </color>" + visible);
+
+            if (m_Visible != visible)
+                this.RestartCoroutine(ref m_AnimationVisibilityCoroutine, UpdateVisibility(visible));
+        }
+
+        IEnumerator UpdateVisibility(bool visible)
+        {
+            m_Visible = visible;
+
+            const float kDurationMultiplier = 1.5f;
+            var currentCanvasGroupAlpha = m_CanvasGroup.alpha;
+            var targetCanvasGroupAlpha = visible ? 0f : 1f;
+            var amount = 0f;
+            while (amount < 1f)
+            {
+                var shapedAmount = MathUtilsExt.SmoothInOutLerpFloat(amount += Time.unscaledDeltaTime * kDurationMultiplier);
+                m_CanvasGroup.alpha = Mathf.Lerp(currentCanvasGroupAlpha, targetCanvasGroupAlpha, shapedAmount);
+                yield return null;
+            }
+
+            m_CanvasGroup.alpha = targetCanvasGroupAlpha;
+            m_AnimationVisibilityCoroutine = null;
         }
     }
 }
