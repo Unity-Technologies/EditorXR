aad92725dc37802bff822168aef9833f4a13178e
Dylan Urquidi <dylanu@unity3d.com>
2018-08-08 15:55:16 -0700

    Add new SpatialUICore abstract class; the SpatialMenu is the first robust implementation, SpatialContextUI is planned to derive from core later

---
+++ Menus/SpatialUI/Scripts/SpatialUICore.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR.Menus
+{
+    /// <summary>
+    /// Mandates that derived classes implement core SpatialUI implementation
+    /// The SpatialMenu is the first robust implementation, SpatialContextUI is planned to derive from core
+    /// </summary>
+    public abstract class SpatialUICore : MonoBehaviour
+    {
+        protected SpatialUIToggle m_SpatialPinToggle { get; set; }
+    }
+}
+#endif
