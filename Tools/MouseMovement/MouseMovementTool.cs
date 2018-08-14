566f7c17b254fd2207775f05ace3a4647c88e3f0
Jasmine Roberts <39929370+jasminerunity3d@users.noreply.github.com>
2018-08-14 11:16:30 -0700

    replacing files with new input
    still need to fix float deltaScroll = UnityEngine.Input.mouseScrollDelta.y;

--- Tools/MouseMovement/MouseMovementTool.cs
+++ Tools/MouseMovement/MouseMovementTool.cs
@@ -2,14 +2,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
+using UnityEngine.InputNew;
 using UnityEngine.UI;
 
 namespace UnityEditor.Experimental.EditorVR.Tools
 {
     sealed class MouseMovementTool : MonoBehaviour, ITool, ILocomotor, IRayVisibilitySettings, IUsesViewerScale,
-        IUsesDeviceType, IGetVRPlayerObjects, IBlockUIInteraction, IRequestFeedback
+        IUsesDeviceType, IGetVRPlayerObjects, IBlockUIInteraction, IRequestFeedback, IProcessInput
     {
+
+        private MouseActionMap m_mouseInput;
 
         public Transform cameraRig { private get; set; }
 
@@ -28,30 +32,42 @@ namespace UnityEditor.Experimental.EditorVR.Tools
 
         void Start()
         {
-            GameObject instance = Instantiate(m_RingPrefab, cameraRig, false);
+            GameObject instance = ObjectUtils.Instantiate(m_RingPrefab, cameraRig, false);
             instance.transform.localPosition = ringOffset;
-            GameObject instanceHole = Instantiate(m_Hole, cameraRig, false);
+            GameObject instanceHole = ObjectUtils.Instantiate(m_Hole, cameraRig, false);
             instance.transform.localPosition = new Vector3(0f, 0f, .4f);
             m_Ring = instance.GetComponent<Ring>();
         }
 
-        void FixedUpdate()
+        void Update()
         {
-            bool mouse0 = UnityEngine.Input.GetMouseButton(0);
-            bool mouse0Down = UnityEngine.Input.GetMouseButtonDown(0);
-            bool mouse1 = UnityEngine.Input.GetMouseButton(1);
-            bool mouse1Down = UnityEngine.Input.GetMouseButtonDown(1);
+            
+        }
 
-            if (mouse0)
+        public void ProcessInput(ActionMapInput input, ConsumeControlDelegate consumeControl)
+        {
+            m_mouseInput = (MouseActionMap)input;
+
+            var mouse0 = m_mouseInput.mB0;
+            //bool mouse0Down = UnityEngine.Input.GetMouseButtonDown(0);
+            //bool mouse1 = UnityEngine.Input.GetMouseButton(1);
+            //bool mouse1Down = UnityEngine.Input.GetMouseButtonDown(1);
+
+            if (mouse0.isHeld)
             {
+                consumeControl(mouse0);
+
                 const string mouseX = "Mouse X";
                 const string mouseY = "Mouse Y";
 
                 var k_forward = Vector3.Scale(cameraRig.forward, new Vector3(1f, 0f, 1f)).normalized;
                 var k_right = new Vector3(-k_forward.z, 0f, k_forward.x);
-                var delta = (UnityEngine.Input.GetAxis(mouseX) * k_right + UnityEngine.Input.GetAxis(mouseY) * k_forward)
+                var delta = (m_mouseInput.mX.value * k_right + m_mouseInput.mY.value * k_forward)
                     * m_MovementMultiplier;
-                
+
+                consumeControl(m_mouseInput.mX);
+                consumeControl(m_mouseInput.mY);
+
                 cameraRig.position += delta;
 
                 m_Ring.SetEffectWorldDirection(delta.normalized);
@@ -59,11 +75,11 @@ namespace UnityEditor.Experimental.EditorVR.Tools
 
             float deltaScroll = UnityEngine.Input.mouseScrollDelta.y;
             cameraRig.position += deltaScroll * Vector3.up * 0.1f;
-            
+
             if (deltaScroll != 0f)
             {
                 m_Ring.SetEffectCore();
-                
+
                 if (deltaScroll > 0f)
                 {
                     m_Ring.SetEffectCoreUp();
@@ -75,7 +91,6 @@ namespace UnityEditor.Experimental.EditorVR.Tools
                 }
             }
         }
-
     }
 }
 #endif
