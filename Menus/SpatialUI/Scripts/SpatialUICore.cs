fafe4a5545d85520a79dad15cec80995852cdccd
Dylan Urquidi <dylanu@unity3d.com>
2018-08-15 18:00:05 -0700

    Add haptic pulse fields to SpatialUICore

--- Menus/SpatialUI/Scripts/SpatialUICore.cs
+++ Menus/SpatialUI/Scripts/SpatialUICore.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_EDITOR
+using System;
+using UnityEditor.Experimental.EditorVR.Core;
 using UnityEngine;
 
 namespace UnityEditor.Experimental.EditorVR.Menus
@@ -7,7 +9,7 @@ namespace UnityEditor.Experimental.EditorVR.Menus
     /// Mandates that derived classes implement core SpatialUI implementation
     /// The SpatialMenu is the first robust implementation, SpatialContextUI is planned to derive from core
     /// </summary>
-    public abstract class SpatialUICore : MonoBehaviour
+    public abstract class SpatialUICore : MonoBehaviour, IControlHaptics
     {
         public enum SpatialInterfaceInputMode
         {
@@ -18,8 +20,19 @@ namespace UnityEditor.Experimental.EditorVR.Menus
             BCI
         }
 
-        //[Header("Prefabs")]
+        [Header("Haptic Pulses")]
+        [SerializeField]
+        protected HapticPulse m_AdaptivePositionPulse;
+
+        [SerializeField]
+        protected HapticPulse m_HighlightUIElementPulse;
+
+        [SerializeField]
+        protected HapticPulse m_SustainedHoverUIElementPulse;
+
         protected SpatialUIToggle m_SpatialPinToggle { get; set; }
+
+        public HapticPulse highlightUIElementPulse { get { return m_HighlightUIElementPulse; } }
     }
 }
 #endif
