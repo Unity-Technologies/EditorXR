e74d0f407f8b47f77a19fc8dc46a22d6d6e4ffa2
Dylan Urquidi <dylanu@unity3d.com>
2018-08-24 14:47:54 -0700

    Rename showDirectionalTrigger to showSpatialMenu in the SpatialMenu's actionMap

--- Menus/SpatialUI/SpatialMenu/SpatialMenuInput.cs
+++ Menus/SpatialUI/SpatialMenu/SpatialMenuInput.cs
@@ -20,6 +20,6 @@ namespace UnityEngine.InputNew
 		public AxisInputControl @localPositionZ { get { return (AxisInputControl)this[10]; } }
 		public Vector3InputControl @localPosition { get { return (Vector3InputControl)this[11]; } }
 		public ButtonInputControl @confirm { get { return (ButtonInputControl)this[12]; } }
-		public AxisInputControl @showDirectionalTrigger { get { return (AxisInputControl)this[13]; } }
+		public AxisInputControl @showSpatialMenu { get { return (AxisInputControl)this[13]; } }
 	}
 }
