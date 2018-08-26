386299bfd251ce559e93e1341dcf04f7b6da39c0
Dylan Urquidi <dylanu@unity3d.com>
2018-08-25 22:14:53 -0700

    Remove spatial ghost-ray logic from EditorVR.Tools

--- Scripts/Core/EditorVR.Tools.cs
+++ Scripts/Core/EditorVR.Tools.cs
@@ -151,15 +151,9 @@ namespace UnityEditor.Experimental.EditorVR.Core
                     toolsMenu.setButtonForType(typeof(IMainMenu), null, null);
                     toolsMenu.setButtonForType(typeof(SelectionTool), selectionToolData != null ? selectionToolData.icon : null, k_SelectionToolDescription);
 
-                    var spatialMenu = ObjectUtils.AddComponent<SpatialMenu>(evr.gameObject);// menus.SpawnMenu<SpatialMenu>(rayOrigin);
+                    var spatialMenu = ObjectUtils.AddComponent<SpatialMenu>(evr.gameObject);
                     this.ConnectInterfaces(spatialMenu, rayOrigin);
                     spatialMenu.Setup();
-                    // Only setup the single/shared decoupled spatial ray source if this is the first time the spatial menu was setup
-                    if (!multiRayInputModule.SourceAlreadyAdded(spatialMenu.rayBasedInteractionSource))
-                        multiRayInputModule.AddRaycastSource(proxy, deviceData.node, spatialMenu.rayBasedInteractionSource);
-
-                    //menuHideData[spatialMenu] = new Menus.MenuHideData();
-                    //spatialMenu.hideFlags = 0;
                 }
 
                 evr.GetModule<DeviceInputModule>().UpdatePlayerHandleMaps();
