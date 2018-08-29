809ceacb6c0d2a7f460213cdec2b6e06b3bb4dcd
Dylan Urquidi <dylanu@unity3d.com>
2018-08-29 16:31:34 -0700

    Fix SpatialMenuInput ActionMap thumbstick/show-menu related actions

--- Menus/SpatialUI/SpatialMenu/SpatialMenuInput.cs
+++ Menus/SpatialUI/SpatialMenu/SpatialMenuInput.cs
@@ -16,7 +16,7 @@ namespace UnityEngine.InputNew
 		public AxisInputControl @localRotationW { get { return (AxisInputControl)this[6]; } }
 		public QuaternionInputControl @localRotationQuaternion { get { return (QuaternionInputControl)this[7]; } }
 		public AxisInputControl @localPositionX { get { return (AxisInputControl)this[8]; } }
-		public AxisInputControl @localPositionY { get { return (AxisInputControl)this[9]; } }
+		public Vector2InputControl @localPositionY { get { return (Vector2InputControl)this[9]; } }
 		public AxisInputControl @localPositionZ { get { return (AxisInputControl)this[10]; } }
 		public Vector3InputControl @localPosition { get { return (Vector3InputControl)this[11]; } }
 		public ButtonInputControl @confirm { get { return (ButtonInputControl)this[12]; } }
