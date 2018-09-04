02b35eb08475809da5c31ba7ec704180bb3531ed
Dylan Urquidi <dylanu@unity3d.com>
2018-09-03 20:59:23 -0700

    Add summaries to IControlInputIntersection interface

--- Scripts/Interfaces/FunctionalityInjection/IControlInputIntersection.cs
+++ Scripts/Interfaces/FunctionalityInjection/IControlInputIntersection.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 namespace UnityEditor.Experimental.EditorVR
 {
     /// <summary>
-    ///
+    /// Gives decorated class the ability to Enable/disable a given
+    /// ray-origin's ability to intersect/interact with non UI objects
     /// </summary>
     public interface IControlInputIntersection
     {
@@ -16,8 +17,10 @@ namespace UnityEditor.Experimental.EditorVR
         internal static Action<Transform, bool> setRayOriginEnabled { private get; set; }
 
         /// <summary>
-        ///
+        /// Enable/disable a given ray-origin's ability to intersect/interact with non UI objects
         /// </summary>
+        /// <param name="rayOrigin">RayOrigin to enable/disable</param>
+        /// <param name="enabled">Enabled/disabled state of RayOrigin</param>
         public static void SetRayOriginEnabled(this IControlInputIntersection obj, Transform rayOrigin, bool enabled)
         {
             setRayOriginEnabled(rayOrigin, enabled);
