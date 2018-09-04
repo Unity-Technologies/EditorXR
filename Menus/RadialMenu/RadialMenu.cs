1963c7692d3234b8182252bf64b93777c3168411
Dylan Urquidi <dylanu@unity3d.com>
2018-09-03 18:10:44 -0700

    Remove SpatialMenu provider logic from RadialMenu (used to populate actions in the SpatialMenu); move the logic into the ActionsModule

--- Menus/RadialMenu/RadialMenu.cs
+++ Menus/RadialMenu/RadialMenu.cs
@@ -10,7 +10,7 @@ using UnityEngine.InputNew;
 namespace UnityEditor.Experimental.EditorVR.Menus
 {
     sealed class RadialMenu : MonoBehaviour, IInstantiateUI, IAlternateMenu, IUsesMenuOrigins, ICustomActionMap,
-        IControlHaptics, IUsesNode, IConnectInterfaces, IRequestFeedback, IActionsMenu, ISpatialMenuProvider
+        IControlHaptics, IUsesNode, IConnectInterfaces, IRequestFeedback, IActionsMenu
     {
         const float k_ActivationThreshold = 0.5f; // Do not consume thumbstick or activate menu if the control vector's magnitude is below this threshold
         const string k_SpatialDisplayName = "Actions";
@@ -37,7 +37,6 @@ namespace UnityEditor.Experimental.EditorVR.Menus
         MenuHideFlags m_MenuHideFlags = MenuHideFlags.Hidden;
 
         readonly BindingDictionary m_Controls = new BindingDictionary();
-        readonly List<SpatialMenu.SpatialMenuData> m_SpatialMenuData = new List<SpatialMenu.SpatialMenuData>();
 
         public event Action<Transform> itemWasSelected;
 
@@ -55,12 +54,6 @@ namespace UnityEditor.Experimental.EditorVR.Menus
         public ActionMap actionMap { get { return m_ActionMap; } }
         public bool ignoreActionMapInputLocking { get { return false; } }
 
-        // Spatial UI support
-        public string spatialMenuName { get { return k_SpatialDisplayName; } }
-        public string spatialMenuDescription { get { return k_SpatialDescription; } }
-        public bool displayingSpatially { get; set; }
-        public List<SpatialMenu.SpatialMenuData> spatialMenuData { get { return m_SpatialMenuData; } }
-
         public List<ActionMenuData> menuActions
         {
             get { return m_MenuActions; }
@@ -70,16 +63,6 @@ namespace UnityEditor.Experimental.EditorVR.Menus
                     .Where(a => a.sectionName != null && a.sectionName == ActionMenuItemAttribute.DefaultActionSectionName)
                     .OrderBy(a => a.priority)
                     .ToList();*/
-
-                m_SpatialMenuData.Clear();
-                var spatialMenuActions = new List<SpatialMenu.SpatialMenuElement>();
-                var spatialMenuData = new SpatialMenu.SpatialMenuData(k_SpatialDisplayName, k_SpatialDescription, spatialMenuActions);
-                m_SpatialMenuData.Add(spatialMenuData);
-                foreach (var action in m_MenuActions)
-                {
-                    if (action.addToSpatialMenu)
-                        spatialMenuActions.Add(new SpatialMenu.SpatialMenuElement(action.name, null, action.tooltipText, action.action.ExecuteAction));
-                }
 
                 if (m_RadialMenuUI)
                     m_RadialMenuUI.actions = value;
