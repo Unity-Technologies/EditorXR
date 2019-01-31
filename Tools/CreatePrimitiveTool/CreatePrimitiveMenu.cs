015c4c5ecc785574809a2deb3979ad13ea9a7c65
dunity <dylanu@unity3d.com>
2019-01-31 11:53:44 -0800

    Remove "highlight object" logic from  CreatePrimitiveMenu; no longer used

--- Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
+++ Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
@@ -1,6 +1,5 @@
 #if UNITY_EDITOR
 using System;
-using UnityEditor.Experimental.EditorVR.Helpers;
 using UnityEditor.Experimental.EditorVR.Menus;
 using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
@@ -16,15 +15,11 @@ namespace UnityEditor.Experimental.EditorVR.Tools
         [SerializeField]
         Renderer m_TitleIcon;
 
-        [SerializeField]
-        GameObject[] m_HighlightObjects;
-
         Material m_TitleIconMaterial;
 
         public Action<PrimitiveType, bool> selectPrimitive;
         public Action close;
 
-        readonly GradientPair k_EmptyGradient = new GradientPair(UnityBrandColorScheme.light, UnityBrandColorScheme.darker);
         public Bounds localBounds { get; private set; }
         public int priority { get { return k_Priority; } }
 
@@ -42,34 +37,16 @@ namespace UnityEditor.Experimental.EditorVR.Tools
             m_TitleIconMaterial = MaterialUtils.GetMaterialClone(m_TitleIcon);
             m_TitleIconMaterial.SetColor(k_TopGradientProperty, UnityBrandColorScheme.saturatedSessionGradient.a);
             m_TitleIconMaterial.SetColor(k_BottomGradientProperty, UnityBrandColorScheme.saturatedSessionGradient.b);
-
-            for (var i = 0; i < m_HighlightObjects.Length; ++i)
-            {
-                var go = m_HighlightObjects[i];
-                go.SetActive(false);
-            }
         }
 
         public void SelectPrimitive(int type)
         {
             selectPrimitive((PrimitiveType)type, false);
-
-            // the order of the objects in m_HighlightObjects is matched to the values of the PrimitiveType enum elements
-            for (var i = 0; i < m_HighlightObjects.Length; ++i)
-            {
-                //var go = m_HighlightObjects[i];
-                //go.SetActive(i == type);
-            }
         }
 
         public void SelectFreeformCuboid()
         {
             selectPrimitive(PrimitiveType.Cube, true);
-
-            foreach (var go in m_HighlightObjects)
-            {
-                //go.SetActive(false);
-            }
         }
 
         public void Close()
