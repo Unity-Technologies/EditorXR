d4e210d160f047788a9549461d43a77ed934e200
dunity <dylanu@unity3d.com>
2019-01-30 17:26:13 -0800

    Add support for assigning gradient colors to the title icon in the CreatePrimitiveMenu controller

--- Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
+++ Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR
 using System;
+using UnityEditor.Experimental.EditorVR.Helpers;
 using UnityEditor.Experimental.EditorVR.Menus;
 using UnityEditor.Experimental.EditorVR.Utilities;
 using UnityEngine;
@@ -9,13 +10,21 @@ namespace UnityEditor.Experimental.EditorVR.Tools
     sealed class CreatePrimitiveMenu : MonoBehaviour, IMenu
     {
         const int k_Priority = 1;
+        const string k_BottomGradientProperty = "_ColorBottom";
+        const string k_TopGradientProperty = "_ColorTop";
+
+        [SerializeField]
+        Renderer m_TitleIcon;
 
         [SerializeField]
         GameObject[] m_HighlightObjects;
 
+        Material m_TitleIconMaterial;
+
         public Action<PrimitiveType, bool> selectPrimitive;
         public Action close;
 
+        readonly GradientPair k_EmptyGradient = new GradientPair(UnityBrandColorScheme.light, UnityBrandColorScheme.darker);
         public Bounds localBounds { get; private set; }
         public int priority { get { return k_Priority; } }
 
@@ -30,6 +39,9 @@ namespace UnityEditor.Experimental.EditorVR.Tools
         void Awake()
         {
             localBounds = ObjectUtils.GetBounds(transform);
+            m_TitleIconMaterial = MaterialUtils.GetMaterialClone(m_TitleIcon);
+            m_TitleIconMaterial.SetColor(k_TopGradientProperty, k_EmptyGradient.a);
+            m_TitleIconMaterial.SetColor(k_BottomGradientProperty, k_EmptyGradient.b);
         }
 
         public void SelectPrimitive(int type)
