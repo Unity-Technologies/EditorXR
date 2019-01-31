dddecbb0a98cb83e018453cb1304291537000f0b
dunity <dylanu@unity3d.com>
2019-01-30 16:32:27 -0800

    Cleanup formatting & conventions in CreatePrimitiveMenu

--- Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
+++ Tools/CreatePrimitiveTool/CreatePrimitiveMenu.cs
@@ -8,6 +8,8 @@ namespace UnityEditor.Experimental.EditorVR.Tools
 {
     sealed class CreatePrimitiveMenu : MonoBehaviour, IMenu
     {
+        const int k_Priority = 1;
+
         [SerializeField]
         GameObject[] m_HighlightObjects;
 
@@ -15,7 +17,7 @@ namespace UnityEditor.Experimental.EditorVR.Tools
         public Action close;
 
         public Bounds localBounds { get; private set; }
-        public int priority { get { return 1; } }
+        public int priority { get { return k_Priority; } }
 
         public MenuHideFlags menuHideFlags
         {
@@ -35,7 +37,7 @@ namespace UnityEditor.Experimental.EditorVR.Tools
             selectPrimitive((PrimitiveType)type, false);
 
             // the order of the objects in m_HighlightObjects is matched to the values of the PrimitiveType enum elements
-            for (var i = 0; i < m_HighlightObjects.Length; i++)
+            for (var i = 0; i < m_HighlightObjects.Length; ++i)
             {
                 var go = m_HighlightObjects[i];
                 go.SetActive(i == type);
@@ -47,7 +49,9 @@ namespace UnityEditor.Experimental.EditorVR.Tools
             selectPrimitive(PrimitiveType.Cube, true);
 
             foreach (var go in m_HighlightObjects)
+            {
                 go.SetActive(false);
+            }
         }
 
         public void Close()
