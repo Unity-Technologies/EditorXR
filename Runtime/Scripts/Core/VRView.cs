950ca3b4c9434fe412a3f7bef35f09a34a329ef3
Matt Schoen <schoen@unity3d.com>
2019-08-27 18:04:54 -0700

    Fix compiler warnings in 2019 related to InputTracking

--- Runtime/Scripts/Core/VRView.cs
+++ Runtime/Scripts/Core/VRView.cs
@@ -11,6 +11,10 @@ using UnityEngine.XR;
 using InputTracking = UnityEngine.XR.InputTracking;
 using TrackingSpaceType = UnityEngine.XR.TrackingSpaceType;
 
+#if UNITY_2019_1_OR_NEWER
+using System.Collections.Generic;
+#endif
+
 namespace UnityEditor.Experimental.EditorVR.Core
 {
     sealed class VRView
@@ -66,6 +70,10 @@ namespace UnityEditor.Experimental.EditorVR.Core
 
         Rect m_ToggleDeviceViewRect = new Rect(5, 0, 140, 20); // Width will be set based on window size
         Rect m_PresentationCameraRect = new Rect(0, 0, 165, 20); // Y position and width will be set based on window size
+
+#if UNITY_2019_1_OR_NEWER
+        XRNodeState m_HeadNode;
+#endif
 
         public static Transform cameraRig
         {
@@ -249,6 +257,19 @@ namespace UnityEditor.Experimental.EditorVR.Core
 
             if (viewEnabled != null)
                 viewEnabled();
+
+#if UNITY_2019_1_OR_NEWER
+            var nodeStates = new List<XRNodeState>();
+            InputTracking.GetNodeStates(nodeStates);
+            foreach (var nodeState in nodeStates)
+            {
+                if (nodeState.nodeType != XRNode.Head)
+                    continue;
+
+                m_HeadNode = nodeState;
+                break;
+            }
+#endif
         }
 
         static void CopyImagesEffectsToCamera(Camera targetCamera)
@@ -304,8 +325,19 @@ namespace UnityEditor.Experimental.EditorVR.Core
                 return;
 
             var cameraTransform = m_Camera.transform;
+
+#if UNITY_2019_1_OR_NEWER
+            Vector3 localPosition;
+            if (m_HeadNode.TryGetPosition(out localPosition))
+                cameraTransform.localPosition = localPosition;
+
+            Quaternion rotation;
+            if (m_HeadNode.TryGetRotation(out rotation))
+                cameraTransform.localRotation = rotation;
+#else
             cameraTransform.localPosition = InputTracking.GetLocalPosition(XRNode.Head);
             cameraTransform.localRotation = InputTracking.GetLocalRotation(XRNode.Head);
+#endif
         }
 
         public void CreateCameraTargetTexture(ref RenderTexture renderTexture, Rect cameraRect, bool hdr)
@@ -524,5 +556,5 @@ namespace UnityEditor.Experimental.EditorVR.Core
             }
         }
 #endif
+        }
     }
-}
