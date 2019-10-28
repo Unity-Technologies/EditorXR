f5fdfbcb84f57956e3da6d0474a54a7564dc54ec
Matt Schoen <schoen@unity3d.com>
2019-10-28 14:25:50 -0700

    Move version check into its own file in the CCU assembly; Split asmdef patch into PolyToolkit and SpatialTracking; Update readme to reflect current setup process; Add stub class with serialized fields to ensure EditorVR default references survive package import

---
+++ Scripts/Utilities/Editor/VersionCheck.cs
@@ -0,0 +1,33 @@
+﻿#if !(UNITY_2018_4_OR_NEWER || UNITY_2019_1_OR_NEWER)
+using UnityEngine;
+
+namespace UnityEditor.Experimental.EditorVR.Core
+{
+    [InitializeOnLoad]
+    class VersionCheck
+    {
+        const string k_ShowCustomEditorWarning = "EditorVR.ShowCustomEditorWarning";
+
+        static VersionCheck()
+        {
+            if (EditorPrefs.GetBool(k_ShowCustomEditorWarning, true))
+            {
+                var message = "EditorXR requires Unity 2018.4 or the latest, non-beta version of Unity.";
+                var result = EditorUtility.DisplayDialogComplex("Update Unity", message, "Download", "Ignore", "Remind Me Again");
+                switch (result)
+                {
+                    case 0:
+                        Application.OpenURL("https://unity3d.com/get-unity/download");
+                        break;
+                    case 1:
+                        EditorPrefs.SetBool(k_ShowCustomEditorWarning, false);
+                        break;
+                    case 2:
+                        Debug.Log("<color=orange>" + message + "</color>");
+                        break;
+                }
+            }
+        }
+    }
+}
+#endif
