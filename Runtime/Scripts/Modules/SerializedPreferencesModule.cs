6958e9c3577f15e1645e7683a2171d6be6c2451f
Matt Schoen <schoen@unity3d.com>
2019-11-01 01:19:55 -0700

    Move IntersectionModule back to EditorXR; Fix issues in play mode

--- Runtime/Scripts/Modules/SerializedPreferencesModule.cs
+++ Runtime/Scripts/Modules/SerializedPreferencesModule.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Labs.EditorXR.Interfaces;
+using Unity.Labs.ModuleLoader;
 using UnityEditor.Experimental.EditorVR.Core;
 using UnityEngine;
 
